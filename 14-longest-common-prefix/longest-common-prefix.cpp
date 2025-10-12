6e77754e3d28e2564526d12097198d804758eeb5
borisnorm <borisnorm@gmail.com>
2025-10-11 20:48:09 -0700

    Time: 0 ms (100.00%) | Memory: 11.6 MB (95.22%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+         if (strs.empty())
+           return "";
+
+         int m = strs.size();
+         int n = strs[0].size();
+
+         for (int col = 0; col < n; col++)
+         {
+            char c = strs[0][col];
+            for (int row = 1; row < m; row++)
+            {
+              if (strs[row].size() == col || strs[row][col] != c)
+                return strs[0].substr(0, col);  
+            }
+         }
+
+         return strs[0];
+    }
+};
