c9a3b682f9ef82ed7f0795d3f704ec32c20aadf1
borisnorm <borisnorm@gmail.com>
2025-10-12 07:24:23 -0700

    Time: 1 ms (86.88%) | Memory: 13.3 MB (74.83%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     vector<string> res;
-    string track = "";
+    string track;
     vector<string> generateParenthesis(int n) {
-
-        if (n <=0)
-          return res;
-
-        int left = n;    //左边有多少 左括号
-        int right = n;   // 右边有多少 右括号
-        backtrack(left, right);  
-
-        return res;
+         if (n <= 0)
+           return res;
+         
+         int left = n;
+         int right = n;
+         
+         backtrack(left, right);
+         
+         return res;
     }
 
     void backtrack(int left, int right)
     {
-      if (right < left) 
-        return;
-    
-      if (left < 0 || right < 0)
-        return;
-    
-      if (left == 0 && right ==0)
-      {
-        res.push_back(track);
-        return;
-      }
-      if (left > 0)
-      {
-        track.push_back('(');
-        backtrack(left-1, right);
-        track.pop_back();
-      }
+        if (right < left)
+          return;
 
-      if (right > left)
-      {
-        track.push_back(')');
-        backtrack(left, right-1);
-        track.pop_back();  
-      }  
+        if (right < 0)
+          return;
+         
+        if (left == 0 && right == 0)
+        {
+            res.push_back(track);
+            return;
+        }
+
+        if (left > 0)
+        {
+            track.push_back('(');
+            backtrack(left-1, right);
+            track.pop_back();
+        }
+
+        if (right > left)
+        {
+           track.push_back(')');
+           backtrack(left, right-1);
+           track.pop_back(); 
+        }
     }
-
 };
