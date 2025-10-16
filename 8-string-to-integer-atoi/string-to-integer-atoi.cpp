8ebaf5d30d43c80993560039db9ce4513a5738f7
borisnorm <borisnorm@gmail.com>
2025-10-16 12:45:43 -0700

    Time: 0 ms (100.00%) | Memory: 9.2 MB (54.71%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+
+        int sign = 1;
+        long long res = 0;
+
+        int i = 0;
+        while (i < n && s[i] == ' ')
+          i++;    
+        if (i == n)
+          return 0;
+
+        if (s[i] == '-')
+        {
+          sign = -1;
+          i++;
+        }
+        else if (s[i] == '+')
+        {
+          i++;
+        }
+        if (i==n)
+          return 0;
+
+        while (i < n && '0' <= s[i] && s[i] <= '9')
+        {
+            int digit = s[i] - '0';
+            
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > 7)) {
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
+
+            res = res*10 + digit;
+            
+           i++;
+        }
+
+        return (int) (res * sign);
+        
+    }
+};
