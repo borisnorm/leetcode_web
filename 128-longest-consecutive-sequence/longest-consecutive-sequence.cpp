d138b61cff02d477ef6b3941c4e439aa58369bb5
borisnorm <borisnorm@gmail.com>
2025-10-18 13:16:05 -0700

    Time: 94 ms (24.75%) | Memory: 88.9 MB (35.68%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.empty()) return 0;
+        if (nums.empty())
+          return 0;
 
-        unordered_set<int> s(nums.begin(), nums.end()); // 去重 + O(1) 查找
+        unordered_set<int> nums_set(nums.begin(), nums.end());
+
         int ans = 0;
+        int cur_len = 0;
+        int cur_num = 0;
+        for (int num : nums_set)   // for (int num: nums_set)
+        {
+           if (!nums_set.count(num-1))
+           {
+              // this num is the start point
+              cur_len = 1;
+              cur_num = num;
 
-        for (int x : s) {
-            // 只有在 x-1 不存在时，x 才可能是一个连续段的起点
-            if (!s.count(x - 1)) {
-                int cur = x;
-                int len = 1;
-                while (s.count(cur + 1)) {
-                    cur++;
-                    len++;
-                }
-                ans = max(ans, len);
-            }
+              while (nums_set.count(cur_num+1))
+              {
+                cur_len++;
+                cur_num = cur_num + 1;
+              }
+           }
+
+           ans = max (ans, cur_len);
         }
+
         return ans;
     }
 };
