59ce75503e5f73dec4b972b4ce3398c415a819a2
borisnorm <borisnorm@gmail.com>
2025-10-18 23:14:26 -0700

    Time: 0 ms (100.00%) | Memory: 52.3 MB (65.85%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+         if (nums.empty())
+           return false;
+
+         int farthest = 0;
+         int n = nums.size();
+         for (int i = 0; i < n-1; i++)
+         {
+            farthest = max(farthest, i + nums[i]);
+            if (farthest <= i)
+              return false;
+         }
+
+         return true;
+    }
+};
