8ece63cad1d559f237ddabe07cede0d4018efd89
borisnorm <borisnorm@gmail.com>
2025-10-18 17:03:38 -0700

    Time: 1 ms (36.10%) | Memory: 97.4 MB (60.61%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_price = INT_MAX;   // 到目前为止见过的最低买入价
-        int max_profit = 0;        // 最大收益（不能为负）
-        for (int p : prices) {
-            if (p < min_price) {
-                min_price = p;     // 更新最低买点
-            } else {
-                // 以今天价格作为卖点，尝试更新收益
-                max_profit = max(max_profit, p - min_price);
-            }
-        }
-        return max_profit;
-    }
+       int min_price = INT_MAX;
+       int max_profit = 0;
+
+       for (int p: prices)
+       {
+          if (p < min_price)
+            min_price = p;
+          else
+            max_profit = max(max_profit, (p-min_price));
+       }
+
+       return max_profit;
+    };
+   
 };
