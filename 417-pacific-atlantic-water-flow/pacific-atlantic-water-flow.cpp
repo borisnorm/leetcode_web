84601114e6dbd6d184221293ea64555fec61da34
borisnorm <borisnorm@gmail.com>
2025-10-18 20:13:26 -0700

    Time: 0 ms (100.00%) | Memory: 23.7 MB (38.20%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,47 +1,30 @@
 
-
 class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
          if (heights.empty())
            return {};
-
          if (heights[0].empty())
            return {};
-
-         int m = heights.size();
+         int m = heights.size();
          int n = heights[0].size();
 
          vector<vector<int>> pac(m, vector<int>(n, 0));
          vector<vector<int>> alt(m, vector<int>(n, 0));
 
-    auto dfs = [&](auto&& self, int r, int c, vector<vector<int>>& vis) -> void {
-      vis[r][c] = 1;
-      static int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-      for (auto& d : dirs)
-      {
-        int nr = r + d[0];
-        int nc = c + d[1];
-        if (nr < 0 || nr >= m || nc < 0 || nc >= n)
-          continue;
-        if (!vis[nr][nc] && heights[nr][nc] >= heights[r][c])
-          self(self, nr, nc, vis);
-      }
-    };
- 
     //pac 
     for (int r = 0; r < m; r++)
-      dfs(dfs, r, 0, pac);
+      dfs(r, 0, pac, heights);
     
     for (int c = 0; c < n; c++)
-      dfs(dfs, 0, c, pac);
+      dfs(0, c, pac, heights);
 
     //alt
     for (int r = 0; r < m; r++)
-      dfs(dfs, r, n-1, alt);
+      dfs(r, n-1, alt, heights);
     
     for (int c = 0; c < n; c++)
-      dfs(dfs, m-1, c, alt);
+      dfs(m-1, c, alt, heights);
 
     vector<vector<int>> ans;
 
@@ -51,10 +34,28 @@ public:
            ans.push_back({r, c});
 
     return ans;
+  }
 
+    void dfs(int r, int c, vector<vector<int>>& vis, vector<vector<int>>& heights)
+    {
+        vis[r][c] = 1;
+        static int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int m = vis.size();
+        int n = vis[0].size();
 
+        for (auto& d : dirs)
+        {
+          int nr = r + d[0];
+          int nc = c + d[1];
 
+          if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+            continue;
 
-
+          if (!vis[nr][nc] && heights[nr][nc] >= heights[r][c])
+            dfs(nr, nc, vis, heights);
+        }
     }
+    
+
+
 };
