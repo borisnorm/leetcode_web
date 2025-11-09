4e863dc56c6693a6966e9f21c7756794f3035f65
borisnorm <borisnorm@gmail.com>
2025-11-08 19:41:07 -0800

    Time: 0 ms (100.00%) | Memory: 29.8 MB (45.03%) - LeetSync

---
+++ 593-valid-square/valid-square.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+  
+    using T = vector<int>;
+    
+    long long cal_dist(T& x, T& y)
+    {
+       long long dist_val = (x[0]-y[0])*(x[0]-y[0]) + (x[1]-y[1])*(x[1]-y[1]);
+       return dist_val;
+    }
+
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        // distancance p1 -> p2 == p3 -> p2
+        vector<T> points = {p1, p2, p3, p4};
+
+        vector<long long> dist_vec;
+        for (int i = 0; i < 4; i++)
+          for (int j = i + 1; j < 4; j++)
+            dist_vec.push_back(cal_dist(points[i], points[j]));
+
+        sort(dist_vec.begin(), dist_vec.end());
+
+        if (dist_vec[0] == 0)
+           return false;
+        
+        if (!(dist_vec[0] == dist_vec[1] && dist_vec[1] == dist_vec[2] && dist_vec[2] == dist_vec[3]))
+          return false;
+
+        if (!(dist_vec[4] == dist_vec[5]))
+          return false;
+
+        if (!(2* dist_vec[0] == dist_vec[4]))
+          return false;
+
+        return true;
+        
+    }
+};
