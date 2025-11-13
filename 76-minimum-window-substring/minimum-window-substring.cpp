3cdd750f64a0fe7e95daa0e231cb26041582a662
borisnorm <borisnorm@gmail.com>
2025-11-12 18:29:32 -0800

    Time: 4 ms (74.81%) | Memory: 11.8 MB (10.38%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        string res = "";
+        if (s.empty() || t.empty()) 
+          return res;
+
+        int m = s.size();
+        int n = t.size();
+         
+        unordered_map<char, int> need;
+        unordered_map<char, int> window;
+
+        for (char c : t)
+          need[c]++;
+
+        int l = 0;
+        int r = 0;
+        int valid = 0;
+        int len = INT_MAX;
+        int start = 0;
+
+        while (r < m)
+        {
+            char c = s[r];
+            r++;
+
+            if (need.count(c))
+            {
+               window[c]++;
+               if (window[c] == need[c])
+                 valid++;
+            }
+
+            while (valid == need.size())
+            {
+              
+               if (r - l  < len)
+               {
+                 len = r - l;
+                 start = l;
+               }
+
+               char d = s[l];
+
+               if (need.count(d))
+               {
+                 if(window[d] == need[d])
+                    valid--;
+                  window[d]--;
+               } 
+               l++;
+            }
+        }
+    
+       return  len == INT_MAX ? "" : s.substr(start, len);
+    }
+};
