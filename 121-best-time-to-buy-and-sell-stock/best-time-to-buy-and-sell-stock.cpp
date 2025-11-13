6d5fa9c551868ba2fa04f5f4fbb8621a3a074701
borisnorm <borisnorm@gmail.com>
2025-11-13 12:59:40 -0800

    Time: 0 ms (100.00%) | Memory: 97.3 MB (86.85%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+
+    /*
        int min_price = INT_MAX;
        int max_profit = 0;
 
@@ -12,7 +14,45 @@ public:
             max_profit = max(max_profit, (p-min_price));
        }
 
-       return max_profit;
+       return maxprofit;
+       */
+
+
+       int n = prices.size();
+       int maxProfit = 0;
+       int minPrice = INT_MAX;
+       // 不能 一次循环 直接找出 minPrice  与 maxPrice, 因为 有 时序关系在 控制着, 
+       // 只能变计算 max profit, 边前进
+
+       for (int i = 1; i < n; i++)
+       {
+          minPrice = min(minPrice, prices[i-1]);
+
+          if (prices[i] > minPrice)
+            maxProfit = max(maxProfit, (prices[i] - minPrice));
+       }
+
+       return maxProfit;
+
+       /*
+       // Time Limit Exceeded
+
+       int n = prices.size();
+       int maxProfit = 0;
+       int minPrice = INT_MAX;
+       for (int i = 0; i < n; i++)
+       {
+          minPrice = min(minPrice, prices[i]);
+          for (int j = i + 1; j < n; j++)
+          {
+              if (prices[j] > minPrice)
+                maxProfit = max(maxProfit, (prices[j]-minPrice));
+          }
+       }
+ 
+       return maxProfit;
+       */
+
     };
    
 };
