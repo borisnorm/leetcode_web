52bcfbbe6689eb8c5d706c465f4597320066c1a1
borisnorm <borisnorm@gmail.com>
2025-11-18 10:38:52 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (24.22%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,22 +5,27 @@ public:
             return 0;
           
           int n = nums.size();
-          vector<int> dp1(n, 0);
-          vector<int> dp2(n, 0);
 
-          dp1[0] = nums[0];
-          dp2[0] = nums[0];
+          long long maxVal = nums[0];
+          long long minVal = nums[0];
+          long long ans = nums[0];
 
-          for (int i = 1; i < nums.size(); i++)
+          for (int i = 1; i < n; i++)
           {
-             dp1[i] = max({dp1[i-1]*nums[i], dp2[i-1]*nums[i], nums[i]});
-             dp2[i] = min({dp1[i-1]*nums[i], dp2[i-1]*nums[i], nums[i]});
+              long long x = nums[i];
+              if (x < 0)
+                swap(maxVal, minVal);
+            
+              maxVal = max(x, maxVal * x);
+              minVal = min(x, minVal * x);
+
+              ans = max(ans, maxVal);
           }
 
-          int max_val = INT_MIN;
-          for (int i = 0; i < n; i++)
-             max_val = max(dp1[i], max_val);
-
-          return max_val;
+          return (int)ans;
     }
 };
+
+
+       //   vector<long long> dpMax(n, 1);
+       //   vector<long long> dpMin(n, 1);
