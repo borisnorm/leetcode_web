503dd3114919cbe75fbfd8cc65c43ecce6494acb
borisnorm <borisnorm@gmail.com>
2025-11-29 10:19:49 -0800

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.22%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        if (nums.empty())
+          return {};
+
+        int n = nums.size();
+
+        vector<int> res(n, 1);
+
+        for (int i = 1; i < n; i++)
+         res[i] = res[i-1] * nums[i-1];
+
+        int tmp = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            res[i] = res[i] * tmp;
+            tmp = nums[i] * tmp;
+        }
+
+        return res;
+    }
+};
