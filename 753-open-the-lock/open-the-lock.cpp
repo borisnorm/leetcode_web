048962af18f29caadb0f08ffdbb7c994acb3b7cd
borisnorm <borisnorm@gmail.com>
2025-11-29 17:47:50 -0800

    Time: 250 ms (29.65%) | Memory: 120.7 MB (13.71%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string> dead(deadends.begin(), deadends.end());
+
+        if (dead.count("0000"))
+          return -1;
+        
+        if (target == "0000")
+          return 0;
+
+        auto neighbors=[&](const string& s){
+            vector<string> res;
+            for (int i = 0; i < 4; i++)
+            {
+                string t = s;
+
+                t[i] = (s[i] == '9') ? '0' : s[i] + 1;
+                res.push_back(t);
+
+                t[i] = (s[i] == '0') ? '9' : s[i] - 1;
+                res.push_back(t);
+            }
+
+            return res;
+        };
+
+        queue<string> q;
+        unordered_set<string>  visited;
+        q.push("0000");
+        visited.insert("0000");
+
+        int steps = 0;
+        while (!q.empty())
+        {
+           int q_size = q.size();
+           steps++;
+           for (int i = 0; i < q_size; i++)
+           {
+              string cur = q.front();
+              q.pop();
+
+              for (auto& nei : neighbors(cur))
+              {
+                 if (dead.count(nei) || visited.count(nei))
+                   continue;
+                
+                 if (nei == target)
+                   return steps;
+
+                  visited.insert(nei);
+
+                  q.push(nei);
+              }
+           }
+        }
+
+        return -1;
+    }
+
+};
