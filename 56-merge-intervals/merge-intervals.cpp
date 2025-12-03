298ed6fd7799dd4e441dae7f7a8bf7315d2d0bcc
borisnorm <borisnorm@gmail.com>
2025-12-03 09:12:37 -0800

    Time: 3 ms (89.99%) | Memory: 23.8 MB (59.98%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -16,10 +16,9 @@ public:
           {
              auto& last = res.back();
              if (intervals[i][0] <= last[1])
-                last[1] = max(last[1], intervals[i][1]);
+               last[1] = max(last[1], intervals[i][1]);
              else
                res.push_back(intervals[i]);
-
           }
 
           return res;
