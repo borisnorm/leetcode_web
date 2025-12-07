03d6d9f4c44391593d092c1f1e1a0c6a43c0e555
borisnorm <borisnorm@gmail.com>
2025-12-06 19:59:52 -0800

    Time: 2 ms (46.62%) | Memory: 10.9 MB (17.76%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        if (s.empty())
-          return "";
-
-        string res;
-
-        //vector<pair<char, int>> char2idx;
-        vector<int> char2idx;
+        unordered_set<char> vowels_set = {'a', 'e', 'i', 'o','u', 'A', 'E', 'I', 'O', 'U'};
 
         int n = s.size();
-        for (int i = 0; i < n; i++)
-        {
-          if (isVowel(s[i]))
-            char2idx.push_back(i);
-          //  char2idx.push_back(make_pair(s[i], i)); 
-        }
 
         int l = 0;
-        int r = char2idx.size() - 1;
+        int r = n - 1;
 
         while (l < r)
         {
-           //int l_idx = char2idx[l].second;
-           //int r_idx = char2idx[r].second;
+           while (l < r && !vowels_set.count(s[l]))
+             l++;
 
-           int l_idx = char2idx[l];
-           int r_idx = char2idx[r];
-           swap(s[l_idx], s[r_idx]);
+           while (l < r && !vowels_set.count(s[r]))
+             r--;
 
-           l++;
-           r--;
+           if (l < r)
+           {
+              swap(s[l], s[r]);
+              l++;
+              r--;
+           }
+
         }
 
-        res = s;
-
-        return res;
-    }
-
-    bool isVowel(const char c)
-    {
-       if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-           c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-         return true;
-    
-       return false;
+       return s;
     }
 };
+ 
