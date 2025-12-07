2b2db9da22406de88926decd6c51ab0baa718285
borisnorm <borisnorm@gmail.com>
2025-12-06 23:08:01 -0800

    Time: 2 ms (85.26%) | Memory: 47.9 MB (16.32%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int n = nums.length;
+
+        int slow = 0;
+        for (int fast = 0; fast < n; fast++)
+        {
+           if (nums[fast] != 0)
+           {
+              int tmp = nums[slow];
+              nums[slow] = nums[fast];
+              nums[fast] = tmp;
+              slow++;
+           }
+        }
+    }
+}
