bf302f34ae8951a965fce8359671cb4079653fba
borisnorm <borisnorm@gmail.com>
2025-12-10 08:57:01 -0800

    Time: 0 ms (100.00%) | Memory: 9.2 MB (68.91%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -3,6 +3,8 @@ public:
     int uniquePaths(int m, int n) {
        vector<vector<int>> dp(m, vector<int>(n, 1));
 
+       // m and n was init by default 1, 代表路径数
+       // 开始条件也是从 1 开始的
        for (int i = 1; i < m; i++)
          for (int j = 1; j < n; j++)
            dp[i][j] = dp[i][j-1] + dp[i-1][j];
