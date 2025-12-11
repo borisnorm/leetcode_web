dc2761cb9fbdf19f2a36ce31acdafcbdade8766c
borisnorm <borisnorm@gmail.com>
2025-12-11 12:46:29 -0800

    Time: 0 ms (100.00%) | Memory: 14.5 MB (75.92%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -2,12 +2,37 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         
-         int size = 1 << n;
-         vector<int> res(size);
+        
+        vector<int> res;
+        res.push_back(0);
+        if (n == 0)
+          return res;
+
+       // G(n) = G(n-1) + reverse(G(n-1)) with highest bit = 1
+    
+       for (int i = 1; i <= n; i++)
+       {
+          int prefix = 1 << (i-1);
+          int size = res.size();
+          
+          for (int j = size - 1; j >= 0; j--)
+             res.push_back(res[j] | prefix);
+       }
+        
+        return res;
+    }
+
+
+    /*
+    vector<int> grayCode(int n) {
+        
+        int size = 1 << n;
+        vector<int> res(size);
         // gray[i] = i ^ (i >> 1);
         for (int i = 0; i < size; i++)
           res[i] = i ^ (i >> 1);
         
         return res;
     }
+    */
 };
