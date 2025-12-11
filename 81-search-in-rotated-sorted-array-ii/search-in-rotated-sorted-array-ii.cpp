c79a6695873b1ec41a921644a5094010850b55a1
borisnorm <borisnorm@gmail.com>
2025-12-11 09:46:36 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (55.51%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        
-
         int n = nums.size();
+        if (n == 0) return false;
+
         int l = 0;
         int r = n - 1;
 
-       // 奇数 落在 中间,  偶数 落在左侧, 所以while 循环中 l 不需要 == r
-       // (0 + 1) /2 = 0
-       // (1 + 2) /2 = 1
-       // (2 + 3) /2 = 2
-       //[l, r]
-       while (l <= r)
-       {
-          int mid = l + (r-l)/2;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
 
-          if (nums[mid] == target)
-            return true;
-          
-          //全相同就收缩 重新判定
-          if (nums[l] == nums[mid] && nums[mid] == nums[r])
-          {
-             l++;
-             r--;
-          }
-          else if (nums[mid] < nums[r])
-          {
-            // pivot in left side
-            if (nums[mid] < target && target <= nums[r])
-              l = mid + 1;
-            else
-              r = mid - 1;
-          }
-          else if (nums[mid] >= nums[l])
-          {  
-            // pivot in the right side
-            if (nums[l] <= target && target < nums[mid])
-              r = mid - 1;
-            else
-              l = mid + 1;
-          }
-          else
-            r--;
-       }
+            if (nums[mid] == target)
+                return true;
 
-       return false;
+            // 关键：处理重复元素的情况
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                // 无法判断哪一边有序，只能缩小范围
+                l++;
+                r--;
+            }
+            // 左半边有序
+            else if (nums[l] <= nums[mid]) {
+                // target 在左半边
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            // 右半边有序
+            else {
+                // target 在右半边
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return false;
     }
 };
