b897cd5d04e745d665d0b5381dcc1fb464065515
borisnorm <borisnorm@gmail.com>
2025-12-11 07:19:46 -0800

    Time: 0 ms (100.00%) | Memory: 15.4 MB (87.76%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -18,6 +18,13 @@ public:
         ListNode* even = head->next;
         ListNode* evenHead = even;
 
+       //            |--------------|
+       //            |   [3] + [4]  |
+       //  (1) --  (2) --- (3) --- (4) --- (5)
+       //   |   [1] + [2]   ^
+       //   |---------------|
+ 
+        //odd 永远在 even 的后面, even 先到 nullptr, 保证了 odd 不会为 nullptr,而是最后一个节点,进而可以 链接新的 even list
         while (even && even->next)
         {
            odd->next = even->next;
