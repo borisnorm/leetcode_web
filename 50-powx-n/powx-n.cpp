25f87b2655f2b1abf47886a6acfa448bf2bfd762
borisnorm <borisnorm@gmail.com>
2025-12-11 17:25:03 -0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.09%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -12,13 +12,14 @@ public:
 
         double result = 1.0;
 
+        //x^n = x^(n/2) * x^(n/2) * x^(n%2)
         while (exp)
         {
            if (exp & 0x1)
-              result *= x;
+              result = result * x;    // 当 exp (n) 是一个奇数的时候, 第一次运行的时候, result 就被赋予了 x, 作为 x 的奇数项 乘进去
 
-           x = x * x;
-           exp >>= 1;
+           x = x * x;  // x(x^2) * x(x^2) = x^4
+           exp >>= 1;  // exp = exp/2
         }
 
         return result;
