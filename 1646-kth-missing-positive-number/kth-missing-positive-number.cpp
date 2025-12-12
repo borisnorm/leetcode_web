a22371420382fcdb90691085f1cf1b7ba932bc0c
borisnorm <borisnorm@gmail.com>
2025-12-12 07:43:35 -0800

    Time: 0 ms (100.00%) | Memory: 13.3 MB (59.06%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
+
+        int n = (int)arr.size();
+
+        auto missing = [&](int i){
+            return arr[i] - (i+1);
+        };
+        
+        if (missing(n-1) < k)
+          return arr[n-1] + (k - missing(n-1));
+
+        int l = 0;
+        int r = n - 1;
+
+        while (l < r)
+        {
+            int mid = l + (r-l)/2;
+            if (missing(mid) >= k)
+              r = mid;
+            else
+              l = mid + 1;
+        }
+        // arr[l-1] < 第 k 个 missing 数 ≤ arr[l]
+        // arr[l-1] < x ≤ arr[l]
+        // arr[0] ....... arr[l-1] 共 l 个
+        // x(不缺失时候应该出现的数字数量) - l (缺失时 出现的数字数量) = k 
+        return l + k;
+
+    }
+
+/*
+        int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
         
         unordered_set<int> arr_set;
@@ -24,4 +55,5 @@ public:
            return arr[n-1] + offset;
         }
     }
+    */
 };
