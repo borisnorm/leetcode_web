b2b4193e860b48caa017d30fbfcae16de8e14421
borisnorm <borisnorm@gmail.com>
2025-12-13 23:32:34 -0800

    Time: 2 ms (51.60%) | Memory: 7.8 MB (84.06%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    int count = 0;
+
+    vector<bool> col;
+    vector<bool> diag1;
+    vector<bool> diag2;
+
+
+    int totalNQueens(int n) {
+
+        col.assign(n, false);
+        diag1.assign(2*n, false);
+        diag2.assign(2*n, false);
+
+        dfs(0, n);
+
+        return count;
+    }
+
+    void dfs(int row, int n)
+    {
+       if  (row == n)
+       {
+          count++;
+          return;
+       }
+
+       for (int c = 0; c < n; c++)
+       {
+
+            if (col[c])
+              continue;
+            
+            if (diag1[row - c + n])
+              continue;
+            
+            if (diag2[row + c ])
+              continue;
+            
+
+            col[c] = true;
+            diag1[row - c +n ] = true;
+            diag2[row +c] = true;
+
+            dfs(row + 1, n);
+
+
+            col[c] = false;
+            diag1[row -c + n] = false;
+            diag2[row + c] = false;
+       }
+    }
+};
