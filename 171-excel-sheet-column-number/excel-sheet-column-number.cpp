5389e9dbe394fd8ba22ce86b6a200f8ceeab3f60
borisnorm <borisnorm@gmail.com>
2025-12-14 10:45:34 -0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (15.80%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
+    
+       if (columnTitle.empty())
+         return 0;
+        
+       int n = columnTitle.size();
+       long long base = 1;
+       long long sum  = 0;
+
+       for (int i = n - 1; i >= 0; i--)
+       {
+          int digit = columnTitle[i] - 'A' + 1;
+          // digit 在 'Z' 时候没有 取余数 mode, 就是就是 digit = 26 - 1 + 1 = 26 
+          // sum = 1 * 26 = 26;
+          sum += base * digit;
+          base = base * 26;
+       }
+
+       return sum;
+    
+    }
+
+
+    /*
+    int titleToNumber(string columnTitle) {
         if (columnTitle.empty())
           return 0;
 
@@ -15,4 +39,5 @@ public:
 
         return static_cast<int>(sum);
     }
+    */
 };
