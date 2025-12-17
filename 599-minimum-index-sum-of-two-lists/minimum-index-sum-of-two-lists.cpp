ad8b6f95f18cd1973b442f42586badef3664b700
borisnorm <borisnorm@gmail.com>
2025-12-16 23:18:19 -0800

    Time: 7 ms (96.61%) | Memory: 41.4 MB (24.50%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+
+vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        vector<string> res;
+
+        int m = list1.size();
+        int n = list2.size();
+        
+        unordered_map<string, int> list1_word2idx;
+        for (int i = 0; i < m; i++)
+          list1_word2idx[list1[i]] = i;
+
+        int minIdxSum = INT_MAX;
+        for (int j = 0; j < n; j++)
+        {
+            if (list1_word2idx.count(list2[j]))
+            {
+               int idxSum = j + list1_word2idx[list2[j]];
+               if (idxSum < minIdxSum)
+               {
+                  res.clear();
+                  res.push_back(list2[j]);
+                  minIdxSum = idxSum;
+               }
+               else if (idxSum == minIdxSum)
+                 res.push_back(list2[j]);
+            }
+        }
+
+        return res;
+    }
+    /*
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         vector<string> res;
 
@@ -9,7 +40,7 @@ public:
         int iSum = INT_MAX;
         for (int i = 0; i < m; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++)   // hash map  在这里使用
             {
                if (list1[i] == list2[j])
                {
@@ -27,6 +58,6 @@ public:
         }
 
         return res;
-
     }
+    */
 };
