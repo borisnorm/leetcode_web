2acd82c24fc346a3821b17dac00597f3277cef8a
borisnorm <borisnorm@gmail.com>
2025-12-17 17:19:47 -0800

    Time: 0 ms (100.00%) | Memory: 9.7 MB (63.26%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+
+  vector<vector<int>> generate(int numRows) {
+      vector<vector<int>> res;
+      if (numRows == 0)
+        return res;
+
+      for (int i = 0; i < numRows; i++)
+      {
+         vector<int> row(i+1, 1);
+
+         for (int j = 1; j < i; j++)
+           row[j] = res[i-1][j-1] + res[i-1][j];
+        
+         res.push_back(row);
+      }
+
+      return res;
+  }
+
+
+   /*
     vector<vector<int>> generate(int numRows) {
         if (numRows <= 0)
           return {};        
@@ -21,12 +42,10 @@ public:
             int n = pre.size();
 
             row.push_back(1);
-            
             for (int j = 1 ; j < n; j++)
             {
                row.push_back(pre[j-1] + pre[j]);   
             }
-
             row.push_back(1);
             
 
@@ -35,4 +54,6 @@ public:
 
         return res;
     }
+
+    */
 };
