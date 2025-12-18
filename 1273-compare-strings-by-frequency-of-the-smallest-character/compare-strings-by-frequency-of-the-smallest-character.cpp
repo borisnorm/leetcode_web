b9c1418d972923eb08c8e088a4abdfe5a10f74b8
borisnorm <borisnorm@gmail.com>
2025-12-18 10:26:20 -0800

    Time: 0 ms (100.00%) | Memory: 16 MB (54.40%) - LeetSync

--- 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,9 +1,56 @@
 class Solution {
 public:
+
+    int f(const string& s)
+    {
+       char minChar = 'z';
+       int cnt = 0;
+       
+       for (char c: s)
+       {
+          if (c < minChar)
+          {
+             minChar = c;
+             cnt = 1;
+          }
+          else if (c == minChar)
+            cnt++;
+       }
+
+       return cnt;
+    }
+    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+        vector<int> wf;
+        for (auto & word: words)
+        {
+           wf.push_back(f(word));
+        }
+
+        // sort 是为了 upper_bound 的 二分法
+        sort(wf.begin(), wf.end());
+
+        vector<int> res;
+
+        for (auto & query : queries)
+        {
+            int fq = f(query);
+
+            auto it = upper_bound(wf.begin(), wf.end(), fq);
+            
+            // it > fq 的 iterator, wf.end(), 是数组的 最后一个空位
+            // wf [ 1, 2, 3, 4, 5, wf.end()]
+            // wf.end()-it, 就是比 fq 大的元素的个数
+            res.push_back(wf.end()-it);
+        }
+
+        return res;
+
+    }
+/*
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
         
         int m = words.size();
-        vector<char> fwordFreq(m, 0);
+        vector<int> fwordFreq(m, 0);
 
         int idx = 0;
         for (string word : words)
@@ -63,4 +110,5 @@ public:
 
         return res;
     }
+    */
 };
