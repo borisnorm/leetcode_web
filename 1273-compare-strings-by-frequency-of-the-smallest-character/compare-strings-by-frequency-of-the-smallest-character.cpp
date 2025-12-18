e8287f32ab6e0cb3d78a7c4b37fe85a5a6c4ce77
borisnorm <borisnorm@gmail.com>
2025-12-18 09:40:52 -0800

    Time: 26 ms (16.04%) | Memory: 16.2 MB (42.77%) - LeetSync

---
+++ 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+        
+        int m = words.size();
+        vector<int> fwordFreq(m, 0);
+
+        int idx = 0;
+        for (string word : words)
+        {
+           int charfreq[26] = {0};
+           for (int i = 0; i < word.size(); i++)
+             charfreq[word[i]-'a']++;
+            
+           int minFreq = INT_MAX;
+           for (int i = 0; i < 26; i++)
+           {
+              if (charfreq[i] != 0 && charfreq[i] < minFreq)
+              {
+                 minFreq = charfreq[i];
+                 break;
+              }
+           }
+
+           fwordFreq[idx++] = minFreq;
+        }
+
+        int n = queries.size();
+        vector<int> fqueryFreq(n, 0);
+        int qidx = 0;
+        
+        for (string query: queries)
+        {
+           int charfreq[26] = {0};
+           for (int i = 0; i < query.size(); i++)
+             charfreq[query[i]-'a']++;
+
+           int minFreq = INT_MAX;
+           for (int i = 0; i < 26; i++)
+           {
+              if (charfreq[i] != 0 && charfreq[i] < minFreq)
+              {
+                 minFreq = charfreq[i];
+                 break;
+              }
+           }
+
+           fqueryFreq[qidx++] = minFreq;
+        }
+
+        vector<int> res;
+        for (int i = 0; i < fqueryFreq.size(); i++)
+        {
+            int cnt = 0;
+            for (int j = 0; j < fwordFreq.size(); j++)
+            {
+               if (fwordFreq[j] > fqueryFreq[i])
+                 cnt++;
+            }
+
+            res.push_back(cnt);
+        }
+
+        return res;
+    }
+};
