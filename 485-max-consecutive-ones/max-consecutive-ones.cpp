4b8a5972936cdc7dc8937ece4b75b78b06fcfa1c
borisnorm <borisnorm@gmail.com>
2025-12-18 19:25:52 -0800

    Time: 0 ms (100.00%) | Memory: 50.1 MB (67.86%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -4,15 +4,16 @@ public:
         
         int n = nums.size();
         int zero_cnt = 0;
-        int max_zero_cnt  = INT_MIN;
+        int max_zero_cnt  = 0;
         for (int i = 0; i < n; i++)
         {
            if (nums[i] == 1)
+           {
              zero_cnt++;
+             max_zero_cnt = max(max_zero_cnt, zero_cnt);
+           }
            else
              zero_cnt = 0;
-           
-           max_zero_cnt = max(max_zero_cnt, zero_cnt);
         }
 
         return max_zero_cnt;
