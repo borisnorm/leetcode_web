5c88d8a2c3bedae5af140f4bfb3f3ec0f8bc406c
borisnorm <borisnorm@gmail.com>
2025-12-20 18:15:24 -0800

    Time: 51 ms (61.68%) | Memory: 13.6 MB (78.01%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+       
+         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+         for (auto str: strs)
+         {
+            int zero_cnt = 0;
+            int one_cnt = 0;
+
+            for (auto c: str)
+            {
+                if (c == '0')
+                  zero_cnt++;
+                else
+                  one_cnt++;
+            }
+
+            for (int i = m; i >= zero_cnt; i--)
+            {
+                for (int j = n; j >= one_cnt; j--)
+                {
+                    dp[i][j] = max(dp[i][j],  dp[i-zero_cnt][j-one_cnt] + 1);
+                }
+            }
+         }
+
+
+         return dp[m][n];
+    }
+};
