81e248e05429d95d058089b9d1be361f30f31915
borisnorm <borisnorm@gmail.com>
2025-12-23 16:54:10 -0800

    Time: 0 ms (100.00%) | Memory: 10.5 MB (78.62%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> dp(n+1, 0);
-        for (int i = 1; i <=n ; i++)
-          dp[i] = dp[i & (i-1)] + 1;
-        return dp;   
+        for (int i = 1; i <= n; i++)
+           dp[i] = dp[(i & (i-1))] + 1;
+        return dp;
     }
 };
