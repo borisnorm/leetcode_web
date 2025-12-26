aa0e7aad296ab79db08dd6d13ff258d2dcddbec3
borisnorm <borisnorm@gmail.com>
2025-12-25 22:54:04 -0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (69.38%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     bool hasSameDigits(string s) {
+         int n = s.size();
+
+         if (n < 2)
+           return false;
+        
+         while(n > 2)
+         {
+            for (int i = 0; i < n - 1 ; i++)
+            {
+               int digit = ((s[i] - '0') + (s[i+1]-'0')) % 10;
+               s[i] = char(digit);
+            }
+            n--;
+         }
+
+         return s[0] == s[1];
+
+    }
+
+    /*
+    bool hasSameDigits(string s) {
         
         int n = s.size();
         if (n < 2)
@@ -14,17 +35,19 @@ public:
               for (int j = i + 1; j < n; j++)
               {
                  int digit = ((s[i] - '0') + (s[j] - '0')) % 10;
-                 num_str.push_back(digit + '0');
+                 num_str.push_back(char(digit + '0'));
                  //num_str.push_back(to_string(digit));
                  break;
               }
            }
 
-           s = num_str;
-           //n = s.size();
-           n--;
+           s = move(num_str);
+           n = s.size();
+           //n--;
         }
 
         return s[0] == s[1];
     }
+
+    */
 };
