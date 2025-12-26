3df508e9c187c14f41cef40534d2ef6932ba6776
borisnorm <borisnorm@gmail.com>
2025-12-26 00:49:23 -0800

    Time: 7 ms (13.93%) | Memory: 16.1 MB (10.96%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<string> res;
+    vector<string> letterCasePermutation(string s) {
+        if (s.empty())
+          return  {};
+        
+        string permuation;
+        backtrack(s, 0, permuation);
+
+        return res;
+    }
+
+    void backtrack(string s, int pos, string permutation)
+    {
+    
+       int n = s.size();
+       if (permutation.size() == s.size())
+       {
+          res.push_back(permutation);
+          return;
+       }
+       else if (pos >= n)
+       {
+          return;
+       }
+       
+       /*
+       if (isdigit(s[pos]))
+       {
+         permutation += s[pos];
+         pos++;
+       }
+       */
+
+       if (isdigit(s[pos]))
+       {
+          permutation.push_back(s[pos]);
+          backtrack(s, pos + 1, permutation);
+          permutation.pop_back();
+       }
+       else if (isalpha(s[pos]))
+       {
+          char c = s[pos];
+          
+          permutation.push_back(tolower(s[pos]));
+          backtrack(s, pos + 1, permutation);
+          permutation.pop_back();
+
+          permutation.push_back(toupper(s[pos]));
+          backtrack(s, pos + 1, permutation);
+          permutation.pop_back();
+       }
+
+       return;
+    }
+};
