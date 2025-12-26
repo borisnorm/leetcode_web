a90377a42717ebc24bf891ccbf243714e9e476f3
borisnorm <borisnorm@gmail.com>
2025-12-26 14:55:23 -0800

    Time: 8 ms (38.87%) | Memory: 25.7 MB (47.47%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -14,11 +14,35 @@ public:
             int b = nums[i-1];
             int c = nums[i];
             if (a + b > c)
+               return a + b + c;
+        }
+
+        return 0;
+    }
+
+    /*
+      // 还是慢一点
+    int largestPerimeter(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3)
+          return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int maxPerim = 0;
+        for (int i = n - 1; i - 2 >= 0; i--)
+        {
+            int a = nums[i-2];
+            int b = nums[i-1];
+            int c = nums[i];
+            if (a + b > c)
                maxPerim = max(maxPerim, (a+b+c));
         }
 
         return maxPerim;
     }
+
+    */
 
 
    /*
