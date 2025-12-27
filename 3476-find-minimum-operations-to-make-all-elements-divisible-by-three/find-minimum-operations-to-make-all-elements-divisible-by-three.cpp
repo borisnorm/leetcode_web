eb0185dfb5c24c03361e34de9d4f62c955a09997
borisnorm <borisnorm@gmail.com>
2025-12-26 20:32:09 -0800

    Time: 0 ms (100.00%) | Memory: 23.1 MB (78.69%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+   int minimumOperations(vector<int>& nums) {
+        if (nums.empty())
+         return 0;
+
+       // 一次操作可以对某个元素 `+1` 或 `-1`。
+       //对任意数 `x`，只看 `r = x % 3`：
+       //r == 0`：已经能被 3 整除，0 次
+       //r == 1`：`x-1` 就能整除，1 次（比 `x+2` 更优）
+       //r == 2`：`x+1` 就能整除，1 次（比 `x-2` 更优）
+
+       //所以每个元素只要 **不是 3 的倍数，就一定需要 1 次操作**。
+       // 答案就是统计 `num % 3 != 0` 的个数。 
+        int minOps = 0;
+        for (int i = 0; i < nums.size(); i++)
+          if (nums[i] % 3 != 0)
+            minOps++;
+        return minOps;
+   }
+    /*
     int minimumOperations(vector<int>& nums) {
         // 0 3 6 9 12 
 
@@ -34,7 +53,6 @@ public:
         }
 
         return minOps;
-   
-
     }
+    */
 };
