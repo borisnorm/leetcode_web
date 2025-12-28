3b1b32185546dcb77a080b82f8e2ccb3a2791bee
borisnorm <borisnorm@gmail.com>
2025-12-27 16:42:18 -0800

    Time: 0 ms (100.00%) | Memory: 13.8 MB (51.29%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -11,9 +11,13 @@ public:
         unordered_set<int> num_set;
         for (int i = 0; i < arr.size(); i++)
         {
+
+            // 10, 20
             if (i > 0 && arr[i] % 2 == 0 && num_set.count(arr[i]/2))
               return true;
 
+             // 当 第1个数是后面2倍的时候 
+             // -20 -10
             if (i > 0 && num_set.count(arr[i]*2))
               return true;
 
