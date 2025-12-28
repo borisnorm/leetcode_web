0203b51a8a288606719f4a83c11c85eb1ead32f7
borisnorm <borisnorm@gmail.com>
2025-12-27 17:10:26 -0800

    Time: 0 ms (100.00%) | Memory: 8.3 MB (64.51%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+  int numJewelsInStones(string jewels, string stones) {
+        bool jewelType[128] = {false};
+        for (char c : jewels)
+          jewelType[c] = true;
+
+        int cnt = 0;
+        for (char c: stones)
+          if (jewelType[c])
+            cnt++;
+        
+        return cnt;
+  }
+/*
     int numJewelsInStones(string jewels, string stones) {
         vector<int> stonefreq(128, 0);
 
@@ -14,6 +27,7 @@ public:
         }
 
         return stoneCnt;
-
     }
+
+    */
 };
