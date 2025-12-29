02b2f5f7e99a4ccd4c591a97067b58bfd836b960
borisnorm <borisnorm@gmail.com>
2025-12-29 15:38:33 -0800

    0408-VALID-WORD-ABBREVIATION Stats: Time: 0 ms (100%), Space: 8 MB (22.36%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
     bool validWordAbbreviation(string word, string abbr) {
-        int m = word.size();
-        int n = abbr.size();
-
-        if (word == abbr)
-           return true;
-
-        int i = 0; 
-        int j = 0;
-        while (i < m && j < n){
-
-            while (i < m && j < n && word[i] == abbr[j])
+         int m = word.size();
+         int n = abbr.size();
+         int i = 0; 
+         int j = 0;
+         while (i < m && j < n){
+            if (isalpha(abbr[j]))
             {
-              i++;
-              j++;
+                if (word[i] != abbr[j])
+                  return false;
+                
+                i++;
+                j++;
             }
-
-            if (i == m && j == n)
-              return true;
-            
-            if (!(i < m && j < n))
-              return false;
-
-            if (word[i] != abbr[j] && !isdigit(abbr[j]))
-              return false;
-            
-            if (isdigit(abbr[j]) && abbr[j] == '0')
-              return false;
-
-            int num = 0;
-            while (j < n && isdigit(abbr[j]))
+            else
             {
-              num = num * 10 + (abbr[j]-'0');
-              j++;
+               if (j < n && abbr[j] == '0')
+                 return false;
+                
+               int num = 0;
+               while (j < n && isdigit(abbr[j])){
+                  num = num*10 + (abbr[j]-'0');
+                  j++;
+               }
+
+               i+= num;
             }
-
-            i+= num;
-            if (i < m && j < n && word[i] != abbr[j])
-              return false;
-        }
-
-        if (i == m && j == n)
-          return true;
-
-        return false;
+         }
+         return i == m && j == n;
     }
 };
