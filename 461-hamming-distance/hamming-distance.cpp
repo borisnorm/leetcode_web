5e10f7a27d29d008c21c62dc2f13556efa08e25b
borisnorm <borisnorm@gmail.com>
2025-12-28 21:16:50 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (34.67%) - LeetSync

--- 461-hamming-distance/hamming-distance.cpp
+++ 461-hamming-distance/hamming-distance.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
+        int n = x ^ y;
+
+        int cnt = 0;
+
+        while (n)
+        {
+           n = n & (n-1);
+           cnt++;
+        }
+
+        return cnt;
+        
+    }
+};
+
+/*
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int n = x ^ y;
+        vector<int> dp(n+1, 0);
+        
+        for (int i = 1; i <= n; i++)
+        {
+           dp[i] = dp[i >> 1] + (i & 0x1);
+        }
+
+        return dp[n];
+    }
+};
+
+*/
+
+
+/*
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
         
         unsigned int n = (x ^ y);
         int cnt = 0;
@@ -14,3 +52,4 @@ public:
         return cnt;
     }
 };
+*/
