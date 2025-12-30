7db20c49d73aaad77fc38c0d0e0d0d5aad995ec8
borisnorm <borisnorm@gmail.com>
2025-12-29 19:10:35 -0800

    Time: 0 ms (100.00%) | Memory: 31.2 MB (79.11%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        int l = 0;
+        int r = n;
+
+        while (l < r)
+        {
+           int mid = l + (r-l)/2;
+           if (nums[mid] < target)
+           {
+              l = mid + 1;
+           }
+           else if (nums[mid] > target)
+           {
+              r = mid;
+           }
+           else
+           {
+              return mid;
+           }
+        }
+
+        return -1;
+    }
+};
