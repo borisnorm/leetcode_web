43de14f405db1f23ce379f799459438a76b0ed16
borisnorm <borisnorm@gmail.com>
2025-12-29 21:32:41 -0800

    0270-CLOSEST-BINARY-SEARCH-TREE-VALUE Stats: Time: 0 ms (100%), Space: 21.2 MB (78.78%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
@@ -9,6 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int closestValue(TreeNode* root, double target) {
@@ -35,3 +48,41 @@ public:
           return closestNodeVal;
     }
 };
+
+ /*
+class Solution {
+public:
+    double minDiff = INT_MAX * 1.0;
+    int    minDiffNode = INT_MAX;
+    int closestValue(TreeNode* root, double target) {
+         minDiffNode = root ? root->val : 0; // é²ç©º
+         dfs(root, target);
+
+         return minDiffNode;
+    }
+
+    void dfs(TreeNode* root, double target)
+    {
+         if (!root)
+           return;
+
+         double diff = fabs(target - root->val);
+         if (diff < minDiff)
+         {
+            minDiff = diff;
+            minDiffNode = root->val; 
+         }
+         else if (diff == minDiff)
+         {
+            if (root->val < minDiffNode)
+              minDiffNode = root->val;
+         }
+         
+         if (root->left && root->val > target)
+           dfs(root->left, target);
+        
+         if (root->right && root->val < target)
+           dfs(root->right, target);
+    }
+};
+*/
