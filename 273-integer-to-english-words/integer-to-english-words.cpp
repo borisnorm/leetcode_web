bbdb70c1b081583a7e1ed1c9c450b6e9101fc63e
borisnorm <borisnorm@gmail.com>
2025-12-30 18:01:28 -0800

    Time: 3 ms (39.63%) | Memory: 12.5 MB (5.61%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+
+   vector<string> below_10 = {
+    "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
+
+   vector<string> below_20 = {
+   "", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" 
+   };
+
+   vector<string> below_100 = {
+    "",  "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+   string helper(int num){
+       if (num == 0)
+         return "";
+
+       if (num <= 10)
+         return below_10[num] + " ";
+
+       if (num < 20)
+         return below_20[num%10] + " ";
+
+       if (num < 100)
+         return below_100[num/10] + " " + helper(num%10);
+
+       return below_10[num/100] + " Hundred" + " " + helper(num%100);
+   }
+
+    string numberToWords(int num) {
+        if (num == 0)
+          return "Zero";
+
+        string res;
+        long long base;
+
+        base = 1000000000;
+        if (num >= base)
+        {
+           int data = num / base;
+           res += helper(data) + "Billion" + " " ;
+           num = num % base;
+        }    
+
+        base = 1000000;
+        if (num >= base)
+        {
+           int data = num / base;
+           res += helper(data) + "Million" + " ";
+           num = num % base;
+        }
+
+        base = 1000;
+        if (num >= base)
+        {
+           int data = num / base;
+           res += helper(data) + "Thousand" + " ";
+           num = num % base;
+        }
+
+       if (num > 0)
+         res += helper(num);
+
+
+       if (!res.empty() && res.back() == ' ')
+         res.pop_back();
+    
+       return res;
+
+
+    }
+
+};
