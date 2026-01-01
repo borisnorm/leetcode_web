e0d393e0ca0fd1e6b87ac99b8c4e24f97c5e6abc
borisnorm <borisnorm@gmail.com>
2025-12-31 16:27:14 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.30%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+
+    int maximumSwap(int num) {
+        
+        string num_str = to_string(num);
+        int n = num_str.size();
+
+        vector<int> lastPos(10, -1);  // 0-9
+
+        for (int i = 0; i < n; i++)
+        {
+          char c = num_str[i];
+          lastPos[c-'0'] = i;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+           int digit = num_str[i] - '0';
+
+           for (int d = 9; d > digit; d--)
+           {
+              if (lastPos[d] > i)
+              {
+                 swap(num_str[i], num_str[lastPos[d]]);
+                 return stoi(num_str);
+              }
+           } 
+        }
+        
+        return num;
+
+    }
+/*
     int maximumSwap(int num) {
         
         string num_str = to_string(num);
@@ -16,6 +48,7 @@ public:
                 } 
             }
 
+            // 遇到 相等的情况下, 没有 num_str[maxPos] > num_str[i], 他就错误地直接返回了
             if (maxPos != i && num_str[maxPos] > num_str[i])
             {
               swap(num_str[i], num_str[maxPos]);
@@ -25,4 +58,6 @@ public:
 
         return num;
     }
+
+    */
 };
