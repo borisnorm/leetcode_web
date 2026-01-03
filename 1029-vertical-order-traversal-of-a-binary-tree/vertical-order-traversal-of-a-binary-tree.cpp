b411f274e00b1e936fd592404d7f78d5d3a4125a
borisnorm <borisnorm@gmail.com>
2026-01-02 22:11:05 -0800

    Time: 3 ms (39.61%) | Memory: 15.2 MB (96.09%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<tuple<int, int, int>> nodes;
+        
+        dfs(root, 0,  0, nodes);
+
+        using T = tuple<int, int, int>;
+        auto cmp = [](const T& a, const T& b){
+            if (get<0>(a) != get<0>(b))
+               return get<0>(a) < get<0>(b);
+            
+            if (get<1>(a) != get<1>(b))
+               return get<1>(a) < get<1>(b);
+            
+            return get<2>(a) < get<2>(b);
+        };
+
+        sort(nodes.begin(), nodes.end(), cmp);
+
+        vector<vector<int>> res;
+        int lastCol = INT_MIN;
+
+        for(auto& [col, row, val]: nodes)
+        {
+            if (col != lastCol)
+            {
+               res.push_back({});
+               lastCol = col;
+            }
+
+            res.back().push_back(val);
+        }
+
+        return res;
+    }
+
+    void dfs(TreeNode* node, int row, int col, vector<tuple<int, int, int>>& nodes)
+    {
+        if (!node)
+          return;
+        
+        nodes.emplace_back(col, row, node->val);
+
+        dfs(node->left, row + 1, col - 1, nodes);
+        dfs(node->right, row + 1, col + 1, nodes);
+    }
+};
