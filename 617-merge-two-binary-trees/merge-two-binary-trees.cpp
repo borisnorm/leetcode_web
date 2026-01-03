d397c9b82c494934c33d05ebb7eaf96039c591b7
borisnorm <borisnorm@gmail.com>
2026-01-02 16:20:31 -0800

    Time: 3 ms (21.35%) | Memory: 34.9 MB (15.61%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/merge-two-binary-trees.cpp
@@ -9,6 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+
+    }
+};
+*/
+/*
+
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
@@ -49,7 +59,11 @@ public:
         return root1;
     }
 };
+
+*/
+
 /*
+// 正确
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
@@ -68,21 +82,22 @@ public:
 
 */
 
- /*
+
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if (!root1)
-          return root2;
-        if (!root2)
-          return root1;
+          if (!root1 && !root2)
+            return nullptr;
+          
+          int val = (root1 ? root1->val : 0) +
+                    (root2 ? root2->val : 0);
+          TreeNode* root = new TreeNode(val);
 
-        TreeNode* root =  new TreeNode(root1->val + root2->val);
-
-        root->left = mergeTrees(root1->left, root2->left);
-        root->right = mergeTrees(root1->right, root2->right);
-
-        return root;
+          root->left = mergeTrees(root1 ? root1->left: nullptr, 
+                                  root2 ? root2->left: nullptr);
+          root->right = mergeTrees(root1 ? root1->right: nullptr, 
+                                   root2 ? root2->right: nullptr);
+           
+          return root;
     }
 };
-*/
