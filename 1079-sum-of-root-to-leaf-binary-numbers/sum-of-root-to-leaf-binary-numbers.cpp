0e0fcd1d798dfe917df70c54b4b54f4537725391
borisnorm <borisnorm@gmail.com>
2026-01-10 08:16:20 -0800

    Time: 0 ms (100.00%) | Memory: 17.5 MB (27.66%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -12,6 +12,42 @@
 class Solution {
 public:
 
+    int sumRootToLeaf(TreeNode* root) {
+        if (!root)
+          return 0;
+
+        stack<pair<TreeNode*, int>> st;
+
+        int sum = 0;
+        st.push({root, 0});
+
+        while (!st.empty())
+        {
+            auto [node, cur] = st.top();
+            st.pop();
+
+            cur = cur << 1 | node->val;
+
+            if (!node->left && !node->right)
+            {
+                sum += cur;
+            }
+
+            if (node->left)
+              st.push({node->left, cur});
+            if (node->right)
+              st.push({node->right, cur});
+        }
+
+        return sum;
+    }
+
+};
+
+/*
+class Solution {
+public:
+
     long long sum = 0;
 
     int sumRootToLeaf(TreeNode* root) {
@@ -28,7 +64,42 @@ public:
        if (!root->left && !root->right)
           return path;
 
+      //父节点只是把左右子树的结果 加起来
       return dfs(root->left, path) + dfs(root->right, path);
        
     }
 };
+*/
+
+
+/*
+class Solution {
+public:
+
+    long long sum = 0;
+
+    int sumRootToLeaf(TreeNode* root) {
+        int path = 0;
+        dfs(root, 0);
+
+        return sum;
+    }
+
+    void dfs(TreeNode* root, int path)
+    {
+       if (!root)
+         return;
+    
+       path = path << 1 | root->val;
+       
+       if (!root->left && !root->right)
+       {
+          sum += path;
+       }
+
+       dfs(root->left, path);
+       dfs(root->right, path);
+    }
+};
+
+*/
