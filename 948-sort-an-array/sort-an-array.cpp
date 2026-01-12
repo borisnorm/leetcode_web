7ba2ed80b7895fbdcca46e1a6493544bd0c126f2
borisnorm <borisnorm@gmail.com>
2026-01-11 17:42:27 -0800

    Time: 1455 ms (7.76%) | Memory: 71 MB (83.30%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+
+vector<int> sortArray(vector<int>& nums) {
+     int n = nums.size();
+     quickSort(nums, 0, n-1);
+    return nums;
+}
+
+void quickSort(vector<int>& nums, int left, int right)
+{
+    if (left >= right)
+      return;
+
+    int pivotIdx =  left + rand() % (right-left + 1);
+    int pivot = nums[pivotIdx];
+
+    swap(nums[pivotIdx], nums[right]);
+
+    int i = left;
+    for (int j = left; j < right; j++)
+    {
+        if (nums[j] < pivot)
+        {
+            swap(nums[i], nums[j]);
+            i++;
+        }
+    }
+    swap(nums[i], nums[right]);
+
+    quickSort(nums, left, i-1);
+    quickSort(nums, i+1, right);
+}
+  /*
     vector<int> sortArray(vector<int>& nums) {
         int n = nums.size();
         mergeSort(nums, 0, n-1);
@@ -44,5 +76,7 @@ public:
         for (int i = 0; i < n; i++)
          nums[left+i] = temp[i];
     }
+
+    */
 
 };
