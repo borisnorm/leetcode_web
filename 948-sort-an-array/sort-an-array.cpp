1998eecf748be8599c7d241832857d383ea7dc85
borisnorm <borisnorm@gmail.com>
2026-01-11 20:13:40 -0800

    Time: 1505 ms (5.94%) | Memory: 70.9 MB (83.30%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -2,6 +2,41 @@ class Solution {
 public:
 
 vector<int> sortArray(vector<int>& nums) {
+   int n = nums.size();
+   quickSort(nums, 0, n-1);
+   return nums;
+}
+
+void quickSort(vector<int>& nums, int left, int right)
+{
+    if (left >= right)
+      return;
+    
+    int pivotIdx = left + rand() % (right - left + 1);
+    int pivot = nums[pivotIdx];
+
+    swap(nums[pivotIdx], nums[right]);
+
+    int i = left;
+    for (int j = left; j < right; j++)
+    {
+        if (nums[j] < pivot)
+        {
+           swap(nums[i], nums[j]);
+           i++;
+        }
+    }
+
+    swap(nums[i], nums[right]);
+
+    quickSort(nums, left, i-1);
+    quickSort(nums, i+1, right);
+
+}
+
+
+/*
+vector<int> sortArray(vector<int>& nums) {
      int n = nums.size();
      quickSort(nums, 0, n-1);
     return nums;
@@ -31,6 +66,7 @@ void quickSort(vector<int>& nums, int left, int right)
     quickSort(nums, left, i-1);
     quickSort(nums, i+1, right);
 }
+*/
   /*
     vector<int> sortArray(vector<int>& nums) {
         int n = nums.size();
