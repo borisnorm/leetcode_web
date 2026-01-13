8229dd530ba34808e07346678b8fe827a0a8c414
borisnorm <borisnorm@gmail.com>
2026-01-13 11:47:08 -0800

    Time: 108 ms (22.86%) | Memory: 182.2 MB (26.57%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,6 +1,8 @@
 class LRUCache {
     int capacity;
     list<pair<int, int>> kvl;
+
+    // list 的 iterator 是那个 pair <int, int>
     unordered_map<int, list<pair<int, int>>::iterator> key2it; 
 public:
     LRUCache(int capacity) {
@@ -10,9 +12,8 @@ public:
     int get(int key) {
         if (!key2it.count(key))
           return -1;
-        
-        auto it = key2it[key];
-        int val = it->second;
+
+        int val = key2it[key]->second;
         updateLRUentry(key, val);
 
         return val;
@@ -20,32 +21,40 @@ public:
     
     void put(int key, int value) {
         
+        // key 不在 kv iter map 里面, 且 kvl 达到它的上限
         if (!key2it.count(key) && kvl.size() == capacity)
           removeLRUentry();
         
+        // 如果 key 存在, 就更新 这个 key with this value
         updateLRUentry(key, value);
     }
 
     void updateLRUentry(int key, int val)
     {
+       //delete it from the list
        if (key2it.count(key))
        {
           auto it = key2it[key];
           kvl.erase(it);
        }
 
+       // add it to front
        kvl.push_front({key, val});
-       auto it = kvl.begin();
-       key2it[key] = it;
+
+       // update it kv 
+       key2it[key] = kvl.begin();
     }
     
     void removeLRUentry()
     {
+       // 找到最后一个元素, kvl.end是 最后元素的下一个元素, 是空的
        auto it = prev(kvl.end());
 
+       // 根据 key 在  key2it 中进行删除
        int key = it->first;
        key2it.erase(key);
 
+       // pop掉最尾端 least visted 的 entry
        kvl.pop_back();
     }
 };
