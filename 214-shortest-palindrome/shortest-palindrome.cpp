73e61e695da23b431abfbf79982713e005f21bca
borisnorm <borisnorm@gmail.com>
2026-01-12 17:24:21 -0800

    Time: 1 ms (82.06%) | Memory: 15.6 MB (51.60%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,5 +1,41 @@
 class Solution {
 public:
+
+    string shortestPalindrome(string s) {
+      
+       int n = s.size();
+       
+       string r = s;
+       reverse(r.begin(), r.end());
+       
+       string t = s + '#' + r;
+
+       int m = t.size();
+
+       vector<int> lps(m, 0);
+       for (int i = 1; i < m; i++)
+       {
+          int j = lps[i-1];
+
+          while (j > 0 && t[i] != t[j])
+            j = lps[j-1];
+
+          if (t[i] == t[j])
+            j++;
+    
+          lps[i] = j;
+       }
+
+       int L = lps.back();
+       
+       string add = s.substr(L);
+       reverse(add.begin(), add.end());
+
+       return add + s;
+
+    }
+
+/*
     string shortestPalindrome(string s) {
         int n = s.size();
         if (n <= 1)
@@ -30,7 +66,6 @@ public:
         reverse(add.begin(), add.end());
 
         return add + s;
-
-
     }
+    */
 };
