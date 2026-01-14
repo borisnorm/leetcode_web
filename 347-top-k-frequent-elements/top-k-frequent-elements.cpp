b82089d51105945f1fdbc454895e7b9579a0ec64
borisnorm <borisnorm@gmail.com>
2026-01-14 12:38:11 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (84.11%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -17,19 +17,21 @@ public:
 
         vector<int> res;
         int n = nums.size();
-        unordered_map<int, int> mp;
-        for (int i  = 0; i < n; i++)
-          mp[nums[i]]++;
 
-        auto cmp = [](pair<int, int>& a, pair<int, int>& b) {
+        unordered_map<int, int> num2freq;
+        for (int i  = 0; i < n; i++)
+          num2freq[nums[i]]++;
+
+        //小顶堆是 大的元素 沉在 堆底  
+        using T = pair<int, int>;
+        auto cmp = [](const T& a, const T& b) {
               return a.second > b.second;   
         };
+        priority_queue<T,  vector<T>, decltype(cmp)> pq;  //小顶堆
 
-        priority_queue<pair<int, int>,  vector<pair<int, int>>, decltype(cmp)> pq;  //小顶堆
-
-        for (auto& iter: mp)
+        for (auto it: num2freq)
         {
-           pq.push(make_pair(iter.first, iter.second));
+           pq.push({it.first, it.second});
 
            if (pq.size() > k)
              pq.pop();
@@ -38,6 +40,7 @@ public:
         int cnt = 0;
         while(!pq.empty() && cnt < k)
         {
+           //栈顶是 freq 最小的元素, 然后随着 pop, freq 依次变大
            pair<int, int> top_pair = pq.top();
            pq.pop();
 
