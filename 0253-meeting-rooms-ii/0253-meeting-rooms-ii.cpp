b5f2350dca99bf9f89143f00dabc999191e8e10f
borisnorm <borisnorm@gmail.com>
2026-01-15 00:38:11 -0800

    0253-MEETING-ROOMS-II Stats: Time: 0 ms (100%), Space: 16.3 MB (50.93%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,5 +1,50 @@
 class Solution {
 public:
+int minMeetingRooms(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        if (n <= 1)
+          return n;
+
+        // get begins and ends vector
+        vector<int> begin(n);
+        vector<int> end(n);
+        for (int i = 0; i < n; i++)
+        {
+           begin[i] = intervals[i][0];
+           end[i] = intervals[i][1];
+        }
+
+        // sort begin and ends
+        sort(begin.begin(), begin.end());
+        sort(end.begin(), end.end());
+
+        // handle the conflict with time by sweep the line
+
+        int i = 0;
+        int j = 0;
+        int room = 0;
+        int res = 0;
+
+        while (i < n && j < n)
+        {
+            if (begin[i] < end[j])
+            {
+                room++;
+                i++;
+            }
+            else
+            {
+                room--;
+                j++;
+            }
+
+            res = max(res, room);
+        }
+
+        return res;
+}
+
+/*
     int minMeetingRooms(vector<vector<int>>& intervals) {
         int n = intervals.size();
         vector<int> begin(n);
@@ -36,4 +81,5 @@ public:
 
         return res;        
     }
+    */
 };
