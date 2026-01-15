20b6c1e2d0dfd3aa21a5cb3ce3cdabbfd61475e1
borisnorm <borisnorm@gmail.com>
2026-01-15 07:47:36 -0800

    Time: 5 ms (10.62%) | Memory: 63 MB (47.50%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+
+         int n = height.size();
          int left = 0;
-         int right = height.size()-1;
-         int area = 0;
+         int right = n-1;
+         int maxArea = 0;
 
          while (left < right)
          {
             int w = right - left;
             int h = min(height[left], height[right]);
-            int tmp_area = h * w;
-            area = max(area, tmp_area);
+            int area = h * w;
+            maxArea = max(maxArea, area);
             
+
+           // for next higher height for shorter side;
+           // higher height can get large area, even with less width.
+           // 在短板一侧 向前移动找更大的挡板, 去计算可能的更大值, 挡板越大,蓄水越多
             if (height[left] < height[right])
               left++;
             else
               right--;
          }
 
-         return area;  
+         return maxArea;  
     }
 };
