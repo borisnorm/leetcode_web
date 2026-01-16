703e3d749f30940472c66eb8c553bfc85c3a1ce0
borisnorm <borisnorm@gmail.com>
2026-01-15 19:22:17 -0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (86.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,5 +1,57 @@
 class Solution {
 public:
+   bool isValid(string s) {
+      // more left
+      // more right
+      // mismatched left and right;
+
+      if (s.empty())
+        return false;
+
+      stack<char> st;
+      for (char c: s)
+      {
+        if (c == '[' || c == '(' || c == '{')
+        {
+           st.push(c);
+        }
+        else
+        {
+           // more right parenthese
+           if (st.empty())
+             return false;
+           
+           if (c == matchedChar(st.top()))
+           {
+              st.pop();
+           }
+           else
+           {
+              return false;
+           }
+        }
+
+         
+      }
+      return st.empty() ? true: false;
+   }
+
+   char matchedChar(char c)
+   {    
+     // if (c == '')
+      //  return '';
+
+      if (c == '(')
+        return  ')';
+      else if (c == '[')
+        return  ']';
+      else if (c == '{')
+        return  '}';
+      else 
+        return  ' ';
+
+   }
+    /*
     bool isValid(string s) {
        stack<char> st;
        unordered_map<char, char> mp = {
@@ -26,5 +78,5 @@ public:
        return st.empty();
     }
 
-
+    */
 };
