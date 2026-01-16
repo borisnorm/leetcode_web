24232d6a4a514954201dec216f26b7ddfe152ec6
borisnorm <borisnorm@gmail.com>
2026-01-15 23:37:53 -0800

    Time: 0 ms (100.00%) | Memory: 29.6 MB (65.70%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -11,10 +11,23 @@ public:
         k = k % n;
         if (!k)
           return;
+ 
+        //[1, 2, 3, 4, 5, 6, 7]
 
+        //[7, 6, 5, 4, 3, 2, 1]
         reverse(nums.begin(), nums.end());
+
+        // start_idx + k = end_idx
+        // [start_idx, end_idx)
+        // end_idx - start_idx = k
+        // reverse的语义
+        // [5, 6, 7, 4, 3, 2, 1]
         reverse(nums.begin(), nums.begin()+k);
+
+        // [5, 6, 7, 1, 2, 3, 4]
         reverse(nums.begin()+k, nums.end());
+
+         //[5, 6, 7, 1, 2, 3, 4]
 
         return;  
     }
