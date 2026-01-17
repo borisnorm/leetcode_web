d6982dbc5619d69c98423fcc7aca9431f033b287
borisnorm <borisnorm@gmail.com>
2026-01-17 15:19:24 -0800

    Time: 0 ms (100.00%) | Memory: 8.3 MB (8.86%) - LeetSync

---
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+int makeTheIntegerZero(int num1, int num2) {
+
+    if (num1 == 0)
+      return -1;
+
+    for (long long k = 0; k <= 60; k++)
+    {
+        long long x = num1 - k * num2;
+
+        int pc = __builtin_popcountll(x);
+
+        if (pc <= k && k <= x)
+          return (int)k;
+    }
+
+    return -1;
+}
+  /*
+    int makeTheIntegerZero(int num1, int num2) {
+        
+        // num1(i) = num1(i-1) - 2^i - num2 = 0;
+        //           num1(i-1) - num2 = 2 ^i
+
+        //  num1 = num1 - 2^(i+1) - num2;
+
+        int cnt = 0;
+        while(num1 != 0)
+        {
+        
+           num1 = num1 - (2 * cnt > 0 ? 2 * cnt : 1) - num2;
+           cnt++;
+
+           if(num1 == 0) 
+             return cnt; 
+        }
+
+        return -1;
+    }
+    */
+};
