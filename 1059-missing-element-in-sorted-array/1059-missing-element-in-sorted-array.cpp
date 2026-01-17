080669a41ba377779543ff08e498ba53d3154a3e
borisnorm <borisnorm@gmail.com>
2026-01-16 17:43:40 -0800

    1059-MISSING-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 35.4 MB (38.73%) - LeetHub

--- 1059-missing-element-in-sorted-array/1059-missing-element-in-sorted-array.cpp
+++ 1059-missing-element-in-sorted-array/1059-missing-element-in-sorted-array.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
+    
+        int n = nums.size();
+
+        auto miss = [&](int i){
+            return nums[i] - nums[0] - i;
+        };
+
+        if (miss(n-1) < k)
+          return nums[n-1] + (k - miss(n-1));
+        
+        int l = 1;
+        int r = n;
+
+        // first >= k
+        while(l < r)
+        {
+           int mid = l + (r-l)/2;
+           
+           if (miss(mid) < k)
+              l = mid + 1;
+           else
+              r = mid;
+        }
+
+        return nums[l-1] + (k - miss(l-1)); 
+    }
+/*
+    int missingElement(vector<int>& nums, int k) {
         
         int n = nums.size();
         int base = nums[0];
@@ -20,6 +48,8 @@ public:
            base = nums[i];
         }
 
+        // the last one;
         return base + k;
     }
+    */
 };
