eb430ccc9585e477f76261d4cd08b264cb0b5a8a
borisnorm <borisnorm@gmail.com>
2026-01-17 21:46:10 -0800

    Time: 4 ms (29.02%) | Memory: 9.3 MB (19.62%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        
+
+         vector<unsigned long long> dp(target + 1, 0);
+         dp[0] = 1;
+
+         for (int i = 1; i <= target; i++)
+         {
+            for (int num : nums)
+            {
+                if (i - num >= 0)
+                {
+                   //dp[i] 表示 不选
+                   //dp[i-num] 表示 选了 num 后 剩余的 方案数
+                   dp[i] = dp[i] + dp[i-num];
+                }
+            }
+
+         }
+
+         return dp[target];
+    }
+};
