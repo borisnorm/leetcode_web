91b253ac664282236c4536dcab4c3f6db34f4048
borisnorm <borisnorm@gmail.com>
2026-01-17 21:55:00 -0800

    Time: 0 ms (100.00%) | Memory: 9.3 MB (19.62%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        
+        const long long LIM = (long long)INT_MAX; // 最终返回 int，就截到这里
+        vector<long long> dp(target + 1, 0);
+        dp[0] = 1;
 
-         vector<unsigned long long> dp(target + 1, 0);
-         dp[0] = 1;
-
-         for (int i = 1; i <= target; i++)
-         {
-            for (int num : nums)
-            {
-                if (i - num >= 0)
-                {
-                   //dp[i] 表示 不选
-                   //dp[i-num] 表示 选了 num 后 剩余的 方案数
-                   dp[i] = dp[i] + dp[i-num];
+        for (int s = 1; s <= target; s++) {
+            long long cur = 0;
+            for (int x : nums) {
+                if (s >= x) {
+                    cur += dp[s - x];
+                    if (cur >= LIM) { // 防止 cur 继续涨到 long long 溢出
+                        cur = LIM;
+                        break;
+                    }
                 }
             }
-
-         }
-
-         return dp[target];
+            dp[s] = cur;
+        }
+        return (int)dp[target];
     }
 };
