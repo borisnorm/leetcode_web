ebbcbbb5e64d71d8d397255dbb3dd83037681392
borisnorm <borisnorm@gmail.com>
2026-01-17 22:00:07 -0800

    Time: 0 ms (100.00%) | Memory: 9.2 MB (46.57%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -5,19 +5,52 @@ public:
         vector<long long> dp(target + 1, 0);
         dp[0] = 1;
 
-        for (int s = 1; s <= target; s++) {
-            long long cur = 0;
-            for (int x : nums) {
-                if (s >= x) {
-                    cur += dp[s - x];
-                    if (cur >= LIM) { // 防止 cur 继续涨到 long long 溢出
+        for (int i = 1; i <= target; i++) 
+        {
+            long long cur = dp[i];
+
+            for (int num : nums) 
+            {
+                if (i - num >= 0) 
+                {
+                    cur += dp[i - num];
+                    if (cur >= LIM) // 防止 cur 继续涨到 long long 溢出
+                    { 
                         cur = LIM;
                         break;
                     }
                 }
             }
-            dp[s] = cur;
+            dp[i] = cur;
         }
         return (int)dp[target];
     }
 };
+
+/*
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        
+
+         vector<unsigned long long> dp(target + 1, 0);
+         dp[0] = 1;
+
+         for (int i = 1; i <= target; i++)
+         {
+            for (int num : nums)
+            {
+                if (i - num >= 0)
+                {
+                   //dp[i] 表示 不选
+                   //dp[i-num] 表示 选了 num 后 剩余的 方案数
+                   dp[i] = dp[i] + dp[i-num];
+                }
+            }
+
+         }
+
+         return dp[target];
+    }
+};
+*/
