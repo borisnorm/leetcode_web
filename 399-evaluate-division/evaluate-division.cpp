2a672ba0074341d16b49275473ee0bd4a4e9b38a
borisnorm <borisnorm@gmail.com>
2026-01-19 14:38:44 -0800

    Time: 3 ms (18.24%) | Memory: 12.3 MB (14.91%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        
+        int n = equations.size();
+
+        unordered_map<string, vector<pair<string, double>>> graph;
+        for (int i  = 0; i < n; i++)
+        {
+            auto equation = equations[i];
+
+            string a = equation[0];
+            string b = equation[1];
+            double val = values[i];
+
+            graph[a].push_back({b, val});
+            graph[b].push_back({a, 1.0/val});
+        }
+
+        vector<double> res;
+        for (auto& q: queries)
+        {
+           unordered_set<string> visited;
+
+           double ans = dfs(graph, q[0], q[1], visited);
+           
+           res.push_back(ans);
+        }
+
+        return res;
+    }
+
+    double dfs(unordered_map<string, vector<pair<string, double>>>& graph,
+               string cur, string target, unordered_set<string>& visited)
+    {
+
+        if (!graph.count(cur) || !graph.count(target))
+           return -1.0;
+
+        if (cur == target)
+           return 1.0;
+
+         visited.insert(cur);
+
+        for (auto& [next, weight] : graph[cur])
+        {
+            if (visited.count(next))
+              continue;
+
+            double sub = dfs(graph, next, target, visited);
+            if (sub != -1.0)
+            {
+                return sub * weight;
+            }
+        }
+        
+        // 没有关联的, 比如 a, b, c, d   a/b = 2, b/c= 2,  然后木诶呦 b/d 或是 c/d 的关系
+        return -1.0;
+    }
+};
