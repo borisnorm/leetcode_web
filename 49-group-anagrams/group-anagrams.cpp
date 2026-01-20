9b070e89d11b4e456b80df28f04ab9d6c77dd63c
borisnorm <borisnorm@gmail.com>
2026-01-20 15:41:08 -0800

    Time: 63 ms (15.33%) | Memory: 29.9 MB (20.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,27 +3,29 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
         
-        unordered_map<string, vector<string>> mp;
-        for (auto& s : strs)
+        unordered_map<string, vector<string>> key2anagram;
+        for (auto& word : strs)
         {
+           //统计频率
            int cnt[26]={0};
-           for (char c: s)
+           for (char c: word)
              cnt[c-'a']++;
 
+           //由频次生成 key
            string key;
            for (int i = 0; i < 26; i++)
            {
               key.push_back('#');
               key += to_string(cnt[i]);
               //key += "#" + to_string(cnt[i]);
-              
            } 
 
-           mp[key].push_back(s);
+           // anagram 有相同的的 key, 以上不同 str 会在这里完成汇聚
+           key2anagram[key].push_back(word);
         }
 
-        for (auto& iter: mp)
-           res.push_back(move(iter.second));    
+        for (auto& [key, anagram_vec]: key2anagram)
+           res.push_back(move(anagram_vec));    
 
         return res;
     }
