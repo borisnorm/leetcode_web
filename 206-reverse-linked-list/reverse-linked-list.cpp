ae28828ad4d804a0097b4a92f424986cde1f6ea2
borisnorm <borisnorm@gmail.com>
2026-01-20 15:30:42 -0800

    Time: 0 ms (100.00%) | Memory: 13.3 MB (91.11%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -14,6 +14,30 @@ public:
         if (!head || !head->next)
           return head;
 
+        //  尾插法, pre 要初始化为 nullptr,  把旧节点从 list 拆下来,  放到新的 尾部
+
+        //  node_0 --> node_1 --> node_2 --> nullptr;
+        //  ----------------------------------
+        //  cur(node_0)-> pre              新链
+        //                [nullptr];
+        
+        // node_1 --> node_2 --> nullptr;  当前链
+        // cur = nxt;
+
+        // --------------------------------------
+        // node_1 -->nullptr
+        // pre       (previous pre)
+
+        //  cur-> pre[tail]->nullptr     搬运过程
+
+        // -------------------------
+        //                  [tail] 
+        //  cur(node_1) - > node_0 -> nullptr  新链
+        //  node_2 --> nullptr;                旧链                  
+        //  [nxt]
+
+        // 时间 O(n) n indicates the number of nodes
+        // 空间 O(1)
         ListNode* pre = nullptr;
         ListNode* cur = head;
         ListNode* nxt = nullptr;
