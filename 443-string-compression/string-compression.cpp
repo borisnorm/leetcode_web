5a2d87aba40b5c9f8ba4699978081e0c441904b8
borisnorm <borisnorm@gmail.com>
2026-01-21 12:24:10 -0800

    Time: 0 ms (100.00%) | Memory: 13.8 MB (70.84%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -2,6 +2,41 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int n = chars.size();
+
+        int read = 0;
+        int write = 0;
+
+        // Time Complexity:  O(n)
+        // Space Complexity: O(1)
+        while (read < n)
+        {
+            // go through the read part
+           int start = read;
+           char c = chars[start];
+           while (read < n && chars[read] == c)
+           {
+              read++;
+           }
+           int end = read;
+           int cnt = end - start; // [start, end);
+            
+           // write char
+           chars[write++] = c;
+
+           // write cnt
+           if (cnt > 1)
+           {
+              string cnt_str = to_string(cnt);
+              for (char digit : cnt_str)
+                chars[write++] = digit;
+           }
+        }
+
+        return write;
+    }
+/*
+    int compress(vector<char>& chars) {
+        int n = chars.size();
         
         int write = 0;
         int cnt = 1;
@@ -9,21 +44,26 @@ public:
         for (int i = 1; i <= n; i++) {
             // 处理边界 or 不连续
             if (i == n || chars[i] != chars[i - 1]) {
+
                 chars[write++] = chars[i - 1];  // 写字符
 
                 if (cnt > 1) {
                     string num = to_string(cnt);
-                    for (char c : num) {
+
+                    // cnt 的 num 的 str 也是按 character 去写的
+                    for (char c : num)
                         chars[write++] = c;    // 写数字
-                    }
                 }
 
                 cnt = 1;  // reset
-            } else {
+            } 
+            else 
+            {
                 cnt++;     // same char
             }
         }
 
         return write;
     }
+*/
 };
