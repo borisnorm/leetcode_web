9c878469c5a318606cd7d9a6ab0a24f788eea11d
borisnorm <borisnorm@gmail.com>
2026-01-20 17:40:57 -0800

    Time: 0 ms (100.00%) | Memory: 40.3 MB (58.17%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -10,13 +10,22 @@ public:
         vector<int> res(n, 1);
 
         for (int i = 1; i < n; i++)
-         res[i] = res[i-1] * nums[i-1];
+        {
+           // res[i] 是 i 左边所有数的乘积
+           // ans[i] = nums[0] * nums[1] * ... * nums[i-1]
+
+          // 那 res[i-1] 是 i - 1 这个位置 左边 的所有数的乘积, 
+          // 对于 i 来说就是 从 0 到 i-2的所有乘积
+           res[i] = res[i-1] * nums[i-1];
+        }
 
         int tmp = 1;
         for (int i = n - 1; i >= 0; i--)
         {
-            res[i] = res[i] * tmp;
-            tmp = nums[i] * tmp;
+          // 先更新 倒数的 i, 因为不能 乘 它自己
+          res[i] = res[i] * tmp;
+          // 跟新 temp for 下一个数
+          tmp = nums[i] * tmp;
         }
 
         return res;
