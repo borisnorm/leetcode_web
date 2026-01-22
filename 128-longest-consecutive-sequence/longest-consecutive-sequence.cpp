5ea4f55a6e75a69374c3edace5042a5ecaf1d7a6
borisnorm <borisnorm@gmail.com>
2026-01-22 08:22:37 -0800

    Time: 83 ms (54.28%) | Memory: 89 MB (23.54%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -11,12 +11,14 @@ public:
         int cur_num = 0;
         for (int num : nums_set)   // for (int num: nums_set)
         {
+           // 检查前一个数
            if (!nums_set.count(num-1))
            {
               // this num is the start point
               cur_len = 1;
               cur_num = num;
 
+              // 检查有一个数
               while (nums_set.count(cur_num+1))
               {
                 cur_len++;
