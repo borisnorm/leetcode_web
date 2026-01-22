ddb0a222347a92937f623fe94adb416a8745c0d4
borisnorm <borisnorm@gmail.com>
2026-01-22 00:25:04 -0800

    Time: 0 ms (100.00%) | Memory: 22.7 MB (18.42%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -6,17 +6,20 @@ public:
 
         int n = nums.size();
 
-        int slow = 0;
-
-        for (int fast = 1; fast < n; fast++)
+        // 下一个可以写入的位置
+        int write = 1;
+        
+        // 对于重复的 值, 只保留第一个, 所以 write[0] 被保留下来
+        // [0, , write-1] 表示已经去重的元素 集合
+        //  read 表示可能存在 的重复值
+        for (int read = 1; read < n; read++)
         {
-           if (nums[fast] != nums[slow])
+           if (nums[read] != nums[write-1])
            {
-              slow++;
-              nums[slow] = nums[fast];
+              nums[write++] = nums[read];
            }
         }
 
-        return slow+1;
+        return write;
     }
 };
