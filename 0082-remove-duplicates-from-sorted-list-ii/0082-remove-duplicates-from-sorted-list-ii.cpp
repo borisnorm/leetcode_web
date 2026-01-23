763919780e74e71ca8d201fa75bba4f7a863c74d
borisnorm <borisnorm@gmail.com>
2026-01-23 15:20:17 -0800

    0082-REMOVE-DUPLICATES-FROM-SORTED-LIST-II Stats: Time: 0 ms (100%), Space: 15.7 MB (70.24%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -11,55 +11,35 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode dummy;
-        ListNode *dummy_head=&dummy;
-        dummy_head->next=head;
-
         if (!head || !head->next)
           return head;
 
-        ListNode* left  = dummy_head;
-        ListNode* iter  = head;
-        ListNode* right = head->next;
+        ListNode dummy;
+        ListNode* dummy_head = &dummy;
+        dummy_head->next = head;
 
-        while(right)
+        ListNode* pre = dummy_head;
+        ListNode* cur = head;
+        ListNode* nxt = nullptr;
+
+        while (cur && cur->next)
         {
-          if (iter->val != right->val)
-          {
-             right = right->next;
-             iter  = iter->next;
-             left  = left->next;
-          }
-          else
-          {
-
-            // move to the most right
-            while (right && iter->val == right->val)
-              right = right->next;
-
-            // right possible nullptr
-            
-            //delete duplicate
-            while (iter != right)
-            {
-               // å æ iter ä¹å°±æ¯ leftmost é£ä¸ªèç¹
-               // è¿é left ä¸ç´æ²¡æ update è·æ°, å§ç»ä¿æåå¼
-               left->next = iter->next;
-               iter->next = nullptr;
-               //delete iter;
-
-               iter = left->next;
-            }
-            
-            // right move one more step
-            if (right)
-            {
-              iter = right; 
-              right = right->next;
-            }
-          }            
+           nxt = cur->next;
+           if (cur->val == nxt->val)
+           {
+              int dupVal = cur->val;
+              while (cur && cur->val == dupVal)
+                cur = cur->next;
+              
+              pre->next = cur;
+           }
+           else
+           {
+              pre = cur;
+              cur = cur->next;
+           }
         }
 
-        return dummy_head->next;
+        return dummy.next;
     }
 };
