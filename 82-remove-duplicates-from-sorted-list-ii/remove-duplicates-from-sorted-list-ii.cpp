34aac07c595249ecdaa1732a5377139d3cfb38b4
borisnorm <borisnorm@gmail.com>
2026-01-22 21:49:58 -0800

    Time: 0 ms (100.00%) | Memory: 15.8 MB (38.35%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        if (!head || !head->next)
+          return head;
+
+        ListNode dummy(INT_MAX);
+        ListNode* dummy_head = &dummy;
+        dummy_head->next = head;
+
+        ListNode* pre = dummy_head;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        int preVal = INT_MAX;
+       
+        bool isDuplicate = false;
+        while (fast)
+        {
+           if (fast->val != slow->val)
+           {
+              if (!isDuplicate)
+              {
+                pre = slow;
+                slow = slow->next;
+                fast = fast->next;
+              }
+              else
+              {
+                 //skip to the latest different fast
+                 pre->next = fast;
+               
+                 //slow as the new fast
+                 slow = fast;
+                 fast = fast->next;
+                 isDuplicate = false;
+              }
+           }
+           else
+           {
+              fast = fast->next;
+              isDuplicate = true;
+           }
+        }
+
+        if (isDuplicate)
+          pre->next = fast;
+        
+        return dummy.next;
+    }
+};
