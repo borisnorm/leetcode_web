37fa4780cb80a2045e19544bd722177808dbd820
borisnorm <borisnorm@gmail.com>
2026-01-23 22:48:44 -0800

    Time: 0 ms (100.00%) | Memory: 16.5 MB (68.79%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -8,6 +8,66 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || !head->next || k <= 1)
+          return head;
+        
+        int cnt = 0;
+        ListNode* prob = head;
+        while(prob && cnt < k)
+        {
+           cnt++;
+           prob = prob->next;
+        }
+
+        // k is too big that prob has became null
+        if (cnt < k)
+          return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* dummy_head = &dummy;
+
+        ListNode* pre = dummy_head;
+        ListNode* cur = head;
+        ListNode* nxt = nullptr;
+
+        while(true)
+        {
+           ListNode* kth = pre;
+           for (int i = 0; i < k && kth; i++)
+           {
+              kth = kth->next;
+           }
+
+           if (!kth)
+             break;
+        
+           
+           for (int i = 1; i < k; i++)
+           {
+              nxt = cur->next;
+              cur->next = nxt->next;
+              nxt->next = pre->next;
+
+              pre->next = nxt;
+           }
+
+           pre = cur;
+           cur = cur->next;
+
+        }
+
+        return dummy.next;
+        
+    }
+};
+
+ /*
+
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
@@ -15,13 +75,14 @@ public:
           return head;
         
         // 用来检测, 最后的节点能否 进行 k 个反转的, 如果不行就 返回现在的 head
-        ListNode* check = head;
-        for (int i = 0; i < k; i++)
+        ListNode* cur = head;
+        for (int i = 0; i < k && cur; i++)
         {
-          if (!check)
-            return head;
           check = check->next;   
         }
+
+        if (i < k)
+          return head;
 
         // 1->2->3->4->5
         // 2->1->nullptr,   3->4->5
@@ -48,4 +109,6 @@ public:
     }
 
 };
+
+*/
 
