0d960d8b5ba92f4c4d7ab8e9291de2755427dc20
borisnorm <borisnorm@gmail.com>
2026-01-23 17:29:42 -0800

    Time: 9 ms (49.28%) | Memory: 11.3 MB (83.73%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -11,6 +11,38 @@ public:
     ListNode *detectCycle(ListNode *head) {
         if (!head || !head->next)
           return nullptr;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+            {
+               ListNode* slow2 = head;
+               ListNode* fast2 = fast;
+               while (slow2 != fast2)
+               {
+                  slow2 = slow2->next;
+                  fast2 = fast2->next;
+               }
+
+               return slow2;
+            }
+        }
+
+        return nullptr;
+    }
+};
+ /*
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (!head || !head->next)
+          return nullptr;
 
         ListNode* slow = head;
         ListNode* fast = head;
@@ -39,3 +71,5 @@ public:
         return slow;
     }
 };
+
+*/
