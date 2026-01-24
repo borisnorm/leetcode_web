f9567fdd05863ae459e0cf96671a5461f4c72ad9
borisnorm <borisnorm@gmail.com>
2026-01-23 17:06:02 -0800

    Time: 0 ms (100.00%) | Memory: 11.2 MB (56.46%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -14,6 +14,23 @@ public:
         if (!head || !head->next)
           return head;
         
+        ListNode* cur = head;
+        ListNode* nxt = head->next;
+
+        cur->next = swapPairs(nxt->next);
+
+        nxt->next = cur;
+
+        return nxt;
+    }
+};
+ /*
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next)
+          return head;
+        
         ListNode  dummy(0);
         ListNode* dummy_head = &dummy;
         dummy_head->next = head;
@@ -37,6 +54,9 @@ public:
         return dummy.next;
     }
 };
+
+*/
+
  /*
 class Solution {
 public:
@@ -65,6 +85,58 @@ public:
         }
 
         return dummy.next;
+    }
+};
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ /*
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        // int k = 2;
+      if (!head || !head->next)
+        return head;
+
+      ListNode dummy(0);
+      ListNode* dummy_head = &dummy;
+      dummy_head->next = head;
+
+      ListNode* pre = dummy_head;
+      ListNode* cur = head;
+      ListNode* nxt = nullptr;
+
+      for(int i = 1; i < 2; i++)
+      {
+         cur = cur->next;
+      }
+      
+      ListNode* nxtHead = cur->next;
+      cur->next = nullptr;
+      
+      cur = head;
+      while(cur->next)4                                                                      
+      {
+         nxt = cur->next;
+         cur->next = nxt->next;
+         nxt->next = pre->next;
+
+         pre->next = nxt;
+      }
+
+      cur->next = swapPairs(nxtHead);
+
+      return dummy.next;
     }
 };
 
