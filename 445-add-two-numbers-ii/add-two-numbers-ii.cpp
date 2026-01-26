ba24f6abcc29fd2601dcb4459b7ce511ac146e04
borisnorm <borisnorm@gmail.com>
2026-01-25 19:56:17 -0800

    Time: 7 ms (16.80%) | Memory: 79.2 MB (36.94%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -8,6 +8,69 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            if (!l1 && !l2)
+              return nullptr;
+            
+            stack<int> l1_st;
+            stack<int> l2_st;
+
+            ListNode* l1_cur = l1;
+            ListNode* l2_cur = l2;
+
+            while (l1_cur)
+            {
+               l1_st.push(l1_cur->val);
+               l1_cur = l1_cur->next;
+            }
+
+            while(l2_cur)
+            {
+               l2_st.push(l2_cur->val);
+               l2_cur = l2_cur->next;
+            }
+
+            int carry = 0;
+            int sum = 0;
+
+            ListNode* pre = nullptr;
+
+            while(!l1_st.empty() || !l2_st.empty() || carry)
+            {
+               int l1_val = 0;
+               int l2_val = 0;
+
+               if (!l1_st.empty())
+               {
+                  l1_val = l1_st.top();
+                  l1_st.pop();
+               }
+
+               if (!l2_st.empty())
+               {
+                  l2_val = l2_st.top();
+                  l2_st.pop();
+               }
+               
+               sum = l1_val + l2_val + carry;
+
+               carry = sum/10;
+               sum = sum%10;
+            
+               ListNode* node = new ListNode(sum);
+               node->next = pre;
+               pre = node;
+               // h - 1
+               // h - 2 - 1(pre)
+            }
+
+            return pre;
+    }
+};
+/*
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -74,3 +137,4 @@ public:
 
     }
 };
+*/
