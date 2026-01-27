1f4bb8b2a45d6701ca291ec983b3dc5943c89864
borisnorm <borisnorm@gmail.com>
2026-01-27 00:16:38 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (55.16%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        if (n < 0)
+          return 0;
+        else if (n == 0 || n == 1 )
+          return 1;
+        else if (n == 2)
+          return 2;
+        
+        int prev1 = 1;  //dp[i-1]  1 way 
+        int prev2 = 1;  //dp[i-2]  two ways on 2 steps diff
+        int prev = 0;
+
+        for (int i = 2; i <=n; i++)
+        {
+           prev = prev1 + prev2;
+           prev2 = prev1;
+           prev1 = prev;
+        }
+
+        return prev;
+    }
+    
+  /*
+    int climbStairs(int n) {
     
       if (n == 0 || n == 1)
          return 1;
@@ -17,4 +40,7 @@ public:
     
        return dp[n];
     }
+    */
 };
+
+
