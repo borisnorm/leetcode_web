b581158c9bbf3a0f965ad80c827c3c492c9835f5
borisnorm <borisnorm@gmail.com>
2026-01-26 20:46:34 -0800

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -8,13 +8,11 @@ public:
          return 2;
        
        // answer on ways;
-
        vector<int> dp(n+1, 0);
        dp[0] = 1;
        dp[1] = 1;
-       dp[2] = 2;
 
-       for (int i = 3; i <= n; i++)
+       for (int i = 2; i <= n; i++)
          dp[i] = dp[i-2] + dp[i-1];
     
        return dp[n];
