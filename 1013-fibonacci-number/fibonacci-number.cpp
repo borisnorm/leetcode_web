f10c5c0dc468c948926fb2734776bec6c5316f54
borisnorm <borisnorm@gmail.com>
2026-01-27 13:12:35 -0800

    Time: 2 ms (77.91%) | Memory: 7.8 MB (36.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,3 +1,27 @@
+class Solution{
+public:
+  int fib(int n)
+  {
+    if (n == 0 || n == 1)
+      return n;
+    else if (n < 0)
+      return -1;
+
+     int fn1 = 1; // dp[n-1]
+     int fn2 = 0; // dp[n-2]
+
+     int fn = 0;
+     for (int i = 2; i <= n; i++)
+     {
+        fn = fn1 + fn2;
+        fn2 = fn1;
+        fn1 = fn;
+     }   
+
+     return fn;
+  }
+};
+/*
 class Solution {
 public:
     int fib(int n) {
@@ -6,16 +30,19 @@ public:
 
         vector<int> dp(n+1, 0);
         dp[0] = 0;
-        dp[1] = 1;
+        dp[1] = 1; // n = 0 的时候 dp[1]的访问会失效的 ,所以必须提前判断 n 的大小 
         for (int i = 2; i <= n;  i++)
           dp[i] = dp[i-1] + dp[i-2];
         
         return dp[n];
     }
+*/
+
 /*
     int fib(int n) {
 
         int step = 2;
+
         int fn  = 0;
         int fn0 = 0;
         int fn1 = 1;
@@ -28,6 +55,14 @@ public:
  
         fn1 = fn1;
         fn2 = fn0;
+
+        for (int i = 2; i <= n; i++)
+        {
+           fn = fn0 + fn1;
+           fn1 = fn0;
+           fn0 = fn;
+        }
+
         while (step <= n)
         {
             //fn1 = fn1;
@@ -38,11 +73,12 @@ public:
             fn1 = fn;
 
             step++;
-                
         }
 
         return fn;
     } 
 
-    */
+    
 };
+
+*/
