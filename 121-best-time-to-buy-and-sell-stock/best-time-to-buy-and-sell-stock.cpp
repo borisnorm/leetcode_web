6c49e7f933fd0487ce037fd9d47e0803fac0a8ed
borisnorm <borisnorm@gmail.com>
2026-01-29 10:24:18 -0800

    Time: 151 ms (6.61%) | Memory: 158.2 MB (5.10%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,55 @@
 
 class Solution {
 public:
+   
+    int maxProfit(vector<int>& prices) {
+          int n = prices.size();
+
+          if (n == 0) return 0;
+          vector<vector<int>>  dp(n, vector<int>(2, 0));
+          //dp[i][j] the max amount money on ith day(0-based) for either hold or not hold stock;
+          dp[0][0] = 0;
+          dp[0][1] = -prices[0];  // hold stock on first day 
+
+          for (int i = 1; i < n; i++)
+          {
+             // hold stock
+             dp[i][1] = max(dp[i-1][1], -prices[i]);
+
+
+             // not hold any stock
+             dp[i][0] = max(dp[i-1][0],  dp[i-1][1]+prices[i]);
+          }
+
+          return dp[n-1][0];
+    }
+};
+
+/*
+class Solution {
+public:
+     //Greedy（贪心）解法
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;   // 到目前为止的最低买入价
+        int maxProfit = 0;        // 最大利润
+
+        for (int price : prices) {
+            // 更新最低买入价
+            minPrice = min(minPrice, price);
+
+            // 更新最大利润              //如果今天卖出，能获得的利润
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
+*/
+
+/*
+//同样是贪心
+class Solution {
+public:
     int maxProfit(vector<int>& prices) {
 
 
@@ -21,57 +70,9 @@ public:
        return maxProfit;
 
     };
-   
-};
-
-/*
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-
-       // Time Limit Exceeded
-
-       int n = prices.size();
-       int maxProfit = 0;
-       int minPrice = INT_MAX;
-       for (int i = 0; i < n; i++)
-       {
-          minPrice = min(minPrice, prices[i]);
-          for (int j = i + 1; j < n; j++)
-          {
-              if (prices[j] > minPrice)
-                maxProfit = max(maxProfit, (prices[j]-minPrice));
-          }
-       }
- 
-       return maxProfit;
-    }; 
 };
 */
 
-/*
-class Solution {
-public:
-     //Greedy（贪心）解法
-    int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;   // 到目前为止的最低买入价
-        int maxProfit = 0;        // 最大利润
-
-        for (int price : prices) {
-            // 更新最低买入价
-            minPrice = min(minPrice, price);
-
-            // 如果今天卖出，能获得的利润
-            int profit = price - minPrice;
-
-            // 更新最大利润
-            maxProfit = max(maxProfit, profit);
-        }
-
-        return maxProfit;
-    }
-};
-*/
 
 
 /*  
@@ -93,5 +94,29 @@ public:
 
        return max_profit;
     }
+};
+*/
+
+/*
+// Time Limit Exceeded
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+       int n = prices.size();
+       int maxProfit = 0;
+       int minPrice = INT_MAX;
+       for (int i = 0; i < n; i++)
+       {
+          minPrice = min(minPrice, prices[i]);
+          for (int j = i + 1; j < n; j++)
+          {
+              if (prices[j] > minPrice)
+                maxProfit = max(maxProfit, (prices[j]-minPrice));
+          }
+       }
+ 
+       return maxProfit;
+    }; 
 };
 */
