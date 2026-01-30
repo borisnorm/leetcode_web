deb6d0781ecd8b4c6e66573898a57aeafa7f6e0f
borisnorm <borisnorm@gmail.com>
2026-01-30 14:03:32 -0800

    Time: 0 ms (100.00%) | Memory: 9.5 MB (75.12%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -1,18 +1,81 @@
 class Solution {
 public:
+
+   string reverseStr(string s, int k) {
+      
+      int n = s.size();
+      if (n == 0)
+        return s;
+      
+      for (int i = 0; i < n; i+= 2*k)
+      {
+         int l = i;
+
+        //[l, r]
+         //正常应该是 i+k 的,但题目要求 fewer k characters left, reverse all of them;
+         int r = min(i + k-1, n-1);
+        
+         //reverse(s.begin() + l, s.begin()+r);
+         reverseSubstr(s, l, r);
+         
+      }
+
+      return s;
+}
+
+  void reverseSubstr(string& s, int l, int r)
+  {
+      if (l > r)
+        return;
+      //[l, r]
+      while(l < r)
+      {
+        swap(s[l], s[r]);
+        l++;
+        r--;
+      }
+  }
+
+/*
+class Solution {
+public:
+
+   string reverseStr(string s, int k) {
+      
+      int n = s.size();
+      if (n == 0)
+        return s;
+      
+      for (int i = 0; i < n; i+= 2*k)
+      {
+         int l = i;
+
+         //正常应该是 i+k 的,但题目要求 fewer k characters left, reverse all of them;
+         //[l, r)
+         int r = min(i + k, n);
+        
+         reverse(s.begin() + l, s.begin()+r);         
+      }
+
+      return s;
+}
+*/
+
+/*
+    正确
     string reverseStr(string s, int k) {
         
         int n = s.size();
         if (n == 0 || k == 0)
           return s;
 
-        /*
-        if (n < k)
-        {
-          reverse(s.begin(), s.end());
-          return s;
-        }
-        */
+    
+        //if (n < k)
+        //{
+        //  reverse(s.begin(), s.end());
+        //  return s;
+       // }
+    
 
         bool isReversed = false;
         for (int i = 0; i < n; i+=k)
@@ -37,4 +100,6 @@ public:
 
         return s;
     }
+    
+    */
 };
