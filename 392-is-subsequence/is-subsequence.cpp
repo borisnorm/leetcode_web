62bd83ee88d778c6b854e7058c710d0a6e187eac
borisnorm <borisnorm@gmail.com>
2026-01-30 11:26:49 -0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.99%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -19,3 +19,32 @@ public:
         return (i == m) ? true : false;
     }
 };
+
+
+/*
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+
+        // base case: empty s
+        for (int j = 0; j <= n; j++)
+            dp[0][j] = true;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == t[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = dp[i][j - 1];
+            }
+        }
+
+        return dp[m][n];
+    }
+};
+
+*/
