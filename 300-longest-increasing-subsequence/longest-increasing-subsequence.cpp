c54833f899def993f083afb15fdb4c4014817ba4
borisnorm <borisnorm@gmail.com>
2026-01-29 19:01:22 -0800

    Time: 75 ms (50.72%) | Memory: 14.2 MB (72.77%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,20 +3,26 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         // 定义：dp[i] 表示以 nums[i] 这个数结尾的最长递增子序列的长度
 
-        vector<int> dp(nums.size(), 1);
-        for (int i = 0; i < nums.size(); i++)
+        int n = nums.size();
+        // 以nums[i]结尾的数 包括 nums[i]它自己, 所以长度至少都是1
+        vector<int> dp(n, 1);
+
+        for (int i = 1; i < n; i++)
         {
           for (int j = 0; j < i; j++)
           {
+            //dp[i]是 目前已知的，以 nums[i] 结尾的最长递增子序列长度
+            //dp[j]+1 是 选择把 nums[i] 接在 nums[j] 后面
             if (nums[j] < nums[i])
               dp[i] = max(dp[i], dp[j]+1);
           }
         }
         
-        int res = 0;
+        int maxLen = 0;
         for (int i = 0; i < dp.size(); i++)
-           res = max(dp[i], res);
-        return res;
+           maxLen = max(maxLen, dp[i]);
+
+        return maxLen;
 
     }
 };
