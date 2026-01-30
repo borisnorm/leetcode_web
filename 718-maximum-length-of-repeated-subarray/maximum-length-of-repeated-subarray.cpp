53f492e4a19dfaabb9492bc9c26392fa7561aef3
borisnorm <borisnorm@gmail.com>
2026-01-30 11:54:12 -0800

    Time: 79 ms (70.42%) | Memory: 89.6 MB (10.11%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        // dp[i][j] = 以nums1[i-1]和nums2[j-1]结尾的最长公共子数组长度
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        int maxLen = 0; // 记录全局最大长度
+        
+        // 遍历所有可能的结束位置
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // 如果当前元素相等
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    // 在前一个位置的基础上+1
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    // 更新最大值
+                    maxLen = max(maxLen, dp[i][j]);
+                }
+                // 不相等时dp[i][j]保持为0（已初始化）
+            }
+        }
+        
+        return maxLen;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
         
         int m = nums1.size();
         int n = nums2.size();
@@ -37,3 +65,5 @@ public:
         return maxLen;
     }
 };
+
+*/
