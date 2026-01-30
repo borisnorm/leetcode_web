66988ef71bfdc7678dad2fbcf5860f903abf5ea7
borisnorm <borisnorm@gmail.com>
2026-01-30 01:53:32 -0800

    Time: 31 ms (52.29%) | Memory: 44.4 MB (20.50%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -6,7 +6,9 @@ public:
         int n = t.size();
 
         vector<vector<unsigned long long>> dp(m+1, vector<unsigned long long>(n+1, 0));
-
+         
+         // dp[i][j]   以s[i-1]结尾的 子序列 中 等于 t 的[j-1]的个数
+        
         for (int i = 0; i <= m; i++)
           dp[i][0] = 1;
 
@@ -14,9 +16,12 @@ public:
         {
            for (int j = 1; j <= n; j++)
            {
-              dp[i][j] = dp[i-1][j];
+              // dp[i-1][j] 表示删除 s 的第 i-1个字符后 剩下的字符与 t 进行比较
               if (s[i-1] == t[j-1])
-                dp[i][j] += dp[i-1][j-1]; 
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+              else
+                dp[i][j] = dp[i-1][j];
+                
            }
         } 
 
