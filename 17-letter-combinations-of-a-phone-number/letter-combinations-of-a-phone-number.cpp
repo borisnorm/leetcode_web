723ab96f4099bfbfac531442e82737c6d95b1a18
borisnorm <borisnorm@gmail.com>
2026-02-01 19:33:25 -0800

    Time: 0 ms (100.00%) | Memory: 9.3 MB (57.24%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,7 +1,44 @@
+
+class Solution {
+public:
+    vector<string> digit2letter = {
+            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        string letter;
+        backtrack(digits, 0, letter, res);
+        return res;
+    }
+    
+    void backtrack(string digits, int start, string& letter, vector<string>& res)
+    {
+        if (letter.size() == digits.size())
+        {
+            res.push_back(letter);
+            return;
+        }
+
+        int digit_idx = digits[start] - '0';
+        string letter_comb = digit2letter[digit_idx];
+
+        for (int i = 0; i < letter_comb.size(); i++)
+        {
+            letter.push_back(letter_comb[i]);
+            backtrack(digits, start+1, letter, res);
+            letter.pop_back();
+        }
+
+    }
+
+};
+
+/*
+
 class Solution {
 public:
 
-    unordered_map<char, string> mp={
+    unordered_map<char, string> digit2letter ={
           {'2', "abc"}, {'3',"def"}, {'4',"ghi"}, {'5', "jkl"},
           {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
        };
@@ -25,7 +62,7 @@ public:
         }
 
         char digit = digits[start];
-        for (char c : mp[digit])
+        for (char c : digit2letter[digit])
         {
             track.push_back(c);
             backtrack(digits, start+1);
@@ -33,3 +70,5 @@ public:
         }
     }
 };
+
+*/
