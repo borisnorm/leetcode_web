44cc7d1ced9b99f7e2ed1ea71014bd4ccb36a72c
borisnorm <borisnorm@gmail.com>
2026-02-02 07:36:38 -0800

    Time: 0 ms (100.00%) | Memory: 52.2 MB (91.27%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,3 +1,26 @@
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+         if (nums.empty())
+           return false;
+
+         int farthest = 0;
+         int n = nums.size();
+         for (int i = 0; i < n; i++)
+         {
+            // 不可达
+            // i 是当前值, farthest 是上一次的计算预估值
+            if (i > farthest)
+              return false;
+            farthest = max(farthest, i + nums[i]);
+
+         }
+
+         return true;
+    }
+};
+/*
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
@@ -17,3 +40,4 @@ public:
          return true;
     }
 };
+*/
