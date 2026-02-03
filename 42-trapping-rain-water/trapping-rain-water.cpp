87fe593c34845bda1323ab3ede155af949751b97
borisnorm <borisnorm@gmail.com>
2026-02-02 22:17:29 -0800

    Time: 0 ms (100.00%) | Memory: 26 MB (58.65%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -6,28 +6,30 @@ public:
          if (n <= 2)
            return 0;
 
-         int left = 0;
-         int right = n-1;
+         int l = 0;
+         int r = n-1;
 
          int l_max = 0;
          int r_max = 0;
 
          int ans = 0;
 
-         while (left < right)
+         //l == r 时，该点已经无法形成左右围栏，不可能存水，因此循环条件用 l < r。
+         while (l < r)
          {
-            l_max = max(l_max, height[left]);
-            r_max = max(r_max, height[right]);
+            l_max = max(l_max, height[l]);
+            r_max = max(r_max, height[r]);
 
-            if (l_max <= r_max)
+            
+            if (l_max < r_max)
             {
-               ans += l_max - height[left];  
-               left++;
+               ans += l_max - height[l];  
+               l++;
             }
             else
             {
-               ans += r_max - height[right];
-               right--;
+               ans += r_max - height[r];
+               r--;
             }
          }
 
