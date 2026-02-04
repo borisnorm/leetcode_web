21548f5de9015bb4df3a5b5e8886e110993911dd
borisnorm <borisnorm@gmail.com>
2026-02-04 13:25:55 -0800

    Time: 0 ms (100.00%) | Memory: 23.6 MB (98.05%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        if (nums.empty())
+          return;
+        
+        int n = nums.size();
+
+        int slow = 0;
+        for (int fast = 0; fast < n; fast++)
+        {
+            if (nums[fast] != 0)
+            {
+                swap(nums[slow], nums[fast]);
+                slow++;
+            }
+        }
+
+    }
+};
+
+/*
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int slow = 0;
         int fast = 0;
@@ -17,3 +39,4 @@ public:
         }
     }
 };
+*/
