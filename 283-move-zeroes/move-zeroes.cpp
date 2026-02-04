a3ec9cfe186ea42ead8ed78f4b5b9dda5422b7c9
borisnorm <borisnorm@gmail.com>
2026-02-04 15:17:58 -0800

    Time: 0 ms (100.00%) | Memory: 23.9 MB (54.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,3 +1,27 @@
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        if (nums.empty())
+          return;
+        
+        int n = nums.size();
+
+        int slow = 0;
+        for (int fast = 0; fast < n; fast++)
+        {
+            if (nums[fast] != 0)
+            {
+                if (slow != fast)
+                  swap(nums[slow], nums[fast]);
+                slow++;
+            }
+        }
+    }
+};
+
+/*
+// slow 覆写 然后 把 slow 后面的全部填充为0
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -23,6 +47,7 @@ public:
 
     }
 };
+*/
 
 /*
 class Solution {
