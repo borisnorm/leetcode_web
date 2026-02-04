1d6290899d49d1c755ef2e8bf5da26acf5d7d013
borisnorm <borisnorm@gmail.com>
2026-02-04 01:27:19 -0800

    Time: 0 ms (100.00%) | Memory: 13.4 MB (43.67%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -8,8 +8,13 @@ public:
         int n  = matrix[0].size();
         if (n <=0)
            return false;
-        int left = 0, right = m*n -1;
 
+        int left = 0;
+        int right = m*n -1;
+
+        // 把 2D 的 matrix flatten 成 1D 的数组
+        // 关键是 获取 1D 转 2D 的 val
+        // mid/n 表示行  mid%n 表示列
         while (left <= right)
         {
             int mid = left + (right - left)/2;
