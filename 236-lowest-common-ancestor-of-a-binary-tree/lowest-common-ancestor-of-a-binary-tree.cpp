9ed8c85aeb4c764322e4dd293955cb8cb4797ae7
borisnorm <borisnorm@gmail.com>
2026-02-05 11:36:47 -0800

    Time: 14 ms (36.02%) | Memory: 17.1 MB (97.18%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,16 +10,21 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q)
+        if (!root || !p || !q)
+          return nullptr;
+        
+        //“我已经找到了 p（或 q），请祖先节点帮我判断，这是不是 LCA。”
+        if (root == p || root == q)
           return root;
         
+        //后根向上传递 p与 q 存在的信息 
         TreeNode* left  = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
         
-        if (left && right)
+        // p and q are split here
+        if (left && right) 
           return root;
         
         return left ? left : right;
-
     }
 };
