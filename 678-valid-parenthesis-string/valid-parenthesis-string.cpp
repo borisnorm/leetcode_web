b68cf87723855383b5de56c0169371b0e57868f6
borisnorm <borisnorm@gmail.com>
2026-02-04 22:07:34 -0800

    Time: 0 ms (100.00%) | Memory: 8.1 MB (73.76%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int lo = 0;
-        int hi = 0;
+        if (s.empty())
+          return 0;
+
+        int minLeft = 0;  // minimum possible '(' count
+        int maxLeft = 0;  // maximum possible '(' count
 
         for (char c: s)
         {
            if (c == '(')
            {
-              lo++;
-              hi++;
+              minLeft++;
+              maxLeft++;
            }
            else if (c == ')')
            {
-              lo--;
-              hi--;
+              minLeft--;
+              maxLeft--;
            }
            else if (c =='*')
            {
-              lo--;
-              hi++;
+              minLeft--;   // treat '*' as ')'
+              maxLeft++;   // treat '*' as '('
            }
 
-           if (hi < 0)
+           if (maxLeft < 0)   // treat '*' as '('
              return false;
 
-           lo = max(lo, 0);
+           if (minLeft < 0)
+             minLeft = 0;   // '(' count can't be negative
+
+           // 等效
+           //lo = max(lo, 0);
         }
-       return lo == 0;
+       return minLeft == 0;
 
     }
-
-  
 };
