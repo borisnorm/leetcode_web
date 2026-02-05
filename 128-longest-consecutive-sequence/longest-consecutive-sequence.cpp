f145daffd4577c1c0d70261fc3ae33881fcaddab
borisnorm <borisnorm@gmail.com>
2026-02-04 18:03:11 -0800

    Time: 75 ms (74.96%) | Memory: 88.8 MB (63.50%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -11,21 +11,23 @@ public:
         int cur_num = 0;
         for (int num : nums_set)   // for (int num: nums_set)
         {
-           // 检查前一个数
-           if (!nums_set.count(num-1))
+           // 检查前一个数 确定当前数是不是起点
+           if (nums_set.count(num-1))
+              continue;
+        
+           // this num is the start point
+           // 初始化起点 数据
+           cur_len = 1;
+           cur_num = num;
+
+           // 检查下一个连续的数是否在 num_set 里面
+           while (nums_set.count(cur_num+1))
            {
-              // this num is the start point
-              cur_len = 1;
-              cur_num = num;
-
-              // 检查有一个数
-              while (nums_set.count(cur_num+1))
-              {
-                cur_len++;
-                cur_num = cur_num + 1;
-              }
+              cur_len++;
+              // 更新 cur_num
+              cur_num = cur_num + 1;
            }
-
+           
            ans = max (ans, cur_len);
         }
 
