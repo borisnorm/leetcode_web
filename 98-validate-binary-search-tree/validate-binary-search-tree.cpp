6b7fb94ee1cd85dd6e0f2351e6f35ae904d35567
borisnorm <borisnorm@gmail.com>
2026-02-04 17:17:54 -0800

    Time: 0 ms (100.00%) | Memory: 21.9 MB (73.86%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -9,6 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (!root)
+          return true;
+        
+        return _isValidBST(root, nullptr, nullptr);
+        
+    }
+
+    bool _isValidBST(TreeNode* root, TreeNode* left, TreeNode* right)
+    {
+        if (!root)
+          return true;
+        
+        if (left)
+          if (root->val <= left->val)
+            return false;
+        
+        if (right)
+          if (root->val >= right->val)
+            return false;
+           
+        bool isLeftBST = _isValidBST(root->left, left, root);
+        bool isRightBST = _isValidBST(root->right, root, right); 
+
+        return isLeftBST && isRightBST;
+       
+    }
+ };
+
+
+/*
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
@@ -34,3 +68,5 @@ public:
         return _isValidBST(node->left, low, node) && _isValidBST(node->right, node, high);
     }
 };
+
+*/
