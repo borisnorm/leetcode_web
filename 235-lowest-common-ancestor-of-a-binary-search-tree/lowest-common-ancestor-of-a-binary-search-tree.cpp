b424a17e857ebd42924e8195e3b3e404272c3f3f
borisnorm <borisnorm@gmail.com>
2026-02-05 09:58:18 -0800

    Time: 27 ms (22.20%) | Memory: 23.6 MB (33.78%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,8 +7,86 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root)
+          return nullptr;
+        
+        if (!p || !q)
+          return root;
+        
+        TreeNode* cur = root;
+        while (cur)
+        {
+            int val = cur->val;
+            if (p->val < val && q->val < val)
+              cur = cur->left;
+            else if (p->val > val && q->val > val)
+              cur = cur->right;
+            else
+              return cur;
+        }
 
+        //cur is already nulll at this time
+        return nullptr;
+        
+    }
+};
+/*
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+         if (!root)
+           return nullptr;
+         if (!p && !q)
+           return nullptr;
+        
+         TreeNode* cur = root;
+         TreeNode* pre = nullptr;
+         while (cur)
+         {
+             if (p && q)
+             {
+                int val = cur->val;
+                if (p->val < val && q->val < val)
+                  cur = cur->left;
+                else if (p->val > val && q->val > val)
+                  cur = cur->right;
+                else
+                // p->val > val && q->val < val
+                // p->val < val && q->val > val
+                // p->val > val && q->val < val
+                // p->val < val && q->val > val
+                  return cur;
+             }
+             else if (p || q)
+             {
+                TreeNode* target = p ? p : q;
+                int val = cur->val;
+                if (target->val < val)
+                {
+                  pre = cur;
+                  cur = cur->left;
+                }
+                else if (target->val > val)
+                {
+                  pre = cur;
+                  cur = cur->right;
+                }
+                else 
+                {
+                   return pre;
+                }
+             }
+         }
 
+         return nullptr;
+         
+    }
+};
+*/
+/*
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -29,6 +107,7 @@ public:
         return root;
     }
 };
+*/
 /*
 class Solution {
 public:
