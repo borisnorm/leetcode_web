98a6ea152783b68c134f506b2f003c1848d06a99
borisnorm <borisnorm@gmail.com>
2026-02-05 15:17:53 -0800

    Time: 0 ms (100.00%) | Memory: 34.4 MB (39.97%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -14,23 +14,41 @@ public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if (!root)
           return nullptr;
-        
-        if (root->val < key)
-          root->right = deleteNode(root->right, key);
-        else if (root->val > key)
+
+        if (key < root->val)
           root->left  = deleteNode(root->left, key);
+        else if (key > root->val)
+          root->right = deleteNode(root->right, key);
         else
         {
-           if (!root->left)
-             return root->right;
-           else if (!root->right)
-             return root->left;
-           else
+           if (!root->left && !root->right)
            {
-              TreeNode* minNode = getmin(root->right);
-              root->val = minNode->val;
-              root->right = deleteNode(root->right, minNode->val);
+              delete root;
+              return nullptr;
            }
+
+           if (!root->left)
+           {
+             // 返回后 让 父节点指向 avail 的子节点, 绕过当前节点 达到删除目的
+             TreeNode* right = root->right;
+             delete root;
+             return right;
+           }
+           
+           if (!root->right)
+           {
+             TreeNode* left = root->left;
+             delete root;
+             return left;
+           }
+        
+           // 找右子树最小节点
+           TreeNode* minNode = getmin(root->right);
+           
+           // 用最小节点值替换当前节点
+           root->val = minNode->val;
+           // 删除右子树中该最小节点
+           root->right = deleteNode(root->right, minNode->val);
         }
 
        return root;
