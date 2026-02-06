d7ee53d761eb34be59f99a10655fec45f58640e0
borisnorm <borisnorm@gmail.com>
2026-02-06 12:37:15 -0800

    Time: 0 ms (100.00%) | Memory: 9.1 MB (54.50%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,10 +1,63 @@
 class Solution {
 public:
     int myAtoi(string s) {
+
+        int n = s.size();
+        int sign = 1;
+        
+        int i = 0;
+        while (i < n && s[i] == ' ')
+          i++;
+        if (i == n)
+          return 0;
+        
+        if (i < n && (s[i]=='+' || s[i]=='-'))
+        {
+          sign = (s[i]=='-') ? -1 : 1;
+          i++;
+        }
+        if (i == n)
+          return 0;
+
+        long long res = 0;
+        while (i < n && '0' <= s[i] && s[i] <='9')
+        {
+           int digit = s[i] - '0';
+           res = res * 10 + digit;
+
+            //if (res * sign > INT_MAX) return INT_MAX;
+           // if (res * sign < INT_MIN) return INT_MIN;
+
+        
+                        // ✅ 提前检查：在乘以sign之前就检查边界
+            if (sign == 1 && res > INT_MAX)
+                return INT_MAX;
+            if (sign == -1 && res > (long long)INT_MAX + 1)  // INT_MIN的绝对值
+                return INT_MIN;
+         
+          /*
+           if (sign > 0)
+             if (res > INT_MAX)
+               return INT_MAX;
+            else
+              if (res > (long long)INT_MAX + 1)
+                return INT_MIN;
+            */
+            i++;
+        }
+
+        return (int) (res * sign) ;
+    }
+};
+
+/*
+class Solution {
+public:
+    int myAtoi(string s) {
         int n = s.size();
 
         int sign = 1;
-        int res = 0;
+
 
         int i = 0;
         while (i < n && s[i] == ' ')
@@ -24,6 +77,7 @@ public:
         if (i==n)
           return 0;
 
+        int res = 0;
         while (i < n && '0' <= s[i] && s[i] <= '9')
         {
             int digit = s[i] - '0';
@@ -33,18 +87,18 @@ public:
                 return sign == 1 ? INT_MAX : INT_MIN;
             }
 
-            /*
-            if (sign > 0)
-            {
-              if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > 7))
-                return  INT_MAX;
-            }
-            else
-            {
-              if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > 8)) 
-                return  INT_MIN;
-            }
-            */
+            
+            //if (sign > 0)
+            //{
+            //  if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > 7))
+            //    return  INT_MAX;
+           // }
+            //else
+           // {
+            //  if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > 8)) 
+            //    return  INT_MIN;
+           // }
+            
 
             res = res*10 + digit;
             
@@ -55,3 +109,4 @@ public:
         
     }
 };
+*/
