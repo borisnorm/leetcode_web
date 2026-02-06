9927363efdae0a389ee1f2a60be8afec27773cf7
borisnorm <borisnorm@gmail.com>
2026-02-06 08:43:24 -0800

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.13%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,6 +3,39 @@ public:
     void nextPermutation(vector<int>& nums) {
         if (nums.empty())
           return;
+        
+        int n = nums.size();
+
+        // go through from end to front, find first i < i+1, i as prefix, i+1 to end as suffix, suffix is decreasing in order
+           // reverse the exising max permutation if not found
+        int i = n-2;
+        while (i >= 0 && nums[i] >= nums[i+1])
+          i--;
+        if (i < 0)
+        {
+           reverse(nums.begin(), nums.end());
+           return;
+        }
+    
+        // go throught from end to front, find first j > i
+        // then swap them, to gurantee the min prefix and max suffix
+        // as suffix is been in decrasing order, which is the max number in ascending order
+        int j = n - 1;
+        while (j >= 0 && nums[j] <= nums[i])
+          j--;
+        swap(nums[i], nums[j]);
+        
+        //reverse the suffix to the end 
+        reverse(nums.begin()+i+1, nums.end());
+    }
+};
+
+/*
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (nums.empty())
+          return;
 
         int n = nums.size();
         // 1. find the fisrt nums[p] < nums[p+1]
@@ -25,6 +58,9 @@ public:
         swap(nums[i], nums[j]);
 
         // 4. reverse after p postion suffix
+        // suffix 后面已经是可能的最大值了
         reverse(nums.begin()+i+1, nums.end());
     }
 };
+
+*/
