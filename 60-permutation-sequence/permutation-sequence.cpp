8800577b4124aa55677458b68075dd4099b90e7e
borisnorm <borisnorm@gmail.com>
2026-02-06 16:19:11 -0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (36.58%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,15 +1,53 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        
+         if (k == 0)
+           return "1";
+
+         vector<int> nums(n, 0);
+         for (int i = 0; i < n; i++)
+           nums[i] = i + 1;
+
+         /*
+         for (int i = 1; i <= n; i++)
+           nums[i-1] = i;
+         */
+        
+         vector<int> fact(n, 1);
+         for (int i = 1; i < n; i++)
+           fact[i] = fact[i-1] * i;
+        
+         k--;
+         string res = "";
+         for (int i = n; i >= 1; i--)
+         {
+            int idx = k / fact[i-1];
+            int num = nums[idx];
+            res += to_string(num);
+            nums.erase(nums.begin()+idx);
+
+            k = k % fact[i-1];
+         }
+
+         return res;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    string getPermutation(int n, int k) {
         vector<int> nums;
         for (int i = 1; i <= n; i++)
           nums.push_back(i);
         
         vector<int> fact(n, 1);
-
         for (int i = 1; i < n; i++)
           fact[i] = fact[i-1] * i;
         
+        // 转换为0-based索引
         k--;
 
         string res = "";
@@ -27,3 +65,5 @@ public:
 
     }
 };
+
+*/
