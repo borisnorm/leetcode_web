358138b5bb31e5895618e5685d0b3bd7f140350c
borisnorm <borisnorm@gmail.com>
2026-02-07 15:21:47 -0800

    Time: 840 ms (19.22%) | Memory: 11.7 MB (31.55%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    bool found = false;
+
     bool exist(vector<vector<char>>& board, string word) {
         
-        if (board.empty())
-          return false;
-        if (board[0].empty())
+        if (board.empty() || board[0].empty())
           return false;
 
         int m = board.size();
@@ -15,46 +13,49 @@ public:
         {
           for (int j = 0; j < n; j++)
           {
-            dfs(board, i, j, word, 0);
-            if (found)
+            if (dfs(board, i, j, word, 0))
               return true;
           }
         }
 
-        return found;
+        return false;
     }
 
-    void dfs(vector<vector<char>>& board, int i, int j, string word, int idx)
+    bool dfs(vector<vector<char>>& board, int i, int j, string word, int idx)
     {
        if (idx == word.size())
        {
-         found = true;
-         return;
+         return true;
        }
 
        int m = board.size();
        int n = board[0].size();
 
        if (i < 0 || i >= m || j < 0 || j >= n)
-         return;
-
-       // terminate if already found
-       if (found)
-         return;
+         return false;
 
        if (board[i][j] != word[idx])
-         return;
+         return false;
 
        //choose
        char tmp = board[i][j];
-       board[i][j] = '#';
+       board[i][j] = -board[i][j];
 
-       dfs(board, i+1, j, word, idx+1);  
-       dfs(board, i-1, j, word, idx+1);    
-       dfs(board, i, j+1, word, idx+1);  
-       dfs(board, i, j-1, word, idx+1);
+       if (dfs(board, i+1, j, word, idx+1))
+         return true;
+
+       if (dfs(board, i-1, j, word, idx+1))
+         return true;
+
+       if (dfs(board, i, j+1, word, idx+1))
+         return true;
+
+       if (dfs(board, i, j-1, word, idx+1))
+         return true;
 
        // unchoose
-       board[i][j] = tmp;
+       board[i][j] = -board[i][j];
+
+       return false;
     }
 };
