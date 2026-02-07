e9e8c8b6307d705fe67b014f75142fd9e42cbd3b
borisnorm <borisnorm@gmail.com>
2026-02-06 22:25:46 -0800

    Time: 0 ms (100.00%) | Memory: 18.4 MB (95.37%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -11,25 +11,29 @@ public:
 
          int row0 = 0;
          int col0 = 0;
+
+          // 步骤1: 检查第0列是否有0
          for(int i = 0; i < m; i++)
          {
            if (matrix[i][0] == 0)
            {
-              row0 = 1;
+              col0 = 1;
               break;
            }
          }
 
+         // 步骤2: 检查第0行是否有0
         for (int j = 0; j < n; j++)
         {
             if (matrix[0][j] == 0)
             {
-               col0 = 1;
+               row0 = 1;
                break;
             }
         }
 
-
+        // 步骤3: 扫描剩余矩阵，用第0行和第0列做标记
+        // 从 i = 1 , j = 1 开始遍历 编辑 [i][0] [0][j]
         for (int i = 1; i < m; i++)
         {
             for (int j = 1; j < n; j++)
@@ -42,6 +46,8 @@ public:
             }
         }
 
+        // 步骤4: 根据标记清零（从1开始，避免影响标记区域）
+        // 从 i = 1, j = 1 开始 清除 1
         for (int i = 1; i < m; i++)
         {
             for (int j = 1; j < n; j++)
@@ -49,19 +55,22 @@ public:
                 if (matrix[i][0] == 0 || matrix[0][j] ==0)
                     matrix[i][j] = 0;
             }
+        } 
+
+       // 步骤5: 处理第0行
+        if (row0)
+        {
+            for (int j = 0; j < n; j++)
+              matrix[0][j] = 0;
         }
 
-        if (row0)
+        // 步骤6: 处理第0列
+        if (col0)
         {
             for (int i = 0; i < m; i++)
               matrix[i][0] = 0;
         }
 
-        if (col0)
-        {
-            for (int j = 0; j < n; j++)
-              matrix[0][j] = 0;
-        }
     }
 };
 /* not in place
