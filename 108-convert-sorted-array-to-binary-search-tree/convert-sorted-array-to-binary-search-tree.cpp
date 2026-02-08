dc1792d371638fe2ae55f761a46d4f9781fd3be0
borisnorm <borisnorm@gmail.com>
2026-02-07 17:53:31 -0800

    Time: 0 ms (100.00%) | Memory: 22.2 MB (98.00%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -12,6 +12,85 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.empty())
+          return nullptr;
+        int n = nums.size();
+
+        int l = 0;
+        int r = n - 1;
+        TreeNode* root = nullptr;
+        queue<tuple<TreeNode**, int, int>> q;
+        q.push({&root, l, r });
+        
+        while (!q.empty())
+        {
+            auto [nodePtr, l, r] = q.front();
+            q.pop();
+
+            int mid = l + (r-l)/2;
+            
+            *nodePtr = new TreeNode(nums[mid]);
+            if (l <= mid-1)
+            {
+                q.push({&((*nodePtr)->left), l, mid-1});
+            }
+
+            if (mid+1 <= r)
+            {
+                q.push({&((*nodePtr)->right), mid+1, r});
+            }
+        }
+
+        return root;
+    }
+};
+/*
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.empty())
+          return nullptr;
+        
+        int n = nums.size();
+        int l = 0;
+        int r = n-1;
+
+        TreeNode* root = nullptr;
+        stack<tuple<TreeNode**, int, int>> st;
+        st.push({&root, l, r});
+
+        while (!st.empty())
+        {
+           auto [nodePtr, l, r] = st.top();
+           st.pop();
+
+           int mid = l + (r-l)/2;
+
+           *nodePtr = new TreeNode(nums[mid]);
+
+           // right first, then left
+           if (mid + 1 <= r)
+           {
+              st.push({&((*nodePtr)->right), mid+1, r});
+           }
+
+           if (l <= mid - 1)
+           {
+              st.push({&((*nodePtr)->left), l, mid-1});
+           }
+        }
+
+        return root;
+    }
+};
+*/
+
+/*
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.empty())
+          return nullptr;
         return buildBST(nums, 0, nums.size()-1);      
     }
 
@@ -22,6 +101,7 @@ public:
 
         int mid = left + (right-left)/2;
 
+        //前序遍历
         TreeNode* node = new TreeNode(nums[mid]);
 
         node->left  = buildBST(nums, left, mid-1);
@@ -29,6 +109,5 @@ public:
           
        return node;      
     }
-
-    
 };
+*/
