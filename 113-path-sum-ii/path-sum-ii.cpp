c97c08d25b8c6f66322080979249b3c32a6ba050
borisnorm <borisnorm@gmail.com>
2026-02-09 00:23:01 -0800

    Time: 0 ms (100.00%) | Memory: 21 MB (67.44%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -26,20 +26,60 @@ public:
         if (!root)
           return;
 
-        targetSum = targetSum - root->val;
-
+        //targetSum = targetSum - root->val;
         path.push_back(root->val);
 
-        if (!root->left && !root->right && targetSum == 0)
+        if (!root->left && !root->right && targetSum == root->val)
         {
+           //如果是 leaf 节点, 那么 再次调用 dfs(root->left) 活儿 dfs(root->right) 他们都会返回 null 是
            res.push_back(path);
+           // 没有 return 是因为要 path.pop_back();, 不然会污染路径的
         }
-        //else
-        {
-          dfs(root->left, targetSum, path);
-          dfs(root->right, targetSum, path);
-        }
+ 
+        dfs(root->left, targetSum-root->val, path);
+        dfs(root->right, targetSum-root->val, path);
 
         path.pop_back();
     }
 };
+
+/*
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if (!root)
+          return {};
+
+        vector<int> path;
+        vector<vector<int>> res;
+
+        stack<tuple<TreeNode*, int, vector<int>>> st;
+        st.push({root, targetSum, path});
+
+
+         //TreeNode* cur = root;
+        while(!st.empty())
+        {
+          auto [cur, target, path] = st.top();
+          st.pop();
+
+          target = target - cur->val;
+          path.push_back(cur->val);
+          if (!cur->left && !cur->right && target == 0)
+          {
+             res.push_back(path);
+          }
+
+          if (cur->right)
+            st.push({cur->right, target, path});
+        
+          if (cur->left)
+            st.push({cur->left, target, path});
+        
+        }
+
+        return res;
+    }
+};
+
+*/
