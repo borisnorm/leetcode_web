d6166ffc023a478f141fa24a8651c98a219deff4
borisnorm <borisnorm@gmail.com>
2026-02-08 17:27:43 -0800

    Time: 8 ms (4.99%) | Memory: 31.7 MB (5.39%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -11,6 +11,55 @@
  */
 class Solution {
 public:
+
+    int maxPathSum(TreeNode* root) {
+        if (!root)
+          return 0;
+        
+        stack<TreeNode*> st;
+        unordered_map<TreeNode*, int> n2sum;
+        TreeNode* cur = root;
+        TreeNode* last = nullptr;
+        int maxSum = INT_MIN;
+
+        while (cur || !st.empty())
+        {
+            while (cur)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+
+            TreeNode* node = st.top();
+            if (!node->right || last == node->right)
+            {
+                int l = 0;
+                if (node->left && n2sum.count(node->left))
+                  l = max(0, n2sum[node->left]);
+                
+                int r = 0;
+                if (node->right && n2sum.count(node->right))
+                  r = max(0, n2sum[node->right]);
+                
+                maxSum = max(maxSum, l + node->val + r);
+
+                n2sum[node] = max(l, r) + node->val;
+
+                st.pop();
+                last = node;
+                cur = nullptr;
+            }
+            else
+            {
+               cur = node->right;
+            }
+        }
+        return maxSum;
+    }
+};
+/*
+class Solution {
+public:
     int max_sum = INT_MIN;
     int maxPathSum(TreeNode* root) {
        if (!root)
@@ -28,13 +77,12 @@ public:
 
         int left_sum = max(nodePathSum(node->left), 0);
         int right_sum = max(nodePathSum(node->right), 0);
-
+        
+        //取最大值的逻辑
         int node_sum = left_sum + right_sum + node->val;
-
         max_sum = max(max_sum, node_sum);
 
         return max(left_sum, right_sum) + node->val;
     }
-
-
 };
+*/
