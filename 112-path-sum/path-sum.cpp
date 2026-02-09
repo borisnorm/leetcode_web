bd26f2aaebd8c712e77e765e4afea71ff3a4b862
borisnorm <borisnorm@gmail.com>
2026-02-09 11:23:08 -0800

    Time: 0 ms (100.00%) | Memory: 21.6 MB (33.14%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -14,6 +14,33 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if (!root)
           return false;
+        stack<pair<TreeNode*, int>> st;
+        st.push({root, targetSum});
+
+        while(!st.empty())
+        {
+           auto [cur, targetSum] = st.top();
+           st.pop();
+
+           if (!cur->left && !cur->right && targetSum == cur->val)
+             return true;
+           
+           if (cur->right)
+             st.push({cur->right, (targetSum-cur->val)});
+        
+           if (cur->left)
+             st.push({cur->left, (targetSum-cur->val)});
+        }
+
+        return false;
+    }
+};
+/*
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root)
+          return false;
         
         int rootVal = root->val;
         if (!root->left && !root->right && rootVal == targetSum)
@@ -25,3 +52,4 @@ public:
         return leftFound || rightFound;
     }
 };
+*/
