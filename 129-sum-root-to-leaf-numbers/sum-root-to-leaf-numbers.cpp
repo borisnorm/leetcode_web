54807de0d155e0ca880ff707b93544930c028293
borisnorm <borisnorm@gmail.com>
2026-02-09 20:35:02 -0800

    Time: 0 ms (100.00%) | Memory: 11.8 MB (89.59%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -9,6 +9,73 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        if (!root)
+          return 0;
+        
+        int num = 0;
+        long long res = dfs(root, num);
+
+        return (int)res;
+    }
+
+    long long dfs(TreeNode* root, long long num)
+    {
+        if (!root)
+          return 0;  // 如果左右子节点为空, 应该返回当前的 val
+
+        num = num*10 + root->val;
+        if (!root->left && !root->right)
+          return num;
+        
+        int leftNum = dfs(root->left, num);
+        int rightNum = dfs(root->right, num);
+
+        return leftNum + rightNum;
+    }
+};
+
+/*
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        if (!root)
+          return 0;
+        
+        stack<pair<TreeNode*, long long>> st;
+        st.push({root, 0});
+    
+        long long sum = 0;
+
+        while(!st.empty())
+        {
+           auto [cur, num] = st.top();
+           st.pop();
+
+           num = num * 10 + cur->val;
+           if (!cur->left && !cur->right)
+           {
+              sum += num;
+           }
+
+           if (cur->right)
+             st.push({cur->right, num});
+            
+           if (cur->left)
+             st.push({cur->left, num});
+        }
+
+        return (int)sum;
+    }
+};
+
+*/
+
+
+/*
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
@@ -33,3 +100,4 @@ public:
         return leftVal + rightVal;
     }
 };
+*/
