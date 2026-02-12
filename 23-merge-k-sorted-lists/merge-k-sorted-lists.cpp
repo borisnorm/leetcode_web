9b65b93e496c56e0c2970ef9d6218ee6cd53b8c2
borisnorm <borisnorm@gmail.com>
2026-02-11 20:55:16 -0800

    Time: 1 ms (75.89%) | Memory: 18.7 MB (39.31%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -17,6 +17,16 @@ public:
         int interval = 1;
         int n = (int)lists.size();
 
+        // n 是 奇数  11
+        // interval  1, 2, 4, 8
+        //            Merge 外    从 2 跳到 4 需要 2 * interval, 进行滚动 合并
+        // interval = 1: [0, 1] [2, 3]-- 2*interval -->[4, 5] [6, 7] [8, 9] [10]
+        // interval = 2: [0, 2] [4, 6] [8, 10]
+        // interval = 4: [0, 4] [8]
+        // interval = 8: [0, 8]
+        //            Merge 内   需要 i, i + interval 才可以
+        // 公式 很明显是    lists[i] = mergeTwo(list[i], list[i+interval]);
+        // 正因为这个 interval 我们才需要将 interval 初始化 为 1; 
         while (interval < n)
         {
            for (int i = 0; i + interval < n; i+= interval *2)
@@ -28,30 +38,30 @@ public:
         return lists[0];
     }
 
-    ListNode* mergeTwo(ListNode* a, ListNode* b)
+    ListNode* mergeTwo(ListNode* l1, ListNode* l2)
     {
         ListNode dummy(0);
-        ListNode* dummy_head = &dummy;
+        ListNode* cur = &dummy;
 
-        while (a && b)
+        while (l1 && l2)
         {
 
-           if (a->val <= b->val)
+           if (l1->val <= l2->val)
            {
-             dummy_head->next = a;
-             a = a->next;
+             cur->next = l1;
+             l1 = l1->next;
            }
            else
            {
-             dummy_head->next = b;
-             b = b->next;
+             cur->next = l2;
+             l2 = l2->next;
            }
 
-           dummy_head = dummy_head->next;
+          cur = cur->next;
 
         }
 
-        dummy_head->next = a ? a : b;
+        cur->next = l1 ? l1 : l2;
 
         return dummy.next;
     }
@@ -72,21 +82,21 @@ public:
             pq.push(head);
         
         ListNode dummy(0);
-        ListNode* tail = &dummy;
+        ListNode* cur = &dummy;
 
         while (!pq.empty())
         {
-           ListNode* cur = pq.top();
+           ListNode* node = pq.top();
            pq.pop();
 
-           tail->next = cur;
-           tail = tail->next;
+           cur->next = node;
+           cur = cur->next;
 
-           if (cur->next)
-             pq.push(cur->next);
+           if (node->next)
+             pq.push(node->next);
         }
 
-        tail->next = nullptr;
+        cur->next = nullptr;
 
         return dummy.next;
     }
