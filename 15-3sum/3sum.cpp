56b9a7f64f29d65426b310b8a4b319542fb7e513
borisnorm <borisnorm@gmail.com>
2026-02-11 20:31:09 -0800

    Time: 31 ms (99.17%) | Memory: 29.1 MB (45.60%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -11,35 +11,44 @@ public:
           return res;
 
         sort(nums.begin(), nums.end());
-
+    
+        //换成  nums[i] , nums[i+1], nums[i+2] 就更好理解
+        //              i + 2 < n;
         for (int i = 0; i < n - 2; i++)
         {
+           if (i > 0 && nums[i] == nums[i-1])
+             continue;
+
+           if (nums[i] > 0)
+             break;
+         
+           if (nums[i] + nums[i+1] + nums[i+2] > 0)
+             break;
+           
+           if (nums[i] + nums[n-2] + nums[n-1] < 0)
+             continue;
+
            int target = 0 - nums[i];
 
            int l = i + 1;
            int r = n - 1;
-
-           if (i > 0 && nums[i] == nums[i-1])
-             continue;
          
            while (l < r)
            {
-             int sum = nums[l]+nums[r];
+             long long sum = 1LL*nums[l]+nums[r];
              if (sum < target)
               l++;
              else if (sum > target)
               r--;
              else
              {
-
+                res.push_back({nums[i], nums[l], nums[r]});
+                //留下的是 最后一个 相同值的 l, 不是 l+1, 当 l 与 l+1不相同的时候, l 不会向前迈的
+                //l+1是那个不同, 停在了最后一个与前面值相同的l上面
                 while (l < r && nums[l] == nums[l+1])
                   l++;
-
                 while (r > l && nums[r] == nums[r-1])
                   r--;
-
-                res.push_back({nums[i], nums[l], nums[r]});
-
                 l++;
                 r--;
              }
