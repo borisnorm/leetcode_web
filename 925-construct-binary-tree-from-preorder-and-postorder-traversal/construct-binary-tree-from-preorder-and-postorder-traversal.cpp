51f203f40092030666ee731df57feea85e5b149d
borisnorm <borisnorm@gmail.com>
2026-02-13 20:44:55 -0800

    Time: 2 ms (47.91%) | Memory: 28.7 MB (18.60%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> val2idx;
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        if (preorder.empty() || postorder.empty())
+          return nullptr;
+
+        // 1 [2 4 5] [3 6 7]
+        // [4 5 2] [6 7 3] 1
+        // preIdx++
+        
+        // find root
+        // find left range, right range
+        int m = preorder.size();
+        int n = postorder.size();
+        for(int i = 0; i < n; i++)
+          val2idx[postorder[i]] = i;
+
+        int preIdx = 0;
+        return build(preorder, preIdx, postorder, 0, n-1);
+    }
+
+    TreeNode* build(vector<int>& preorder, int& preIdx, 
+                    vector<int>& postorder, int postL, int postR)
+    {
+        if (postL > postR)
+            return nullptr;
+
+        int rootval = preorder[preIdx++];
+        TreeNode* root = new TreeNode(rootval);
+
+        if (postL == postR)
+          return root;
+        
+        int leftRootVal = preorder[preIdx];
+        int postIdx = val2idx[leftRootVal];
+
+        root->left = build(preorder, preIdx,
+                          postorder, postL, postIdx);
+        root->right = build(preorder, preIdx,
+                          postorder, postIdx+1, postR-1);
+        return root;
+    }
+};
