3d2c790ae78bc03ee231389531a1414144dc8de2
borisnorm <borisnorm@gmail.com>
2026-02-14 14:10:45 -0800

    Time: 0 ms (100.00%) | Memory: 16.4 MB (45.81%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,33 +9,67 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        if (preorder.empty())
+          return nullptr;
+        
+        int preIdx = 0;
+        return build(preorder, preIdx, INT_MIN, INT_MAX);
+    }
+
+    TreeNode* build(vector<int>& preorder, int& preIdx, long long low, long long high)
+    {
+       int n = preorder.size();
+       if (preIdx >= n)
+         return nullptr;
+       
+       int rootval = preorder[preIdx];
+       if (rootval <= low || rootval >= high)
+         return nullptr;
+
+       preIdx++;
+    
+       TreeNode* root = new TreeNode(rootval);
+
+       root->left = build(preorder, preIdx, low, root->val);
+       root->right = build(preorder, preIdx, root->val, high);
+
+       return root;
+    }
+ };
+/*
 class Solution {
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int i = 0;
-        return dfs(preorder, i, INT_MAX);
-
+        int preIdx = 0;
+        return dfs(preorder, preIdx, INT_MAX);
     }
 
-    TreeNode* dfs(vector<int>& preorder, int& i, int bound)
+    TreeNode* dfs(vector<int>& preorder, int& preIdx, int bound)
     {
-
        int n = preorder.size();
        if (i >= n)
          return nullptr;
     
-       int val = preorder[i];
-
+       int val = preorder[preIdx++];
        if (val > bound)
          return nullptr;
        TreeNode* node = new TreeNode(val);  // 构建的时候初始下面为 nullptr;
 
-       i++;
-
-       node->left = dfs(preorder, i, val);
-
-       node->right = dfs(preorder, i, bound);
+       //在构造左子树期间
+       //我们绝对不会看到右子树的元素
+       //因为右子树的元素一定在 preorder 的后面
+       //也就是说：
+       //当我们进入右子树递归时：
+       //所有 < root->val 的值
+       //已经全部被左子树“吃掉”了
+       
+       node->left  = dfs(preorder, preIdx, val);
+       node->right = dfs(preorder, preIdx, bound);
 
        return node;
     }
 };
+*/
