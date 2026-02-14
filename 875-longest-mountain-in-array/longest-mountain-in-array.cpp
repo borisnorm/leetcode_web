d297b5b3faabd801fee7d95f1a2926cde035c450
borisnorm <borisnorm@gmail.com>
2026-02-14 11:13:15 -0800

    Time: 0 ms (100.00%) | Memory: 22.2 MB (79.65%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.cpp
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -8,20 +8,20 @@ public:
         int maxLen = 0;
         int i = 0;
 
-        while (i < n) {
+        while (i < n-1) {
             int start = i;
 
-            // 1) 先走严格上升
+            // 1) 先走严格上升, 如果 相等 = 或是 大于 >, 这里 start 就 == i 了,  
             while (i + 1 < n && arr[i] < arr[i + 1]) 
               i++;
             int peak = i;
 
-            // 2) 再走严格下降
+            // 2) 再走严格下降, 如果 相等 = 或是 小于 <, 这里 end 就 == i 了,
             while (i + 1 < n && arr[i] > arr[i + 1]) 
               i++;
             int end = i;
 
-            // 3) 必须同时有上升和下降，且长度>=3
+            // 3) 必须同时有上升和下降，且长度>=3, 也就是 同时检查 (1) 与 (2)
             // 判断 maxlen 是否合法 
             // 比如 1. 全相等的 len   start = end = 0       len = start - end + 1  = 1
             //     2. 一直升序的 len [1,2,3,4,5]            len = n - 1 - 0 + 1 = n    
