c14385b2e7ef1e7305dfe5da3d4bd3be7583026f
borisnorm <borisnorm@gmail.com>
2026-02-15 14:31:41 -0800

    Time: 2207 ms (5.01%) | Memory: 59.2 MB (97.04%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,3 +1,49 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        int targetIdx = n - k;
+
+        return quickSelect(nums, 0, n-1, targetIdx);
+    }
+
+    int quickSelect(vector<int>& nums, int l, int r, int k)
+    {
+        if (l == r)
+          return nums[l];
+        
+        int pivotIdx = partition(nums, l, r);
+
+        if (pivotIdx == k)
+          return nums[k];
+        else if (pivotIdx > k)
+          return quickSelect(nums, l, pivotIdx-1, k);
+        else
+          return quickSelect(nums, pivotIdx+1, r, k);
+    }
+
+    int partition(vector<int>& nums, int l, int r)
+    {
+        int pivot = nums[r]; //选最右边的元素作为 pivot
+        int i = l; // i指向下一个小于 pivot 的元素应该放的位置
+
+        //遍历[l, r-1] , 把小于 pivot的都移到左边
+        for (int j = l; j < r; j++)
+        {
+            if (nums[j] < pivot)
+            {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
+
+        //把pivot 放到正确的位置(所有小于它的元素之后)
+        swap(nums[i], nums[r]);
+
+        // 返回 pivot 的最终位置
+        return i;
+    }
+};
 /*
 class Solution {
 public:
@@ -23,6 +69,7 @@ public:
 };
 */
 
+/*
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
@@ -34,19 +81,44 @@ public:
         if (n < k)
           return -1;
         
+        // 也可以这么初始化
+        //  priority_queue<int> maxHeap(nums.begin(), nums.end());
         priority_queue<int> pq;
         for (int num: nums)
           pq.push(num);
         
         int kthLargest = 0;
-        /*
         for(int i = 0; i < k; i++)
         {
             kthLargest = pq.top();
             pq.pop();
         }
-        */
+        
+        return kthLargest;        
+    }
+};
+*/
 
+/*
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        if (nums.empty() || k <= 0)
+          return -1;
+        
+        int n = nums.size();
+        
+        if (n < k)
+          return -1;
+        
+        // 也可以这么初始化
+        //  priority_queue<int> maxHeap(nums.begin(), nums.end());
+        priority_queue<int> pq;
+        for (int num: nums)
+          pq.push(num);
+        
+        int kthLargest = 0;
+        
         //pq 没有 for-range的用法
         // 使用正常的 sz loop
         int cnt = 0;
@@ -60,6 +132,8 @@ public:
             if (cnt == k)
               break;
         }
+      
         return kthLargest;        
     }
 };
+*/
