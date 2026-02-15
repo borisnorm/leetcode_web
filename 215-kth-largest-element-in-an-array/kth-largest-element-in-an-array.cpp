a533f5876c835c93cc459547bc8a6b5eca1269e7
borisnorm <borisnorm@gmail.com>
2026-02-15 13:03:13 -0800

    Time: 35 ms (46.78%) | Memory: 64.8 MB (11.20%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,15 +1,16 @@
+/*
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         
-        // 大顶堆
-        
-        if (nums.empty())
-          return 0;
+        //这是小顶堆的解法
+        if (nums.empty() || k <= 0)
+          return -1;
 
         priority_queue<int, vector<int>, greater<int>> nums_pq;  //小顶堆
-
         int n = nums.size();
+        if (n < k)
+          return -1;
         for (int i = 0; i < n; i++)
         {
            nums_pq.push(nums[i]);
@@ -18,5 +19,32 @@ public:
         }
 
         return nums_pq.top();
+    }
+};
+*/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        if (nums.empty() || k <= 0)
+          return -1;
+        
+        int n = nums.size();
+        
+        if (n < k)
+          return -1;
+        
+        priority_queue<int> pq;
+        for (int num: nums)
+          pq.push(num);
+        
+        int cnt = 0;
+        int kthLargest = 0;
+        for(int i = 0; i < k; i++)
+        {
+            kthLargest = pq.top();
+            pq.pop();
+        }
+        return kthLargest;        
     }
 };
