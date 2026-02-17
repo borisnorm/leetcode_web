91d6d9880385de5169e78bde5a24cbb9c96578db
borisnorm <borisnorm@gmail.com>
2026-02-16 16:41:13 -0800

    Time: 25 ms (40.60%) | Memory: 23.4 MB (65.51%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -18,7 +18,8 @@ public:
         
         if (p->val < root->val && q->val < root->val)
           return lowestCommonAncestor(root->left, p, q);
-        else if (p->val > root->val && q->val > root->val)
+        
+        if (p->val > root->val && q->val > root->val)
           return lowestCommonAncestor(root->right, p, q);
         
         return root;
