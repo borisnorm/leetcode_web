e7e144c7375b38a44029061c2b6e63f629a1b476
borisnorm <borisnorm@gmail.com>
2026-02-18 12:51:21 -0800

    Time: 5 ms (17.51%) | Memory: 11.3 MB (23.65%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        if (s.empty())
+          return "";
+    
         string res;
         vector<string> words;
-
-        if (s.empty())
-          return res;
 
         int n = (int)s.size();
         int i = 0;
 
+        //前置 leading space ' ', hello 前面的空格
         while (i < n && s[i] == ' ')
           i++;
 
@@ -21,23 +22,20 @@ public:
            string word = s.substr(start, i - start);
            words.push_back(word);
            
+           //后置 leading space ' ', world 后来的空格
            while (i < n && s[i] == ' ')
             i++;
         }
 
         reverse(words.begin(), words.end());
-
-        string ans;
         for (int i = 0; i < words.size(); i++)
         {
             if (i > 0)
-              ans += ' ';
+              res += ' ';
 
-            ans += words[i];
+            res += words[i];
         }
 
-        return ans;
-
-        
+        return res;
     }
 };
