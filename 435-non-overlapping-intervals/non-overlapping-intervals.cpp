389f5fd5ae79bfb47e0aecb3cc4d7d722f30d10c
borisnorm <borisnorm@gmail.com>
2026-02-18 23:26:50 -0800

    Time: 35 ms (94.11%) | Memory: 94 MB (65.77%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        if (n == 1)
+          return 0;
+        
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b){return a[1] < b[1];});
+
+        int removed = 0;
+        
+        int lastEnd = intervals[0][1];
+
+        for (int i = 1; i < n; i++)
+        {
+            if (intervals[i][0] >= lastEnd)
+            {
+               lastEnd = intervals[i][1];
+            }
+            else
+            {
+               removed++;
+            }
+        }
+
+        return removed;
+        
+    }
+};
+/*
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         
         if (intervals.empty())
           return 0;
@@ -13,8 +43,9 @@ public:
         sort(intervals.begin(), intervals.end(), cmp);
 
         int lastEnd = intervals[0][1];
+
+        // 按照 end 排序后的第一个interval 是结束最早的, 可以保留的
         int cnt = 1;
-        int removed = 0;
 
         for (int i = 1; i < n; i++)
         {
@@ -24,14 +55,11 @@ public:
               cnt++;
               lastEnd = intervals[i][1];
            }
-           else
-           {
-              removed++;
-           }
         }
 
-        return removed;
-        //return n - cnt;
+        return n - cnt;
 
     }
 };
+
+*/
