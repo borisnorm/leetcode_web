edc6e2372ce35d11586eef32744ef8e836919d5e
borisnorm <borisnorm@gmail.com>
2026-02-19 11:56:32 -0800

    Time: 38 ms (91.94%) | Memory: 93.8 MB (93.43%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -9,18 +9,19 @@ public:
          sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b){return a[1] < b[1];});
 
          int nArrows = 1;
-         int posArrow = points[0][1];
-         for (int i = 0; i < n; i++)
+         int lastArrowEnd = points[0][1];
+         for (int i = 1; i < n; i++)
          {
             int start = points[i][0];
             int end   = points[i][1];
 
-            if (start <= posArrow)
+            // 当前 balloon 与 前一个 有重叠, 那么不用 增加 arrow 数量
+            if (start <= lastArrowEnd)
               continue;
             else
             {
                 nArrows++;
-                posArrow = end;
+                lastArrowEnd = end;
             }
          }
 
