feaa89002e58a27e1196572404e2c91392e9c299
borisnorm <borisnorm@gmail.com>
2026-02-19 19:46:57 -0800

    0369-PLUS-ONE-LINKED-LIST Stats: Time: 3 ms (40.31%), Space: 12.2 MB (74.42%) - LeetHub

--- 0369-plus-one-linked-list/0369-plus-one-linked-list.cpp
+++ 0369-plus-one-linked-list/0369-plus-one-linked-list.cpp
@@ -12,61 +12,45 @@ class Solution {
 public:
     ListNode* plusOne(ListNode* head) {
         if (!head)
-          return head;
-
-        ListNode dummy(0);
-        ListNode* dummy_head = &dummy;
-
-        dummy_head->next = reverseList(head);
-
-        int sum   = 0;
-        int carry = 0;
+          return new ListNode(1);
+        
+        ListNode* rHead = reverseList(head);
+        int carry = 1;
+        int sum = 0;
         int digit = 0;
-        ListNode* pre = dummy_head;
-        ListNode* cur = pre->next;
-        ListNode* nxt = nullptr;
 
+        ListNode* cur = rHead;
         while(cur)
         {
-           if (cur == dummy_head->next)
-             sum = cur->val + 1 + carry;
-           else
-             sum = cur->val + carry;
+            sum = cur->val + carry;
+            digit = sum % 10;
+            carry = sum / 10;
 
-           digit = sum % 10;
-           carry = sum / 10;
+            cur->val = digit;
 
-           cur->val = digit;
+            if (!cur->next && carry)
+              cur->next = new ListNode(0);
 
-           pre = cur;
-           cur = cur->next;
+            cur = cur->next;
         }
 
-        if (carry)
-          pre->next = new ListNode(carry);
-
-        dummy_head->next = reverseList(dummy_head->next);
-
-        return dummy.next;
-
+        return reverseList(rHead);
     }
 
     ListNode* reverseList(ListNode* head)
     {
-        if (!head)
-          return nullptr;
-
         ListNode* pre = nullptr;
         ListNode* cur = head;
         ListNode* nxt = nullptr;
 
         while(cur)
         {
-           nxt = cur->next;
-           cur->next = pre;
-           pre = cur;
-           cur = nxt;
+            nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
         }
+
         return pre;
     }
 };
