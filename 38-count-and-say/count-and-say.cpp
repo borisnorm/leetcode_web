cc1c7ab0dadcfbd2012324f88e0a3105d99757bb
borisnorm <borisnorm@gmail.com>
2026-02-21 16:11:38 -0800

    Time: 6 ms (27.40%) | Memory: 9.8 MB (63.74%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     string countAndSay(int n) {
+        if (n == 0)
+          return "";
+        
+        string res = "1";
+
+        for (int i = 1; i < n; i++)
+        {
+           string tmp = "";
+           int j = 0;
+           int n = res.size();
+           while (j < n)
+           {
+              char c = res[j];
+              int cnt = 0;
+              while (j < n && res[j] == c)
+              {
+                 j++;
+                 cnt++;
+              }
+
+              tmp += to_string(cnt) + c;
+           }  
+
+           res = tmp;
+        }
+
+        return res;
+    }
+};
+/*
+class Solution {
+public:
+    string countAndSay(int n) {
         string s = "1";
 
         for (int i = 2; i <= n; i++)
@@ -21,11 +54,28 @@ public:
            }
 
            next += to_string(cnt);
-           next += s[s.size()-1];
+           next += s.back();   // j 在这里已经失效了, 
 
-           s = next;
+           s = next;     // 新的 s 经过压缩的 next, 不是 原来的 
         }
 
         return s;
     }
+
+    //错误的思路
+    //11
+    //211
+    //3211
+    //43211
+    //543211
+    //6543211
+    //76543211
+    //876543211
+    //9876543211
+    //109876543211
+    //11109876543211
+    //1211109876543211
+    //131211109876543211
 };
+
+*/
