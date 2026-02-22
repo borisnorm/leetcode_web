c12d53f8f64d2885149f4c190867f2db1caec9c5
borisnorm <borisnorm@gmail.com>
2026-02-22 00:35:38 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (19.09%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,59 +2,57 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {         
          int n = nums.size();
-         vector<int> res;
          if (n <= 0)
            return {-1, -1};
 
-        int start = findLeftRange(nums, target);
-        int end = findRightRange(nums, target);
-        
-        res.push_back(start);
-        res.push_back(end);
+        int start = findFirst(nums, target);
+        int end = findLast(nums, target);
 
-        return res;
+        return {start, end};
     }
 
-    int findLeftRange(vector<int>& nums, int target)
+    int findFirst(vector<int>& nums, int target)
     {
        int n = nums.size();
        int l = 0;
        int r = n -1;
-       int ans = -1;
+       int res = -1;
+
        while (l <= r)
        {
          int mid = l + (r-l)/2;
+
          if (nums[mid] >= target)
            r = mid - 1;
          else
            l = mid + 1;
         
          if (nums[mid] == target)
-           ans = mid;
+           res = mid;
        }
 
-       return ans;
+       return res;
     }
 
-    int findRightRange(vector<int>& nums, int target)
+    int findLast(vector<int>& nums, int target)
     {
         int n = nums.size();
         int l = 0;
         int r = n -1;
-        int ans = -1;
+        int res = -1;
 
         while (l <= r)
         {
            int mid = l + (r - l)/2;
-           if (nums[mid] <= target)
-             l = mid + 1;
-           else
+           if (nums[mid] > target)
              r = mid - 1;
+           else
+             l = mid + 1;
           
            if (nums[mid] == target)
-             ans = mid;
+             res = mid;
         }
 
-        return ans;
+        return res;
     }
 };
