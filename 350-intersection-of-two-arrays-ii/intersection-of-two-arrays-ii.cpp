8495709e1936ae9177618dfb3807e1e2e50d3e02
borisnorm <borisnorm@gmail.com>
2026-02-21 18:28:05 -0800

    Time: 0 ms (100.00%) | Memory: 14.9 MB (15.69%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+        vector<int> res;
+
+        unordered_map<int, int> nums1_freq;
+        for (int num: nums1)
+          nums1_freq[num]++;
+        
+        for (int num: nums2)
+        {
+           if (nums1_freq[num] > 0)
+           {
+              res.push_back(num);
+              nums1_freq[num]--;
+           }
+        }
+
+        return res;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int> nums1_mp;
         for (auto& num: nums1)
-            nums1_mp[num]++;
+          nums1_mp[num]++;
        
         unordered_map<int, int> nums2_mp;
         for (auto& num: nums2)
-        {
           nums2_mp[num]++;
-        }
         
         vector<int> res;
-        for (auto& it: nums1_mp)
+        for (auto& p: nums1_mp)
         {
-           int num = it.first;
+           int num = p.first;
            if (nums2_mp.count(num))
            {
-             for (int i = 0; i < min(it.second, nums2_mp[num]); i++)
+             for (int i = 0; i < min(p.second, nums2_mp[num]); i++)
                res.push_back(num);
            }
         }
@@ -25,3 +47,5 @@ public:
         return res;
     }
 };
+
+*/
