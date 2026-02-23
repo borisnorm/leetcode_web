05161a33894b4e5f1fae6c74b511f70a3acbb30c
borisnorm <borisnorm@gmail.com>
2026-02-23 13:38:45 -0800

    Time: 0 ms (100.00%) | Memory: 22.5 MB (77.82%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
+
+ int hIndex(vector<int>& citations) {
+     
+     int n = citations.size();
+     int l = 0;
+     int r = n;
+
+     //find first >= h (n-mid) 闭区间写法
+     //二分搜索的是 citations 数组的下标，通过下标间接判断条件
+
+     // 二分 边界搜素 也有 开区间 与 闭区间 2种写法, 
+     // 只是对于 闭区间, 搜索左边界,答案 在 l, 搜索右边界, 答案在 r
+     // 而      开区间, 答案都是 l
+     while (l < r)
+     {
+        int mid = l + (r-l)/2;
+
+        if (citations[mid] >= (n - mid))
+          r = mid;
+        else
+          l = mid + 1;
+     }
+
+     return n-l;
+
+   }
+ };
+
+/*
+class Solution {
+public:
+
     int hIndex(vector<int>& citations) {
-        
 
         int n = citations.size();
         int h = 0;
         for (int i = 0; i < n; i++)
         {
             int papers = n - i;
-            if (papers <= citations[i])
+            if (citations[i] >= papers)
               h = max(h, papers);
         }
 
         return h;
     }
 };
+*/
