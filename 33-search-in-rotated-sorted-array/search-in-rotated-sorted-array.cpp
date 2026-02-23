42ed902335bf632e6e082088ebd3bcd0600be9d3
borisnorm <borisnorm@gmail.com>
2026-02-22 17:34:08 -0800

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.43%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+          int n = nums.size();
+          int l = 0;
+          int r = n - 1;
+
+          while (l <= r)
+          {
+            int mid = l + (r-l)/2;
+
+            if (nums[mid] == target)
+              return mid;
+
+            if (nums[mid] <= nums[r])
+            {
+               if (nums[mid] < target && target <= nums[r])
+                 l = mid + 1;
+               else
+                 r = mid - 1;
+            }
+            else
+            {
+               if (nums[l] <= target && target < nums[mid])
+                 r = mid -1;
+               else
+                 l = mid + 1;
+            }
+          }
+
+          return -1;
+    }
+};
+/*
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
         if (nums.empty())
           return -1;
 
@@ -8,6 +42,8 @@ public:
         int l = 0;
         int r = n - 1;
 
+        // corner case
+        // what if nums[0] < nums[r-1];
         if (n == 1)
           return nums[0] == target ? 0 : -1;
 
@@ -37,3 +73,5 @@ public:
         return -1;
     }
 };
+
+*/
