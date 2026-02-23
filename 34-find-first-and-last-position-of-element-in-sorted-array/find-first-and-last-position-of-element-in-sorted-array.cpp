8995b7c44edd147ef128b1fcfddd7741f1f7d8d3
borisnorm <borisnorm@gmail.com>
2026-02-22 23:26:28 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (54.00%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -49,8 +49,68 @@ public:
            l = mid + 1;
         
         //这里用 res 记录每次命中的 mid，因为闭区间写法循环结束时 lo > hi，lo 不一定是答案。
-        // if (nums[mid] == target)
-        //   res = mid;
+         if (nums[mid] == target)
+           res = mid;
+       }
+
+       return res;
+    }
+
+    int findLast(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        int l = 0;
+        int r = n -1;
+        int res = -1;
+
+        // find first > target
+        while (l <= r)
+        {
+           int mid = l + (r - l)/2;
+           if (nums[mid] > target)
+             r = mid - 1;
+           else
+             l = mid + 1;
+          
+           if (nums[mid] == target)
+             res = mid;
+        }
+
+        return res;
+    }
+};
+
+
+/*
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {         
+         int n = nums.size();
+         if (n <= 0)
+           return {-1, -1};
+
+        int start = findFirst(nums, target);
+        int end = findLast(nums, target);
+
+        return {start, end};
+    }
+
+    int findFirst(vector<int>& nums, int target)
+    {
+       int n = nums.size();
+       int l = 0;
+       int r = n -1;
+       int res = -1;
+
+       while (l <= r)
+       {
+         int mid = l + (r-l)/2;
+
+         if (nums[mid] >= target)
+           r = mid - 1;
+         else
+           l = mid + 1;
        }
 
        if (l < n && nums[l] == target)
@@ -74,9 +134,6 @@ public:
              r = mid - 1;
            else
              l = mid + 1;
-          
-           //if (nums[mid] == target)
-           //  res = mid;
         }
 
         if (r >= 0 && nums[r] == target)
@@ -85,6 +142,8 @@ public:
         return res;
     }
 };
+
+*/
 
 
 /*
