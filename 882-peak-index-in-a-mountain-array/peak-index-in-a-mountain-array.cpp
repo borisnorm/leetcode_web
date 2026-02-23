e759ea82befaf2a8b9e9b328c5b5cf6524ce2bb5
borisnorm <borisnorm@gmail.com>
2026-02-23 11:40:34 -0800

    Time: 0 ms (100.00%) | Memory: 63.4 MB (97.29%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+  int peakIndexInMountainArray(vector<int>& arr) {
+     int n = arr.size();
+     int l = 0;
+     int r = n;
+
+     while (l < r)
+     {
+        int mid = l + (r -l)/2;
+
+        // monolithic increase on this part 
+        // n-1 < n
+        // mid =  n - 1 + n-(n-1)/2 = n - 1 + 1/2 = n + 1/2 
+        if (arr[mid] < arr[mid+1])
+        {
+           l = mid + 1;
+        }
+        else
+        { 
+           r = mid;
+        }
+
+     }
+
+     return l;
+  }
+    /*
     int peakIndexInMountainArray(vector<int>& arr) {
         //monolithc stack 
 
@@ -22,4 +48,5 @@ public:
 
         return peakIdx;
     }
+    */
 };
