d67b34769722fb81ca65b2fecea7ee5194b8cb8a
borisnorm <borisnorm@gmail.com>
2026-02-27 19:11:27 -0800

    Time: 6 ms (89.37%) | Memory: 9.3 MB (79.60%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,10 +1,57 @@
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty())
+          return "";
+        
+        int n = s.size();
+
+        int len = 0;
+        int maxLen = INT_MIN;
+        int start = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+           // odd number
+           int len1 = expand(s, i, i);
+           // even number
+           int len2 = expand(s, i, i+1);
+        
+           len = max(len1, len2);
+           if (len > maxLen)
+           {
+              maxLen = len;
+              //start  = i - (len -1) / 2;
+              start = i - (len+1)/2 + 1;
+           }
+        }
+
+        return s.substr(start, maxLen);
+    }
+
+    int expand(string& s, int l, int r)
+    {
+        int n = s.size();
+        while (l >= 0 && r < n && s[l] == s[r])
+        {
+           l--;
+           r++;
+        }
+
+        return r - l -1;
+    }
+};
+/*
 class Solution {
 public:
     string longestPalindrome(string s) {
         string res ="";
         for (int i = 0; i < s.size(); i++)
         {
+             // 情况1: 奇数长度回文，以i为中心
            string s1 = palindrome(s, i, i);
+            // 情况2: 偶数长度回文，以i和i+1为中心
            string s2 = palindrome(s, i, i+1);
 
            res = s1.size() > res.size() ? s1 : res;
@@ -26,3 +73,6 @@ public:
        return s.substr(l+1, r-l-1); // r-1 - (l + 1) + 1 = r - l - 1   
     }
 };
+
+
+*/
