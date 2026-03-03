6dc2fc4393da03887614e805de4b1a79e61bcfab
borisnorm <borisnorm@gmail.com>
2026-03-03 15:08:43 -0800

    Time: 8 ms (37.90%) | Memory: 102.1 MB (66.97%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        
+        if (grid.empty() || grid[0].empty())
+          return 0;
+
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         int m = grid.size();
-        if (m <= 0)
-          return 0;
         int n = grid[0].size();
-        if (n <= 0)
-          return 0;
+        int perim = 0;
+        for (int i = 0; i < m; i++)
+        {
+           for (int j = 0; j < n; j++)
+           {
+              if (grid[i][j] == 0)
+                 continue;
+
+              perim += 4;
+
+              for (auto dir: dirs)
+              {
+                 int nx = i + dir.first;
+                 int ny = j + dir.second;
+
+                 if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+                   continue;
+                 
+                 if (grid[nx][ny] == 1)
+                   perim--;
+              }
+
+           }
+        }
+
+        return perim;
+    }
+};
+/*
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
         
+        if (grid.empty() || grid[0].empty())
+          return 0;
         // 4 * blocks - 2 * adj blocks; 
         int perim = 0;
-        
 
         //避免算重
         //对当前格子 (i, j)：
@@ -29,6 +61,22 @@ public:
               {
                  perim += 4;
 
+                 //   每个格子指计算 向上 与 向左的 cell == 1 的格子
+                 //   cell 1 不会计算 与 cell 2 的交汇边
+                 //   cell 2 会计算 与 cell 1 的交汇处
+                 //   perim - 2 减的是自己的边, 与 上面的边
+                 //   ____________
+                 //   |           |
+                 //   |     1     |
+                 //   |           |
+                 //   |           |
+                 //   |-----------|
+                 //   |	   ^     |
+                 //   |     |     |
+                 //   |<--- |     | 
+                 //   |     |     |
+                 //   |     2     |
+                 //   |___________|
                  if (i > 0 && grid[i-1][j] == 1)
                    perim -=2;
 
@@ -41,3 +89,5 @@ public:
         return perim;
     }
 };
+
+*/
