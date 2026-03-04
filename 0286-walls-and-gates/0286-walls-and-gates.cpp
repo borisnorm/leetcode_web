18e32c5118bace69441de08463d1edaedbd10490
borisnorm <borisnorm@gmail.com>
2026-03-04 12:53:23 -0800

    0286-WALLS-AND-GATES Stats: Time: 3 ms (74.64%), Space: 18.9 MB (82.44%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
-    vector<pair<int, int>> dirs = 
-    {           {-1, 0},
-         {0, -1},      {0, 1},
-                {1,  0} 
-    };
-
     void wallsAndGates(vector<vector<int>>& rooms) {
-        
+        if (rooms.empty() || rooms[0].empty())
+          return;
+
         int m = rooms.size();
         int n = rooms[0].size();
-        vector<vector<int>> visited(m, vector<int>(n, 0));
 
         queue<pair<int, int>> q;
-
         for (int i = 0; i < m; i++)
-          for (int j = 0; j < n; j++)
-            if (rooms[i][j] == 0)
-               q.push({i, j});
-
-        while (!q.empty())
         {
-            auto [x, y] = q.front();
-            q.pop();
-
-            for (auto d: dirs)
-            {
-                int nx = x + d.first;
-                int ny = y + d.second;
-
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n)
-                  continue;
-                
-                if (rooms[nx][ny] == INT_MAX)
-                { 
-                   rooms[nx][ny] = rooms[x][y] + 1;
-                   q.push({nx, ny});
-                }
-            }
+           for (int j = 0; j < n; j++)
+           {
+              if (rooms[i][j] == 0)
+              {
+                 q.push({i, j});
+              }
+           }
         }
-        return;
+
+        vector<pair<int, int>> dirs = {{0,1}, {0, -1}, {1, 0},  {-1, 0}};
+        int dist = 0;
+        
+        while(!q.empty())
+        {
+           auto [x, y] = q.front();
+           q.pop();
+
+           for(auto& dir: dirs)
+           {
+              int nx = x + dir.first;
+              int ny = y + dir.second;
+
+              if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+                continue;
+            
+              if (rooms[nx][ny] != INT_MAX)
+                continue;
+            
+              rooms[nx][ny] = rooms[x][y] + 1;
+              q.push({nx, ny});
+           }
+        }
     }
+
 };
