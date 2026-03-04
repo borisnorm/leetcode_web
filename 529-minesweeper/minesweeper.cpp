b2c00c7ff19c018d56dc51e787c4bbf4571c8533
borisnorm <borisnorm@gmail.com>
2026-03-03 20:34:40 -0800

    Time: 0 ms (100.00%) | Memory: 16.8 MB (22.96%) - LeetSync

--- 529-minesweeper/minesweeper.cpp
+++ 529-minesweeper/minesweeper.cpp
@@ -29,44 +29,123 @@ public:
         
         if (board[i][j] != 'E')
           return;
-        //if (board[i][j] == 'B' || board[i][j] == 'X')
-        //  return;
-        
-        //if (board[i][j] == 'E')
+
+        int cnt = 0;
+           
+        vector<pair<int, int>> dirs = {
+           {-1, -1}, {-1, 0}, {-1, 1},
+           {0,  -1},          {0, 1}, 
+           {1,  -1}, {1, 0},  {1, 1} 
+           };
+
+        for (auto dir: dirs)
         {
-           int cnt = 0;
+          int nx = i + dir.first;
+          int ny = j + dir.second;
+
+          if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+            continue;
+
+          if (board[nx][ny] == 'M')
+            cnt++;
+        }
+
+        if (cnt > 0)
+        {
+          board[i][j] = cnt + '0';
+          return;
+        }
+        else
+        {
+          board[i][j] = 'B';
            
-           vector<pair<int, int>> dirs = {
-               {-1, -1}, {-1, 0}, {-1, 1},
-               {0,  -1},          {0, 1}, 
-               {1,  -1}, {1, 0},  {1, 1} 
-               };
+          for (auto dir: dirs)
+          {
+            int nx = i + dir.first;
+            int ny = j + dir.second;
 
-           for (auto dir: dirs)
-           {
-              int nx = i + dir.first;
-              int ny = j + dir.second;
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+              continue;
 
-              if (nx >= 0 && nx < m && ny >= 0 && ny < n)
-              {
-                  if (board[nx][ny] == 'M')
-                    cnt++;
-              }
-           }
-
-           if (cnt > 0)
-           {
-             board[i][j] = cnt + '0';
-             return;
-           }
-           else
-           {
-             board[i][j] = 'B';
-           }
-           
-           for (auto& dir: dirs)
-             dfs(board, i + dir.first, j + dir.second);
-
+             if (board[nx][ny] =='E')
+                dfs(board, nx, ny);
+          }
         }
     }
 };
+
+/*
+class Solution {
+public:
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+        
+      // M -> X, 只有 click 到 mine 的时候, 才会爆炸
+      // E -> B
+      // E -> 1-8 digit
+ 
+      if (board.empty() || board[0].empty())
+        return board;
+      
+      int r = click[0];
+      int c = click[1];
+
+      if (board[r][c] == 'M')
+      {
+         board[r][c] = 'X';
+         return board;
+      }
+
+      int m = board.size();
+      int n = board[0].size();
+      queue<pair<int, int>> q;
+      q.push({r, c});
+
+      vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {1, 1}, {1, -1}, {-1, 1}};
+
+      while (!q.empty())
+      {
+         auto [x, y] = q.front();
+         q.pop();
+
+         if (board[x][y] != 'E')
+           continue;
+
+         int cnt = 0;
+         for (auto dir : dirs)
+         {
+           int nx = x + dir.first;
+           int ny = y + dir.second;
+             
+           if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+             continue;
+
+           if (board[nx][ny] == 'M')
+              cnt++;
+          }
+
+          if (cnt > 0)
+          {
+            //如果是数字 就不扩展
+            board[x][y] = cnt + '0';
+            continue;
+          }
+
+          board[x][y] = 'B';
+         
+          for (auto dir : dirs)
+          {
+            int nx = x + dir.first;
+            int ny = y + dir.second;
+
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+              continue;
+            
+            if (board[nx][ny] =='E')
+              q.push({nx, ny});
+          }
+      }
+
+      return board;
+    }
+};
+*/
