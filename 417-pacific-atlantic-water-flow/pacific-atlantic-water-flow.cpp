853dea5692177a73cb04af643af3fd224f247106
borisnorm <borisnorm@gmail.com>
2026-03-03 23:39:54 -0800

    Time: 6 ms (66.05%) | Memory: 24.8 MB (26.46%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,4 +1,88 @@
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        if (heights.empty() || heights[0].empty())
+          return {};
+        
+        int m = heights.size();
+        int n = heights[0].size();
+        
+        vector<vector<int>> pacVis(m, vector<int>(n, 0));
+        vector<vector<int>> atlVis(m, vector<int>(n, 0));
+        
+        //queue<pair<int, int>> pac_q;
+        //queue<pair<int, int>> atl_q;
 
+        vector<pair<int, int>> pac_q;
+        vector<pair<int, int>> atl_q;
+      
+        for (int i = 0; i < m; i++)
+        {
+            pac_q.push_back({i, 0});
+            atl_q.push_back({i, n-1});
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+           pac_q.push_back({0, j});
+           atl_q.push_back({m-1, j});
+        }
+        
+        bfs(pac_q, pacVis, heights);
+        bfs(atl_q, atlVis, heights);
+
+        vector<vector<int>> res;
+        for (int i = 0; i < m; i++)
+        {
+           for (int j = 0; j < n; j++)
+           {
+              if (pacVis[i][j] && atlVis[i][j])
+                res.push_back({i,j});
+           }
+        }
+
+        return res;
+    }
+
+    void bfs(vector<pair<int, int>>& begin_vec, vector<vector<int>>& vis, vector<vector<int>>& heights)
+    {
+        int m = vis.size();
+        int n = vis[0].size();
+
+        queue<pair<int, int>> q;
+        for (auto& [x, y]: begin_vec)
+        {
+            q.push({x, y});
+            vis[x][y] = 1;
+        }
+
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1},  {1, 0},  {-1, 0}};
+        while(!q.empty())
+        {
+           auto [x, y] = q.front();
+           q.pop();
+
+           for (auto& dir : dirs)
+           {
+              int nx = x + dir.first;
+              int ny = y + dir.second;
+
+              if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+                continue;
+              
+              if (vis[nx][ny] == 1)
+                continue;
+
+              if (heights[x][y] <= heights[nx][ny])
+              {
+                  vis[nx][ny] = 1;
+                  q.push({nx, ny});
+              }
+           }
+        }
+    }
+};
+/*
 class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
@@ -66,3 +150,5 @@ public:
         }
     }
 };
+
+*/
