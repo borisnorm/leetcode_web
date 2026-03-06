8ecdd0ebef4fb0e75a3e6bfb09d770d63d827a29
borisnorm <borisnorm@gmail.com>
2026-03-06 14:52:32 -0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (48.14%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+
+        int cnt = 0;
+        while (n)
+        {
+           n = n & (n-1);
+           cnt++;
+        }
+        return cnt;
+    }
+};
+
+/*
+class Solution {
+public:
+    int hammingWeight(int n) {
        int cnt = 0;
        while (n)
        {
@@ -11,3 +26,4 @@ public:
        return cnt;
     }
 };
+*/
