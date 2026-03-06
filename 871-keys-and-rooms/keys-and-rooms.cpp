7729a200ff717d390c82b5199aa5bd40f93a8feb
borisnorm <borisnorm@gmail.com>
2026-03-06 01:14:18 -0800

    Time: 0 ms (100.00%) | Memory: 14.6 MB (54.82%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
+        vector<bool> visited(n, false);
+        
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+
+        while(!q.empty())
+        {
+          int cur = q.front();
+          q.pop();
+
+          for (int next : rooms[cur])
+          {
+              if (visited[next])
+                continue;
+              visited[next] = true;
+              q.push(next);
+          }
+        }
+
+        for (int i = 0; i < n; i++)
+          if (!visited[i])
+            return false;
+        
+        return true;
+    }
+};
+/*
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
         vector<bool> visited(n);
 
         dfs(rooms, 0, visited);
@@ -28,3 +60,5 @@ public:
         }
     }
 };
+
+*/
