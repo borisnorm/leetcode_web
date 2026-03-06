04859d4c0b0932e58bbce4f7ed884cbee5b5ea1c
borisnorm <borisnorm@gmail.com>
2026-03-05 22:32:27 -0800

    Time: 0 ms (100.00%) | Memory: 35 MB (93.77%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -6,6 +6,42 @@ public:
         if (start < 0 || start >= n)
           return false;
 
+        vector<bool> visited(n, 0);
+
+        return dfs(arr, start, visited);
+  }
+
+   bool dfs(vector<int>& arr, int pos, vector<bool>& vis)
+   {
+       int n = arr.size();
+
+       if (pos < 0 || pos >= n)
+         return false;
+       
+       if (vis[pos])
+         return false;
+        
+       vis[pos] = true;
+       
+       if (arr[pos] == 0)
+         return true;
+
+       bool leftFound = dfs(arr, pos-arr[pos], vis);
+       bool rightFound = dfs(arr, pos+arr[pos], vis);
+
+       return leftFound || rightFound;
+   }
+};
+
+/*
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        
+        int n = arr.size();
+        if (start < 0 || start >= n)
+          return false;
+
         vector<bool> visited(n, false);
 
         queue<int> q;
@@ -39,3 +75,5 @@ public:
         return false;
     }
 };
+
+*/
