103be1d73241b3d1c61d2f0a329fbc6a890f9dce
borisnorm <borisnorm@gmail.com>
2026-03-09 01:37:42 -0700

    Time: 7 ms (47.92%) | Memory: 20.3 MB (19.71%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,5 +1,56 @@
 class Solution {
 public:
+    // Time: O(V+E), Space: O(V+E)
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+          vector<vector<int>> adj(numCourses);
+
+          for(auto prereq: prerequisites)
+          {
+             int a = prereq[0];
+             int b = prereq[1];
+
+             adj[b].push_back(a); 
+          }
+
+          vector<int> color(numCourses, 0);
+          for (int i = 0; i < numCourses; i++)
+          {
+              if (!dfs(adj, i, color))
+                return false;
+          }
+
+          return true;
+    }
+
+    // color 0-not_visited 1. in-progress, 2-completed
+    bool dfs(vector<vector<int>>& adj, int u, vector<int>& color)
+    {
+       //if (color[u] == 1)
+       //     return false;
+       color[u] = 1;
+
+        for (int v : adj[u])
+        {
+          if (color[v] == 1)
+            return false;
+
+          if (color[v] == 0)
+          {
+            if (!dfs(adj, v, color))
+              return false;
+          }
+        }
+
+
+        color[u] = 2;
+        return true;
+    }
+};
+/*
+class Solution {
+public:
+    // Time: O(V+E), Space: O(V+E)
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses);
         vector<int> indegree(numCourses, 0);
@@ -26,19 +77,20 @@ public:
         int cnt = 0;
         while (!course_q.empty())
         {
-            int course = course_q.front();
+            int prereqs = course_q.front();
             course_q.pop();
 
             cnt++;
 
-            for (int next : graph[course])
+            for (int pre : graph[prereqs])
             {
-              indegree[next]--;
-              if (indegree[next] == 0)
-                course_q.push(next);
+              indegree[pre]--;
+              if (indegree[pre] == 0)
+                course_q.push(pre);
             }
         }
 
         return cnt == numCourses;
     }
 };
+*/
