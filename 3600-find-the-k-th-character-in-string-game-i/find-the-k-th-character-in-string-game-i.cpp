b12fbadbf6f99eb2affc7c01e5c233cc9344e026
borisnorm <borisnorm@gmail.com>
2026-03-10 12:05:09 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (47.17%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        
+        string s1  = "a";
+        int totalLen = 0;
+        // [0, k-1]
+        // 5 6
+        
+        int loopCnt = 0;
+        for (int i = 0; i < k && totalLen < k; i++)
+        {
+           totalLen = 1 << i;
+           loopCnt = i;
+        }
+
+        for (int i = 0; i < loopCnt; i++)
+        {
+           string s2 = s1;
+
+           for (int j = 0; j < s2.size(); j++)
+           {
+              s2[j] = s1[j] + 1;   
+           }
+
+           s1 += s2;
+        }
+
+        return s1[k-1];
+    }
+};
