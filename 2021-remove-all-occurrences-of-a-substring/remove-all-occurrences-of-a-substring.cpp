0dc40ed3316af5c013aca843a36c605d3462ad53
borisnorm <borisnorm@gmail.com>
2026-03-11 15:19:56 -0700

    Time: 2 ms (41.30%) | Memory: 12.4 MB (5.43%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
+          int pLen = part.size();
+
+         string st = "";
+         for (char c : s)
+         {
+            st += c;
+            // st.size()-pLen 是开始 删除的 pos 直到 末尾
+            if (st.size() >= pLen &&
+                st.substr(st.size()-pLen) == part)
+            {
+               //弹出 part 长度的字符
+               //earse 从 pos 0 开始删除
+               //st.size()-pLen 是开始 删除的 pos 直到 末尾
+               //所以是 栈操作
+               st.erase(st.size()-pLen);
+            }
+         }
+
+         return st;
+    }
+};
+
+/*
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
          if (s.empty() || part.empty())
            return s;
         
@@ -17,3 +43,5 @@ public:
          return s;
     }
 };
+
+*/
