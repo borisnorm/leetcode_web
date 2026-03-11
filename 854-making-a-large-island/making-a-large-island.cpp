9e80cefdd54a43c49dd444ecf225a26df515d10e
borisnorm <borisnorm@gmail.com>
2026-03-10 20:41:57 -0700

    Time: 271 ms (52.80%) | Memory: 151.1 MB (51.89%) - LeetSync

---
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        unordered_map<int, int> area;
+        int id = 2;
+        for (int i = 0; i < n; i++)
+        {
+           for (int j = 0; j < n; j++)
+           {
+              if (grid[i][j] == 1)
+              {
+                 area[id++] = dfs(grid, i, j, id);
+              }
+           }
+        }
+
+        int maxArea = 0;
+        for (auto [id, gridArea]: area)
+          maxArea = max(maxArea, gridArea);
+
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        for (int i = 0; i < n; i++)
+        {
+          for (int j = 0; j < n; j++)
+          {
+            if (grid[i][j] != 0)
+              continue;
+             unordered_set<int> seen;
+            int total = 1; // reverse itselft;
+            
+            for (auto dir: dirs)
+            {
+               int nI = i + dir.first;
+               int nJ = j + dir.second;
+               
+               if (nI < 0 || nI >= n || nJ < 0 || nJ >= n)
+                 continue;
+                
+               int nId = grid[nI][nJ];
+               if (nId >= 2 && !seen.count(nId))
+               { 
+                  seen.insert(nId);
+                  total += area[nId];
+               }
+            }
+            
+            maxArea = max(maxArea, total);
+          }
+        }
+
+        return maxArea;
+    }
+
+    int dfs(vector<vector<int>>& grid, int i, int j, int id)
+    {
+       int n = grid.size();
+       if (i < 0 || i >= n || j < 0 || j >= n)
+         return 0;
+
+       if (grid[i][j] != 1)
+         return 0;
+
+       grid[i][j] = id;
+      
+       int area = 1;
+
+       area += dfs(grid, i+1, j, id);
+       area += dfs(grid, i-1, j, id);
+       area += dfs(grid, i, j+1, id);
+       area += dfs(grid, i, j-1, id);
+
+      return area;
+    }
+};
