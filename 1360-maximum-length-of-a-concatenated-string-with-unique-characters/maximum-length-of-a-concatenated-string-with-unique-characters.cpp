e786557e03fbd18ef918aad7d88f99fd000470a9
borisnorm <borisnorm@gmail.com>
2026-03-12 18:01:41 -0700

    Time: 0 ms (100.00%) | Memory: 10.5 MB (86.02%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int res = 0;
+    int maxLength(vector<string>& arr) {
+        
+        int n = arr.size();
+        vector<int> masks;
+
+        for(string str: arr)
+        {
+          // single words duplicates check
+          bool dup = false;
+          int wordMask = 0;
+          for (char c: str)
+          {
+             int mask = 1 << (c-'a');
+             if ((wordMask & mask) != 0)
+             {
+                dup = true;
+                break;
+             }
+             wordMask |= mask;
+          }
+
+          if (!dup)
+             masks.push_back(wordMask);
+
+        }
+
+        int maxLen = 0;
+        int curMask = 0;
+        int idx = 0;
+        dfs(masks, idx, curMask, maxLen);
+        return maxLen;
+    }
+
+    void dfs(vector<int>& masks, int idx, int curMask, int& maxLen)
+    {
+        maxLen = max(maxLen, __builtin_popcount(curMask));
+
+        if (idx == masks.size())
+          return;
+
+        for (int i = idx; i < masks.size(); i++)
+        {
+           if ((curMask & masks[i]) == 0)
+             dfs(masks, i+1, curMask | masks[i], maxLen);
+        }
+    }
+};
