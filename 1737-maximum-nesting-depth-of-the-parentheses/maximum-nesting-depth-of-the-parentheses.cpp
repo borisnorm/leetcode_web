b2fc2ca17b9516405eaa57d87210458373d7a650
borisnorm <borisnorm@gmail.com>
2026-03-13 18:18:26 -0700

    Time: 0 ms (100.00%) | Memory: 8.3 MB (60.81%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        int depth = 0;
+        int maxDep = 0;
+
+        for (char c: s)
+        {
+          if (c == '(')
+          {
+             depth++;
+             maxDep = max(maxDep, depth);
+          }
+          else if (c == ')')
+          {
+             depth--;
+          }
+        }
+
+        return maxDep;
+    }
+};
+
+/*
+class Solution {
+public:
+    int maxDepth(string s) {
         
         int maxDep = 0;
         stack<char> st;
@@ -20,3 +44,5 @@ public:
         return maxDep;
     }
 };
+
+*/
