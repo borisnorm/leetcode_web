1a5707f422fea3a9b6d023123055632802110088
borisnorm <borisnorm@gmail.com>
2026-03-14 17:52:11 -0700

    Time: 11 ms (97.17%) | Memory: 139.1 MB (66.90%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -10,14 +10,20 @@ public:
 
         for (int i = 0; i < n; i++)
         {
+          //[i−k+1,i] 是 对于 i 这个位置 大小为 k 的 window
+          // 当队列 front 的 index < 当前的 leftmost index 时候
+          // pop 的作用主要是为了保证窗口正确性，而不是单纯维持大小 
           if (!dq.empty() && dq.front() < i - k + 1)
             dq.pop_front();
 
-          while (!dq.empty() && nums[dq.back()] < nums[i])
+          //  维护单调递减, 然后 dq.front 就是这个 window 的最大值
+          while (!dq.empty() && nums[i] > nums[dq.back()])
             dq.pop_back();
 
           dq.push_back(i);
           
+          // k 是 1-based, 要转换成 0-based
+          // 否则 i < k - 1的时候, window 还没有形成
           if (i >= k - 1)
             res.push_back(nums[dq.front()]);
         }
