ee8c7b449068a7d84a9b399a4761e1c152cfdf1a
borisnorm <borisnorm@gmail.com>
2026-03-15 21:17:13 -0700

    Time: 41 ms (63.18%) | Memory: 45.5 MB (53.98%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -18,6 +18,14 @@ public:
            // check if previous prefixSum[i] does exist or not
            // 与 two Sum 那个题 类似
            // [i + 1, j] 就是这个解
+           //这道题是在做：
+
+          // 当前右端点是 i
+          // 找到 mp[cur-k] 个合法左端点
+         // 这 mp[cur-k] 个左端点分别对应 mp[cur-k] 个子数组
+          // 这些子数组都只是新增答案
+          //所以应该加到总数里，而不是和旧答案做组合
+
            if (prefixSumCnt.count(prefixSum - k))
              cnt += prefixSumCnt[prefixSum - k];
 
