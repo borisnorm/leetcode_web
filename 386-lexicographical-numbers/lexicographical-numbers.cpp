a8bfe35a001e4000077d70bd447850eafca28402
borisnorm <borisnorm@gmail.com>
2026-03-16 03:12:40 -0700

    Time: 0 ms (100.00%) | Memory: 13.9 MB (79.61%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+
+        int cur = 1;
+
+        while (res.size() < n)
+        {
+           res.push_back(cur);
+
+           if (cur * 10 <= n)
+             cur *= 10;
+           else
+           {
+              while (cur  % 10 == 9 || cur >= n)
+                cur /= 10;
+              
+              cur++;
+           }
+        }
+
+        return res;
+    }
+};
