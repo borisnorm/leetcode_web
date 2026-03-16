dd35d64effbb1743e5b77252b8468091d640c549
borisnorm <borisnorm@gmail.com>
2026-03-15 21:05:32 -0700

    Time: 35 ms (86.42%) | Memory: 45.5 MB (38.73%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -5,6 +5,7 @@ public:
 
         unordered_map<int, int> prefixSumCnt;
         prefixSumCnt[0] = 1;
+        
         int cnt = 0;
         int prefixSum = 0;
 
@@ -12,6 +13,11 @@ public:
         {
            prefixSum += num;
            
+           // prefixSum[j] - prefixSum[i] = k
+           // prefixSum[i] = prefix[j] - k;
+           // check if previous prefixSum[i] does exist or not
+           // 与 two Sum 那个题 类似
+           // [i + 1, j] 就是这个解
            if (prefixSumCnt.count(prefixSum - k))
              cnt += prefixSumCnt[prefixSum - k];
 
