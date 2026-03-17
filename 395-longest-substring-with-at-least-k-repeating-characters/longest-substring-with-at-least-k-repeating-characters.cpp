8c6131ce92f4331157367dcf9b5b6643ce5dbf1e
borisnorm <borisnorm@gmail.com>
2026-03-17 15:26:12 -0700

    Time: 64 ms (52.24%) | Memory: 12.9 MB (47.84%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+         // >= k  < k 作为边界
+        int n = s.size();
+        // [l, r)
+        return dfs(s, 0, n, k);
+    }
+
+    int dfs(const string& s, int l, int r, int k)
+    {
+         if (r - l < k)
+           return 0;
+         
+         //int freq[26];
+         int freq[26] = {};
+         for (int i = l; i < r; i++)
+           freq[s[i]-'a']++;
+
+        unordered_map<char, int> char2freq;
+       
+        for (int i = l; i < r; i++)
+        {
+            if (freq[s[i]-'a'] < k)
+            {
+               //不包括 s[i]这个位置
+               int left = dfs(s, l, i, k);
+               int right = dfs(s, i+1, r, k);
+               return max(left, right);
+            }
+        }
+
+        // 没有分割点 → [l, r) 整段满足条件 
+        return r-l;
+    }
+};
