d3525c4a87343e3200051885087868fa21b5cd1a
borisnorm <borisnorm@gmail.com>
2026-03-16 22:50:36 -0700

    Time: 45 ms (85.32%) | Memory: 180.4 MB (71.63%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,5 +1,54 @@
 class NumArray {
 public:
+    vector<int> bit;
+    vector<int> orig;
+    int n;
+
+    int lowbit(int x)
+    {
+      return x & (-x);
+    }
+
+    void add(int idx, int delta)
+    {
+      while (idx <= n)
+      {
+        bit[idx] += delta;
+        idx += lowbit(idx);
+      }
+    }
+    int prefixSum(int idx)
+    {
+        int sum = 0;
+        while (idx > 0)
+        {
+           sum += bit[idx];
+           idx -= lowbit(idx);
+        }
+
+        return sum;
+    }
+    NumArray(vector<int>& nums) {
+       n = nums.size();
+       bit.assign(n+1, 0);
+       orig = nums;
+       for (int i = 0; i < n; i++)
+         add(i+1, orig[i]);
+    }
+
+    void update(int index, int val) {
+        int diff = val - orig[index];
+        orig[index] = val;
+        add(index+1, diff);
+    }
+
+    int sumRange(int left, int right) {
+      return prefixSum(right+1) - prefixSum(left);
+    }
+};
+/*
+class NumArray {
+public:
     vector<int> prefixSum;
     vector<int> orig;
     int maxLen = 0;
@@ -34,6 +83,7 @@ public:
     }
 };
 
+*/
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
