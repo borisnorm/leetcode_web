4081cc7c197e66e923007224a2f124867076b02b
borisnorm <borisnorm@gmail.com>
2026-03-17 17:45:15 -0700

    Time: 0 ms (100.00%) | Memory: 7.9 MB (50.43%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -9,4 +9,16 @@ public:
 
         return target % gcd(x, y) == 0;
     }
+
+    //辗转相除法
+    int gcd2(int a, int b)
+    {
+      while (b != 0)              // b 不为 0 就继续
+      {
+        int tmp = a % b;        // 求余数
+        a = b;                  // a 换成 b
+        b = tmp;                // b 换成余数
+      }
+      return a;                   // b=0 时 a 就是答案
+   }
 };
