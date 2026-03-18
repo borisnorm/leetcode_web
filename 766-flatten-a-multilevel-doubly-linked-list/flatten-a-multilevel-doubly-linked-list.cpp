e7012885ed9e1c478e97f5c132d179c046272520
borisnorm <borisnorm@gmail.com>
2026-03-18 16:47:47 -0700

    Time: 3 ms (64.44%) | Memory: 11.8 MB (9.84%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if (!head || (!head->next && !head->child))
+          return head;
+
+        Node* cur = head;
+        Node* nxt = nullptr;
+        Node* cur_child = nullptr;
+        while(cur)
+        {
+           if (!cur->child)
+           {
+              cur = cur->next;
+              continue;
+           }
+          
+           nxt = cur->next;
+           cur_child = cur->child;
+
+           /* child tail */
+           while (cur_child->next)
+             cur_child = cur_child->next;
+          
+          /* tail -> nxt */
+          cur_child->next = nxt;
+          if (nxt)
+            nxt->prev = cur_child;
+
+          cur->next = cur->child;
+          cur->child->prev = cur;
+
+          cur->child = nullptr;
+        }
+        return head;
+    }
+};
