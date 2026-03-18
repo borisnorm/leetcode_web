21d40c75944d7abba6746b595a75f04240d4c4a7
borisnorm <borisnorm@gmail.com>
2026-03-17 18:16:52 -0700

    Time: 0 ms (100.00%) | Memory: 8.1 MB (38.04%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -1,3 +1,5 @@
+
+/*
 class Solution {
 public:
     bool canMeasureWater(int x, int y, int target) {
@@ -21,4 +23,55 @@ public:
       }
       return a;                   // b=0 时 a 就是答案
    }
+};
+*/
+
+class Solution
+{
+public:
+    bool canMeasureWater(int j1, int j2, int target)
+    {
+
+        int x = j1;
+        int y = j2;
+        // 特判
+        if (target == 0) return true;
+        if (target > j1 + j2) return false;
+
+        // BFS 初始化
+        std::queue<std::pair<int,int>> q;
+        std::set<std::pair<int,int>> vis;
+
+        q.push({0, 0});
+        vis.insert({0, 0});
+
+        while (!q.empty())
+        {
+            auto [x, y] = q.front();   // 当前状态
+            q.pop();
+
+            // 找到答案
+            if (x + y == target) return true;
+
+            // 6 种操作，生成下一个状态
+            std::vector<std::pair<int,int>> nexts =
+            {
+                {j1, y},                                        // 装满壶1
+                {x, j2},                                        // 装满壶2
+                {0, y},                                         // 倒空壶1
+                {x, 0},                                         // 倒空壶2
+                {x - std::min(x, j2-y), y + std::min(x, j2-y)}, // 壶1倒入壶2
+                {x + std::min(y, j1-x), y - std::min(y, j1-x)}  // 壶2倒入壶1
+            };
+
+            for (auto& [nx, ny] : nexts)
+            {
+                if (vis.count({nx, ny})) continue;  // 已访问跳过
+                vis.insert({nx, ny});
+                q.push({nx, ny});
+            }
+        }
+
+        return false;   // 所有状态遍历完，找不到
+    }
 };
