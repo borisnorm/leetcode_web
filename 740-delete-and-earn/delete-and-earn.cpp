de5048caf4138562bbf96c55fc737b456968cfee
borisnorm <borisnorm@gmail.com>
2026-03-21 00:47:50 -0700

    Time: 0 ms (100.00%) | Memory: 15.5 MB (98.03%) - LeetSync

---
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        
+        if (nums.empty())
+          return 0;
+
+        int n = nums.size();
+        int maxVal = INT_MIN;
+        for (int& num: nums)
+          maxVal = max(maxVal, num);
+
+        vector<int> points(maxVal + 1, 0);
+        for(int num: nums)
+          points[num] += num;
+        
+        int prev2 = 0;
+        int prev1 = points[0];
+        
+        for (int i = 1; i <= maxVal; i++)
+        {
+           int prev = max(prev1, prev2 + points[i]);
+           prev2 = prev1;
+           prev1 = prev;
+        }
+
+        return prev1;
+    }
+};
