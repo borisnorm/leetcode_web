0c419fa82797e6adfe4a632a3312f598c671d3e2
borisnorm <borisnorm@gmail.com>
2026-03-22 16:03:57 -0700

    Time: 543 ms (8.40%) | Memory: 152.6 MB (65.16%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -5,6 +5,42 @@ public:
 
         for(int x: arr1)
         {
+          // 错误 x_str.substr 是 [l, r) 的用法
+           //for (int i = 0; i < x_str.size(); i++)
+           while (x)
+           {
+              prefix.insert(to_string(x));
+              x /= 10;
+           }
+        }
+
+        int maxLen = 0;
+        for (int y: arr2)
+        {
+          
+           while(y)
+           {
+              if (prefix.count(to_string(y)))
+              {
+                 int len = to_string(y).size();
+                 maxLen = max(maxLen, len);
+                 break;
+              }
+              y /= 10;
+           }
+        }
+
+        return maxLen;
+    }
+};
+/*
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_set<string> prefix;
+
+        for(int x: arr1)
+        {
            string x_str = to_string(x);
 
           // 错误 x_str.substr 是 [l, r) 的用法
@@ -30,3 +66,5 @@ public:
         return maxLen;
     }
 };
+
+*/
