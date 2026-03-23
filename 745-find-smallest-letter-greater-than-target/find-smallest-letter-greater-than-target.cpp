73e1c4dd42eb3f39af1c11375306e593b0f43b4b
borisnorm <borisnorm@gmail.com>
2026-03-23 00:58:15 -0700

    Time: 0 ms (100.00%) | Memory: 19.8 MB (23.41%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        // first two
-
+        
         int n = letters.size();
         int l = 0;
         int r = n - 1;
@@ -10,6 +9,7 @@ public:
         while (l < r)
         {
            int mid = l + (r-l)/2;
+           // upper bound  > target, 返回第一个大于 target 的 l
            if (letters[mid] > target)
            {
               r = mid;
@@ -19,10 +19,37 @@ public:
               l = mid + 1;
            }
         }
-
+        
+        // 没有找到结果, 根据题意返回 第一个字符
         if (letters[l] <= target)
           return letters[0];
         
         return letters[l];
     }
 };
+
+/*
+char nextGreatestLetter(vector<char>& letters, char target)
+{
+    int n = (int)letters.size();
+    int left = 0;
+    int right = n;
+
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (letters[mid] > target)
+        {
+            right = mid;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return left == n ? letters[0] : letters[left];
+}
+
+*/
