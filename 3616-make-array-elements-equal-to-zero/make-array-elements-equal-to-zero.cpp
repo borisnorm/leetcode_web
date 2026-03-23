a635dba9b730f34e2c4610a6176df24747f4565a
borisnorm <borisnorm@gmail.com>
2026-03-23 00:29:22 -0700

    Time: 8 ms (41.24%) | Memory: 22.1 MB (55.74%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,16 +1,51 @@
-
 class Solution
 {
 public:
   int countValidSelections(vector<int>& nums)
   {
+      int total = 0;
+      for (int num: nums)
+        total += num;
+      
+      int cnt = 0;
+      int prefix = 0;
+      int n = nums.size();
+      for (int i = 0; i < n; i++)
+      {
+          if (nums[i] != 0)
+          {
+             prefix += nums[i];
+             continue;
+          }
+
+          // 0 case: check left and right prefixSum;
+
+          int leftSum = prefix;
+          int rightSum = total - prefix;
+
+          if (leftSum == rightSum)
+            cnt += 2;
+          else if (abs(leftSum - rightSum) == 1)
+            cnt += 1;
+      }
+
+      return cnt;
+  }
+};
+/*
+class Solution
+{
+public:
+  int countValidSelections(vector<int>& nums)
+  {
+    int n = nums.size();
     int total = 0;
     for (int x : nums)
        total += x;
     
     int res = 0;
     int prefix = 0;
-    int n = nums.size();
+
     for (int i = 0; i < n; i++)
     {
         if (nums[i] != 0)
@@ -22,10 +57,14 @@ public:
         int L = prefix;
         int R = total - prefix; // nums[i]=0 不影响
         
-        if (L == R)       res += 2; // 两个方向都行
-        else if (abs(L-R) == 1) res += 1; // 只有一个方向行
+        if (L == R)
+          res += 2; // 两个方向都行
+        else if (abs(L-R) == 1)
+          res += 1; // 只有一个方向行
     }
   
     return res;
   }
 };
+
+*/
