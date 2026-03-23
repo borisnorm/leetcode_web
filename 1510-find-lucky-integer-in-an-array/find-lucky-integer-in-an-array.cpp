be3e5956d0cd3c97f6b2c90c39f4966f16b0313a
borisnorm <borisnorm@gmail.com>
2026-03-22 22:02:53 -0700

    Time: 0 ms (100.00%) | Memory: 14 MB (59.79%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -5,12 +5,12 @@ public:
         unordered_map<int, int> num2freq;
         int maxLucky = INT_MIN;
 
-        for (int num: arr)
+        for (int& num: arr)
           num2freq[num]++;
         
-        for (int num: arr)
+        for (auto [num, freq]: num2freq)
         {    
-           if (num2freq[num] == num)
+           if (freq == num)
              maxLucky = max(maxLucky, num);
         }
         return maxLucky == INT_MIN ? -1 : maxLucky;
