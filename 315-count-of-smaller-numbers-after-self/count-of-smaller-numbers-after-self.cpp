cfc86e96ce6f3483b7ea6a78e16a397e33577655
borisnorm <borisnorm@gmail.com>
2026-03-23 20:47:45 -0700

    Time: 945 ms (24.22%) | Memory: 510.4 MB (25.03%) - LeetSync

---
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+    vector<int> ans;
+    void mergeSort(vector<pair<int, int>>& idx_arr, int l, int r)
+    {
+       if (r - l <= 1)
+         return;
+       int mid = l + (r-l)/2;
+       
+       mergeSort(idx_arr, l, mid);
+       mergeSort(idx_arr, mid, r);
+
+       vector<pair<int, int>> tmp;
+       int i = l;
+       int j = mid;
+
+       int right_picked = 0;
+
+       while (i < mid & j < r)
+       {
+          if (idx_arr[j].first < idx_arr[i].first)
+          {
+             tmp.push_back(idx_arr[j++]);
+             right_picked++;
+          }
+          else
+          {
+             ans[idx_arr[i].second] += right_picked;
+             tmp.push_back(idx_arr[i++]);
+          }
+       }
+
+       while(i < mid)
+       {
+         ans[idx_arr[i].second] += right_picked;
+         tmp.push_back(idx_arr[i++]);
+       }
+
+       while(j < r)
+         tmp.push_back(idx_arr[j++]);
+
+       for (int k = l; k < r; k++)
+         idx_arr[k] = tmp[k-l];
+       
+    }
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        ans.assign(n, 0);
+
+        //构建 value, original_idx 数组
+        vector<pair<int, int>> idx_arr(n);
+        for (int i = 0; i < n; i++)
+          idx_arr[i] = {nums[i], i};
+        
+        // [0, n)
+        mergeSort(idx_arr, 0, n);
+        return ans;
+    }
+};
