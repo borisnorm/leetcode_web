713ee5d8886b9df4549f3a5ef12ef5f070210696
borisnorm <borisnorm@gmail.com>
2026-03-23 17:51:00 -0700

    Time: 515 ms (15.39%) | Memory: 159.6 MB (45.12%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,40 +1,37 @@
-class Solution
-{
+class Solution {
 public:
-int maxFrequency(vector<int>& nums, int k, int numOps)
-{
-    sort(nums.begin(), nums.end());         // 排序，方便二分
-    int n = nums.size();
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
 
-    // 收集所有候选目标值：nums[i], nums[i]-k, nums[i]+k
-    set<int> cands;
-    for (int x : nums)
-    {
-        cands.insert(x);
-        cands.insert(x - k);
-        cands.insert(x + k);
+        set<int> cands;
+        for (int num: nums)
+        {
+           cands.insert(num);
+           cands.insert(num - k);
+           cands.insert(num + k);
+        }
+
+        int res = 0;
+        for (int target: cands)
+        {
+           int lo = (int)(lower_bound(nums.begin(), nums.end(), target-k) - nums.begin());
+           int hi = (int)(upper_bound(nums.begin(), nums.end(), target+k) - nums.begin());
+           //[low, hi)
+           int window = hi - lo; 
+
+           int exact_lo = (int)(lower_bound(nums.begin(), nums.end(), target) - nums.begin());
+           int exact_hi = (int)(upper_bound(nums.begin(), nums.end(), target) - nums.begin());
+
+           int exact = exact_hi - exact_lo;
+
+           //需要操作的数量，受 numOps 限制, 所以是 min
+           int ops = min(numOperations, window-exact);
+
+           res = max(res, exact + ops);
+        }
+
+        return res;        
     }
-
-    int ans = 0;
-
-    for (int t : cands)
-    {
-        // 统计 [t-k, t+k] 范围内的元素数（都可以变成 t）
-        int lo = (int)(lower_bound(nums.begin(), nums.end(), t - k) - nums.begin());
-        int hi = (int)(upper_bound(nums.begin(), nums.end(), t + k) - nums.begin());
-        int window = hi - lo;               // 可以变成 t 的元素总数
-
-        // 统计已经等于 t 的元素数（不需要操作）
-        int el = (int)(lower_bound(nums.begin(), nums.end(), t) - nums.begin());
-        int er = (int)(upper_bound(nums.begin(), nums.end(), t) - nums.begin());
-        int exact = er - el;                // 已等于 t 的数量
-
-        // 需要操作的数量，受 numOps 限制
-        int ops = min(window - exact, numOps);
-
-        ans = max(ans, exact + ops);
-    }
-
-    return ans;
-}
 };
