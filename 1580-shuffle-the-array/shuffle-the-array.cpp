5c25040f0ab49dd03b3cdb5315070b3c8726690e
borisnorm <borisnorm@gmail.com>
2026-03-25 17:09:44 -0700

    Time: 4 ms (64.42%) | Memory: 13.5 MB (17.36%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+
+        for (int i = 0; i < n; i++)
+        {
+            // push_back 不能初始化 vector 的大小
+            res.push_back(nums[i]);
+            res.push_back(nums[i + n]);  // 2n-1 = n-1+n 
+        }
+
+        return res;
+    }
+};
+/*
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> res(2*n, 0);
         int idx = 0;
         for (int i = 0; i < n; i++)
@@ -13,3 +29,4 @@ public:
         return res;
     }
 };
+*/
