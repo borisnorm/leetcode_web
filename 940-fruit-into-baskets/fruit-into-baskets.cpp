b6e0ffbb75d4bb22013373b2b7eac2a2e81f1bcb
borisnorm <borisnorm@gmail.com>
2026-03-25 18:19:56 -0700

    Time: 37 ms (76.24%) | Memory: 84.9 MB (63.25%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -5,27 +5,20 @@ public:
         int n = fruits.size();
         int l = 0;
         int r = 0;
-        unordered_map<int, int> fruit2cnt;
-        int window = 0;
-
+        unordered_map<int, int> fruitType2cnt;
         int maxFruits = 0;
         while (r < n)
         {
-            int fruit = fruits[r];
-            if (!fruit2cnt.count(fruit))
-              window++;
-            fruit2cnt[fruit]++;
+            int fruitType = fruits[r];
+            fruitType2cnt[fruitType]++;
             r++;
 
-            while (window > 2)
+            while (fruitType2cnt.size() > 2)
             {
-               int fruit2 = fruits[l];
-               fruit2cnt[fruit2]--;
-               if (fruit2cnt[fruit2] == 0)
-               {
-                  window--;
-                  fruit2cnt.erase(fruit2);
-               }
+               int fruitType2 = fruits[l];
+               fruitType2cnt[fruitType2]--;
+               if (fruitType2cnt[fruitType2] == 0)
+                  fruitType2cnt.erase(fruitType2);
                l++;
             }
 
@@ -65,8 +58,8 @@ public:
                fruit2cnt[fruit2]--;
                if (fruit2cnt[fruit2] == 0)
                {
-                  
                   window--;
+                  fruit2cnt.erase(fruit2);
                }
                l++;
             }
@@ -81,5 +74,4 @@ public:
         return maxFruits;
     }
 };
-
 */
