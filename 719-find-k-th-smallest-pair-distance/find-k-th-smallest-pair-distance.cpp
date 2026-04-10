cf39c95b5dc02ae94d1be5342952f65254a42ea9
borisnorm <borisnorm@gmail.com>
2026-04-09 18:29:44 -0700

    Time: 5 ms (34.24%) | Memory: 14 MB (39.56%) - LeetSync

---
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    int countPairs(vector<int>& nums, int mid)
+    {
+       int cnt = 0;
+       int i = 0;
+       int n = nums.size();
+
+       for (int j = 1; j < n; j++)
+       {
+          // 缩小 左端, 保证 nums[j] - nums[i] <= mid
+          while (nums[j] - nums[i] > mid)
+            i++;
+          // 以 j 为 右端,
+          // 合法左端为 [i, j-1]共 j-i 个 pair
+          cnt += j - i;
+       }
+
+       return cnt;
+    }
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int lo = 0;
+        int hi = nums.back() - nums.front();
+
+        while (lo < hi)
+        {
+           int mid = lo + (hi-lo)/2;
+           if (countPairs(nums, mid) >= k)
+             hi = mid;
+           else
+             lo = mid + 1;
+        }
+
+        return lo;
+    }
+};
