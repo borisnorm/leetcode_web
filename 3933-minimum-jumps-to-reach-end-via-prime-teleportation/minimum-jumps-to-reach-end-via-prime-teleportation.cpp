a5d3f3a99de0885f2a18d8a22b46576c8cb89cde
borisnorm <borisnorm@gmail.com>
2026-04-10 17:46:43 -0700

    Time: 720 ms (54.58%) | Memory: 367.9 MB (36.26%) - LeetSync

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -4,24 +4,24 @@ public:
     int minJumps(vector<int>& nums)
     {
         int n = (int)nums.size();                                  
-        if (n == 1)                                     // 只有一个位置，已经在终点
-            return 0;                                   // 不需要跳
+        if (n == 1)                   // 只有一个位置，已经在终点
+            return 0;                 // 不需要跳
 
-        int maxVal = 0;                                 // 数组中的最大值
-        for (int x : nums)                              // 遍历数组
-          maxVal = max(maxVal, x);                      // 更新最大值
+        int maxVal = 0;               // 数组中的最大值
+        for (int x : nums)            // 遍历数组
+          maxVal = max(maxVal, x);    // 更新最大值
         //Smallest Prime Factor
         vector<int> smallPrimeFactors(maxVal + 1, 0);   // smallPrimeFactor[x] 表示 x 的最小质因子
 
         for (int i = 2; i <= maxVal; i++)
         {
-           if (smallPrimeFactors[i] != 0)           // 如果还没有被标记，说明 i 是质数
+           if (smallPrimeFactors[i] != 0)  // 如果还没有被标记，说明 i 是质数
              continue;
           
            // 下面的 将 以 i 这个质数为底的 合数 给赋值
            // 正好对应上面的检查
-           smallPrimeFactors[i] = i;              // 质数的最小质因子就是自己
-           if ((long long)i * i <= maxVal)        // 防止越界，并且从 i*i 开始筛
+           smallPrimeFactors[i] = i;        // 质数的最小质因子就是自己
+           if ((long long)i * i <= maxVal)  // 防止越界，并且从 i*i 开始筛
            {
              for (long long j = 1LL * i * i; j <= maxVal; j += i)
              {
@@ -46,49 +46,50 @@ public:
             }
         }
 
-        vector<int> dist(n, -1);                           // dist[i] = 从 0 跳到 i 的最少步数
-        vector<char> usedPrime(maxVal + 1, 0);             // usedPrime[p] = prime p 的传送是否已经处理过
-        queue<int> q;                                      // BFS 队列
+        vector<int> dist(n, -1);                 // dist[i] = 从 0 跳到 i 的最少步数
+        vector<char> usedPrime(maxVal + 1, 0);  // usedPrime[p] = prime p 的传送是否已经处理过
+        queue<int> q;                           // BFS 队列
 
-        dist[0] = 0;                                       // 起点距离为 0
-        q.push(0);                                         // 起点入队
+        dist[0] = 0;   // 起点距离为 0
+        q.push(0);     // 起点入队
 
-        while (!q.empty())                                 // 标准 BFS
+        while (!q.empty())      // 标准 BFS
         {
-            int i = q.front();                             // 取出当前下标
-            q.pop();                                       // 出队
+            int i = q.front();  // 取出当前下标
+            q.pop();            // 出队
 
-            int d = dist[i];                                       // 当前下标的最短距离
-            if (i == n - 1)                                        // 如果已经到终点
-                return d;                                          // 直接返回最少步数
+            int d = dist[i];    // 当前下标的最短距离
+            if (i == n - 1)     // 如果已经到终点
+                return d;       // 直接返回最少步数
 
-            if (i - 1 >= 0 && dist[i - 1] == -1)                   // 尝试向左走一步
+            if (i - 1 >= 0 && dist[i - 1] == -1)  // 尝试向左走一步
             {
-                dist[i - 1] = d + 1;                               // 记录距离
-                q.push(i - 1);                                     // 入队
+                dist[i - 1] = d + 1; // 记录距离
+                q.push(i - 1);       // 入队
             }
 
-            if (i + 1 < n && dist[i + 1] == -1)                    // 尝试向右走一步
+            if (i + 1 < n && dist[i + 1] == -1)  // 尝试向右走一步
             {
-                dist[i + 1] = d + 1;                               // 记录距离
-                q.push(i + 1);                                     // 入队
+                dist[i + 1] = d + 1;    // 记录距离
+                q.push(i + 1);          // 入队
             }
 
-            int val = nums[i];                                     // 当前下标对应的值
-            if (val >= 2 && smallPrimeFactors[val] == val && !usedPrime[val])    // 只有当前值本身是质数，且这个 prime 还没用过，才能传送
+            int val = nums[i];          // 当前下标对应的值
+            // 只有当前值本身是质数，且这个 prime 还没用过，才能传送
+            if (val >= 2 && smallPrimeFactors[val] == val && !usedPrime[val])    
             {
-                usedPrime[val] = 1;                                // 标记这个 prime 已经处理过
-                for (int nxt : bucket[val])                        // 枚举所有能被 val 整除的位置
+                usedPrime[val] = 1;           // 标记这个 prime 已经处理过
+                for (int nxt : bucket[val])   // 枚举所有能被 val 整除的位置
                 {
-                    if (dist[nxt] == -1)                           // 如果该位置还没访问过
+                    if (dist[nxt] == -1)      // 如果该位置还没访问过
                     {
-                        dist[nxt] = d + 1;                         // 传送一次，距离加 1
-                        q.push(nxt);                               // 入队
+                        dist[nxt] = d + 1;    // 传送一次，距离加 1
+                        q.push(nxt);          // 入队
                     }
                 }
             }
         }
 
-        return -1;                                                 // 理论上一定可达，这里只是保险返回
+        return -1;   // 理论上一定可达，这里只是保险返回
     }
 };
