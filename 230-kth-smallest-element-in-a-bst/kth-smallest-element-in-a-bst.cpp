765f50ba1277748cc3059e16c9ba68a4d891efbd
borisnorm <borisnorm@gmail.com>
2026-04-15 16:47:57 -0700

    Time: 0 ms (100.00%) | Memory: 24.5 MB (40.22%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -21,7 +21,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// error 
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+       stack<TreeNode*> st;
+       TreeNode* cur = root;
+
+       while (cur || !st.empty())
+       {
+          while (cur)
+          {
+             st.push(cur);
+             cur = cur->left;
+          }
+
+          cur = st.top();
+          st.pop();
+
+          k--;
+          if (k == 0)
+            return cur->val;
+
+          cur = cur->right;
+       }
+
+       return -1;
+    }
+};
+ /*
 class Solution {
 public:
    
@@ -43,11 +70,13 @@ public:
         if (!root)
           return;
         
+        //防御性退出
         if (rank >= k)
           return;
 
         traverse(root->left, k);
         
+        //防御性退出
         if (rank >= k)
           return;
 
@@ -59,5 +88,6 @@ public:
         }
         traverse(root->right, k);
     }
-
 };
+
+*/
