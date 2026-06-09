4a38bc20dc60d5f838cb7486c4532c7ae472566c
borisnorm <borisnorm@gmail.com>
2026-06-08 20:14:50 -0700

    Time: 0 ms (100.00%) | Memory: 11.2 MB (90.24%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,6 +4,42 @@ public:
         if (s.empty())
           return 0;
         
+        int char2freq[128];
+        
+        int l = 0;
+        int r = 0;
+        int n = s.size();
+        int maxLen = 0;
+
+        while (r < n)
+        {
+           char c = s[r];
+           char2freq[c]++;
+           r++;
+
+           while (char2freq[c] > 1)
+           {
+              char d = s[l];
+              char2freq[d]--;
+              l++;
+           }
+
+           maxLen = max(maxLen, r - l);
+        }
+
+        return maxLen;
+    }
+};
+
+
+/*
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty())
+          return 0;
+        
         unordered_map<char, int> char2freq;
         
         int l = 0;
@@ -32,3 +68,5 @@ public:
         return maxLen;
     }
 };
+
+*/
