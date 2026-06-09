3347439612050aec09d95f613b7477325c413cfb
borisnorm <borisnorm@gmail.com>
2026-06-08 17:43:31 -0700

    Time: 19 ms (22.63%) | Memory: 11.6 MB (25.50%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -2,60 +2,62 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         string res = "";
-        if (s.empty() || t.empty()) 
+        if (s.empty() || t.empty())
           return res;
+        
+        // target: minium window of s 
+        //       m = s.size()
 
         int m = s.size();
         int n = t.size();
-        
-         if (n > m)
-           return "";
-           
-        unordered_map<char, int> need;
-        unordered_map<char, int> window;
-         // get t freq
-         // slide window on s to match t
-         // calculate len on matched case
-         // return minLen
-        for (char c : t)
-          need[c]++;
 
+        /* freq window */
+        unordered_map<char, int> window;
+        unordered_map<char, int> need;
+        
+        for(char c: t)
+           need[c]++;
+        
         int l = 0;
         int r = 0;
-        int valid = 0;
+        int valid_cnt = 0;  // valid char cnt in s, which maps to t
         int len = INT_MAX;
         int start = 0;
 
         while (r < m)
         {
-            char c = s[r];  // [l, r)
-            if (need.count(c))
-            {
-               window[c]++;
-               if (window[c] == need[c])
-                 valid++;
-            }
-            r++;
+           char c = s[r];
+           r++;
 
-            while (valid == need.size())
-            {
-               if (r - l  < len)
+           if (need.count(c))
+           {
+              window[c]++;
+              if (window[c] == need[c])
+                valid_cnt++;
+           }
+
+           while(valid_cnt == need.size())
+           {
+               if (r - l < len)
                {
-                 len = r - l;
-                 start = l;
+                  len = r - l;
+                  start = l;
                }
 
                char d = s[l];
+               l++;
+
                if (need.count(d))
                {
-                 if(window[d] == need[d])
-                    valid--;
+                  if (window[d] == need[d])
+                    valid_cnt--;
                   window[d]--;
-               } 
-               l++;
-            }
+               }
+           }
+           
         }
-    
-       return  len == INT_MAX ? "" : s.substr(start, len);
+
+        //  check if s cantains all the character of t
+        return len == INT_MAX ? "" : s.substr(start, len);
     }
 };
